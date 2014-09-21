fd6a84a7d707ebd6d369eae0ddb3a7956c27dea9
mywork-dragon <dragonblueyounger@gmail.com>
2014-09-20 18:32:50 -0700

    add more methods

--- app/services/scrape_service.rb
+++ app/services/scrape_service.rb
@@ -8,7 +8,7 @@ class ScrapeService
   def scrape(company)
     content = open(company.website).read
     result = ScrapedResult.create!(company: company, url: company.website, raw_html: content.truncate(1024), status: :success)
-    matched_services = @matchers.select{|m| m.match?(content)}.map(&:service_id).uniq
+    matched_service = matched_services_in_content(content)
     matched_services.each do |match|
       puts "found a match! #{company.name} is using #{match}"
       company.installations.create!(service_id: match, scraped_result: result)
@@ -16,6 +16,25 @@ class ScrapeService
   rescue
     ScrapedResult.create(company: company, url: company.website, raw_html: $!.message.truncate(1024), status: :fail)
   end
+  
+  def scrape_test(url)
+    content = open(url).read
+    service_names = []
+    matched_services_in_content(content).each do |match|
+      service_name = Service.find(match).name
+      puts "found a match! #{url} is using #{service_name}"
+      service_names << service_name
+    end
+    
+    service_names
+  end
+  
+  private
+  
+    def matched_services_in_content(content)
+      @matchers.select{|m| m.match?(content)}.map(&:service_id).uniq
+    end
+  
 
   class << self
     def scrape_all
@@ -28,6 +47,10 @@ class ScrapeService
 
     def scrape(company)
       ScrapeService.new.scrape(company)
+    end
+    
+    def scrape_test(url)
+      ScrapeService.new.scrape_test(url)
     end
   end
 
