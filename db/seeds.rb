5b688b6a02aacdb45ce63175750155c60abf5069
mywork-dragon <dragonblueyounger@gmail.com>
2014-09-21 16:47:56 -0700

    comment out our seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,104 +26,104 @@ Dir[Rails.root + "db/bizible/services/*.txt"].each do |f|
 end
 
 
-# cbs
-Matcher.create(
-  service: Service.find_by_name("Optimizely"),
-  match_type: :regex,
-  match_string: "cdn.optimizely.com\/js\/([^.]+).js"
-)
-
-# adroll
-Matcher.create(
-  service: Service.find_by_name("Olark"),
-  match_type: :regex,
-  match_string: "olark.identify\(([^)]+)\)"
-)
-
-
-# optimizely, personal capital
-Matcher.create(
-  service: Service.find_by_name("Kissmetrics"),
-  match_type: :regex,
-  match_string: "i.kissmetrics.com\/i.js"
-)
-
-# NBC
-Matcher.create(
-  service: Service.find_by_name("Omniture"),
-  match_type: :regex,
-  match_string: '<a href="http:\/\/www.omniture.com" title="Web Analytics"><img src="\/([^.]+)" height="1" width="1" alt="" \/><\/a>'
-)
-
-# Traklight
-Matcher.create(
-  service: Service.find_by_name("Hubspot"),
-  match_type: :regex,
-  match_string: "https:\/\/js.hscta.net\/cta\/current.js"
-)
-
-# http://businessfactors.com/
-Matcher.create(
-  service: Service.find_by_name("LiveChat"),
-  match_type: :string,
-  match_string: "cdn.livechatinc.com"
-)
-
-Matcher.create(
-  service: Service.find_by_name("SnapEngage"),
-  match_type: :string,
-  match_string: "code.snapengage.com"
-)
-
-Matcher.create(
-  service: Service.find_by_name("Zopim"),
-  match_type: :string,
-  match_string: "window.$zopim"
-)
-
-Matcher.create(
-  service: Service.find_by_name("Google Tag Manager"),
-  match_type: :string,
-  match_string: "googletagmanager.com/gtm.js"
-)
-
-Matcher.create(
-  service: Service.find_by_name("Live Agent"),
-  match_type: :string,
-  match_string: "LiveAgentTracker."
-)
-
-# http://www.emimusicpub.com/
-Matcher.create(
-  service: Service.find_by_name("Marketo"),
-  match_type: :string,
-  match_string: "http://munchkin.marketo.net"
-)
-
-# from Pardot website http://www.pardot.com/faqs/campaigns/tracking-code/
-Matcher.create(
-  service: Service.find_by_name("Pardot"),
-  match_type: :string,
-  match_string: ".pardot.com/pd.js"
-)
-
-# https://www.hightail.com
-Matcher.create(
-  service: Service.find_by_name("Silverpop"),
-  match_type: :string,
-  match_string: "com.silverpop.brandeddomains"
-)
-
-# http://sitescout.com 
-Matcher.create(
-  service: Service.find_by_name("Eloqua"),
-  match_type: :string,
-  match_string: "img.en25.com/i/elqCfg.min.js"
-)
-
-# https://www.kajabiapp.com
-Matcher.create(
-  service: Service.find_by_name("Snapengage"),
-  match_type: :string,
-  match_string: "code.snapengage.com/js"
-)
+# # cbs
+# Matcher.create(
+#   service: Service.find_by_name("Optimizely"),
+#   match_type: :regex,
+#   match_string: "cdn.optimizely.com\/js\/([^.]+).js"
+# )
+#
+# # adroll
+# Matcher.create(
+#   service: Service.find_by_name("Olark"),
+#   match_type: :regex,
+#   match_string: "olark.identify\(([^)]+)\)"
+# )
+#
+#
+# # optimizely, personal capital
+# Matcher.create(
+#   service: Service.find_by_name("Kissmetrics"),
+#   match_type: :regex,
+#   match_string: "i.kissmetrics.com\/i.js"
+# )
+#
+# # NBC
+# Matcher.create(
+#   service: Service.find_by_name("Omniture"),
+#   match_type: :regex,
+#   match_string: '<a href="http:\/\/www.omniture.com" title="Web Analytics"><img src="\/([^.]+)" height="1" width="1" alt="" \/><\/a>'
+# )
+#
+# # Traklight
+# Matcher.create(
+#   service: Service.find_by_name("Hubspot"),
+#   match_type: :regex,
+#   match_string: "https:\/\/js.hscta.net\/cta\/current.js"
+# )
+#
+# # http://businessfactors.com/
+# Matcher.create(
+#   service: Service.find_by_name("LiveChat"),
+#   match_type: :string,
+#   match_string: "cdn.livechatinc.com"
+# )
+#
+# Matcher.create(
+#   service: Service.find_by_name("SnapEngage"),
+#   match_type: :string,
+#   match_string: "code.snapengage.com"
+# )
+#
+# Matcher.create(
+#   service: Service.find_by_name("Zopim"),
+#   match_type: :string,
+#   match_string: "window.$zopim"
+# )
+#
+# Matcher.create(
+#   service: Service.find_by_name("Google Tag Manager"),
+#   match_type: :string,
+#   match_string: "googletagmanager.com/gtm.js"
+# )
+#
+# Matcher.create(
+#   service: Service.find_by_name("Live Agent"),
+#   match_type: :string,
+#   match_string: "LiveAgentTracker."
+# )
+#
+# # http://www.emimusicpub.com/
+# Matcher.create(
+#   service: Service.find_by_name("Marketo"),
+#   match_type: :string,
+#   match_string: "http://munchkin.marketo.net"
+# )
+#
+# # from Pardot website http://www.pardot.com/faqs/campaigns/tracking-code/
+# Matcher.create(
+#   service: Service.find_by_name("Pardot"),
+#   match_type: :string,
+#   match_string: ".pardot.com/pd.js"
+# )
+#
+# # https://www.hightail.com
+# Matcher.create(
+#   service: Service.find_by_name("Silverpop"),
+#   match_type: :string,
+#   match_string: "com.silverpop.brandeddomains"
+# )
+#
+# # http://sitescout.com
+# Matcher.create(
+#   service: Service.find_by_name("Eloqua"),
+#   match_type: :string,
+#   match_string: "img.en25.com/i/elqCfg.min.js"
+# )
+#
+# # https://www.kajabiapp.com
+# Matcher.create(
+#   service: Service.find_by_name("Snapengage"),
+#   match_type: :string,
+#   match_string: "code.snapengage.com/js"
+# )
