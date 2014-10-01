a41ae7c66a745ea547f6353ff1e8db7b53793393
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-01 15:35:52 -0700

    add logs

--- netlog.js
+++ netlog.js
@@ -9,12 +9,12 @@ if (system.args.length === 1) {
     address = system.args[1];
 
     page.onResourceRequested = function (req) {
-      console.log(req.url + "\n");
+      //console.log(req.url + "\n");
       // console.log('requested: ' + JSON.stringify(req, undefined, 4));
     };
 
     page.onResourceReceived = function (req) {
-      console.log(req.url + "\n");
+      //console.log(req.url + "\n");
       // console.log('received: ' + JSON.stringify(req, undefined, 4));
     };
 
@@ -23,9 +23,11 @@ if (system.args.length === 1) {
             console.log('Cannot load the address!');
             phantom.exit(0);
         } else {
+          console.log('Loaded the address')
             window.setTimeout(function () {
+              console.log('Timeout expired. Time to exit.')
                 phantom.exit(0);
-            }, 10000); // ms of how long to wiat
+            }, 10000); // ms of how long to wait
         }
     });
 }
