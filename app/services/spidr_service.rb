402925b1327c1e15f4f7ff81a63b3f2c802eacc1
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-08 16:29:41 -0700

    working

--- app/services/spidr_service.rb
+++ app/services/spidr_service.rb
@@ -14,7 +14,9 @@ class SpidrService
   def run_angellist(url)
     
     i = 0
-    cutoff = 20
+    cutoff = 50
+    
+    urls = []
     
     Spidr.site(url) do |spider|
       spider.every_url do |url|
@@ -25,16 +27,21 @@ class SpidrService
         if company_url.nil?
           puts "could not find company URL"
         else
-          puts "FOUND! URL#{company_url}"
+          urls << company_url
+          puts "FOUND! URL: #{company_url}"
         end
         
         puts ""
         
         i += 1
         
-        break if i == cutoff
+        if i == cutoff
+          return urls
+        end
       end
     end
+    
+    urls
   end
   
   def company_url_from_angellist_url(url)
@@ -43,15 +50,14 @@ class SpidrService
     company_url_classes = data.css(".company_url")
   
     if company_url_classes.blank?
-      puts "couldn't find"
       return nil
     end
   
-    puts "company_url_classes: #{company_url_classes}"
+    #puts "company_url_classes: #{company_url_classes}"
   
     company_url_class = company_url_classes.first
   
-    company_url = company_url_class.child
+    company_url = company_url_class.child.to_s
   end
   
   class << self
