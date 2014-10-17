826a0d8d285b4e821632aa6b940d93c05bdc98eb
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-17 00:24:15 -0700

    remove extra files

--- app/assets/javascripts/status.js
+++ app/assets/javascripts/status.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
-	$('.your-class').slick({
-    // setting-name: setting-value
+	$('.carousel').slick({
     dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
+    slidesToShow: 1
+    // adaptiveHeight: true
 	});
 });
