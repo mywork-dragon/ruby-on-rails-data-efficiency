857e03a5d32f5db88dbd7a71bafcfb913cf08b9a
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-16 21:38:33 -0700

    something shows up

---
+++ app/assets/javascripts/status.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.your-class').slick({
+    // setting-name: setting-value
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+	});
+});
