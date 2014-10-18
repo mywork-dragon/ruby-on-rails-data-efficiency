af8e588338fe1c08abb0ae9365a35d8c9705864c
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-18 13:47:47 -0700

    k

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
   get '/team', to: 'welcome#team'
   post 'contact_us' => 'welcome#contact_us', as: :contact_us
   post 'try_it_out' => 'welcome#try_it_out', as: :try_it_out
-  get 'demo' => 'welcome#demo', as: :demo
-  
-  get 'status' => 'welcome#status'
+  get 'demo' => 'welcome#demo'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
