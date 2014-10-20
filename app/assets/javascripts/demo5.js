283edf7f08e7015ca571b86bc19196ade0bf8165
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-20 13:17:10 -0700

    progress bar works

--- app/assets/javascripts/demo5.js
+++ app/assets/javascripts/demo5.js
@@ -1,20 +1,30 @@
-$(document).ready(function(){
-  console.log("ready")
-  $(function() {
-    var progressbar = $( "#progressbar-2" );
-    $( "#progressbar-2" ).progressbar({
-      value: 30,
-      max:300
-    });
+// $(document).ready(function(){
+//   console.log("ready")
+//   $(function() {
+//     var progressbar = $( "#progressbar-2" );
+//     $( "#progressbar-2" ).progressbar({
+//       value: 30,
+//       max:300
+//     });
+//
+//     function progress() {
+//       var val = progressbar.progressbar( "value" ) || 0;
+//       progressbar.progressbar( "value", val + 1 );
+//       if ( val < 99 ) {
+//        setTimeout( progress, 100 );
+//       }
+//     }
+//     setTimeout( progress, 3000 );
+//   });
+// });
+//
 
-    function progress() {
-      var val = progressbar.progressbar( "value" ) || 0;
-      progressbar.progressbar( "value", val + 1 );
-      if ( val < 99 ) {
-       setTimeout( progress, 100 );
-      }
-    }
-    setTimeout( progress, 3000 );
-  });
+$(document).ready(function(){
+
+  progress(60, $("#progressBar"))
 });
 
+function progress(percent, $element) {
+    var progressBarWidth = percent * $element.width() / 100;
+    $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
+}
