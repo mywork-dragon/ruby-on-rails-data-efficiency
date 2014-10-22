21602ba4b978be8b75f76cc4c1c2a9b83947d760
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-22 15:40:54 -0700

    unix-friendly filename

--- crawl.rb
+++ crawl.rb
@@ -2,6 +2,21 @@ notes = ARGV[0]
 
 puts "notes: #{notes}"
 
-what_to_echo = "hello"
+# what_to_echo = "hello"
+#
+# Dir.mkdir("dummy_folder")
 
-puts `echo #{what_to_echo} > dummy.out`
+notes_filename = friendly_filename(notes)
+puts "notes_filename: #{notes_filename}"
+
+#
+# puts `echo #{what_to_echo} > dummy_folder/#{notes}.out`
+
+BEGIN {
+  def friendly_filename(filename)
+      filename.gsub(/[^\w\s_-]+/, '')
+              .gsub(/(^|\b\s)\s+($|\s?\b)/, '\\1\\2')
+              .gsub(/\s+/, '_')
+  end
+}
+
