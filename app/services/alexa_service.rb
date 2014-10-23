1cc3f6b402da1b319e1351d34d59f2cacb8ed41a
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-22 21:42:08 -0700

    faster CSV

--- app/services/alexa_service.rb
+++ app/services/alexa_service.rb
@@ -6,34 +6,68 @@ class AlexaService
   
   def run(csv_path)
     
-    CSV.foreach(csv_path) do |row|
+    open(csv_path) do |csv|
+      csv.each_line do |line|
+        values = line.split(",")
+        
+        name = values[1]
+        url = "http://" + name
       
-      # use row here...
-      name = row[1]
-      url = "http://" + name
-      
-      company = Company.find_by_name(name)
+        company = Company.find_by_name(name)
 
-      if company.nil?
-        created = Company.create(name: name, website: url, status: :active)
+        if company.nil?
+          created = Company.create(name: name, website: url, status: :active)
 
-        if created
-          puts "Added #{name} (#{url} to DB)"
-        else
-          puts "Error adding #{name} (#{url}) to DB"
+          if created
+            puts "Added #{name} (#{url} to DB)"
+          else
+            puts "Error adding #{name} (#{url}) to DB"
+          end
         end
       end
+    end
+      
+
     end
     
     puts "DONE!"
     
   end
 
+  def test_old(csv_path)
+    
+    CSV.foreach(csv_path) do |row|
+      puts row[0]
+    end
+    
+    puts "DONE!"
+    
+  end
+  
+  def test_new(csv_path)
+    
+    open(csv_path) do |csv|
+      csv.each_line do |line|
+        values = line.split(",")
+        puts values[0]
+      end
+    end
+    
+  end
+  
   
   class << self
   
     def run(csv_path)
       AlexaService.new.run(csv_path)
+    end
+    
+    def test_old(csv_path)
+      AlexaService.new.test_old(csv_path)
+    end
+    
+    def test_new(csv_path)
+      AlexaService.new.test_new(csv_path)
     end
 
   end
