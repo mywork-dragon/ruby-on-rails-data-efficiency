eb6c075aec3abc671091891273d81ad057be177c
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-22 20:38:37 -0700

    cleanup

--- crawl.rb
+++ crawl.rb
@@ -8,12 +8,6 @@ end
 notes = ARGV[0]
 num_processes = ARGV[1].to_i
 
-puts "notes: #{notes}"
-puts "num_processes: #{num_processes}"
-# puts
-
-# what_to_echo = "hello"
-#
 # Dir.mkdir("dummy_folder")
 
 notes_filename = friendly_filename(notes)
@@ -21,15 +15,9 @@ puts "notes_filename: #{notes_filename}"
 
 num_process.times do |process_num|
   
-  command = "nohup bundle exec rake scraper:scrape_all SCRAPE_PERCENTAGE=50 SCRAPE_PAGE_NUMBER=#{process_num} SCRAPE_JOB_NOTES=notes RAILS_ENV=production > /dev/null 2>&1"
+  command = "nohup bundle exec rake scraper:scrape_all SCRAPE_PROCESSES=#{num_processes} SCRAPE_PAGE_NUMBER=#{process_num} SCRAPE_JOB_NOTES=#{notes} RAILS_ENV=production > /dev/null 2>&1"
   
 end
-
-
-# nohup bundle exec rake scraper:scrape_all SCRAPE_PERCENTAGE=50 SCRAPE_PAGE_NUMBER=0 SCRAPE_JOB_NOTES=notes RAILS_ENV=production > /dev/null 2>&1
-
-#
-# puts `echo #{what_to_echo} > dummy_folder/#{notes}.out`
 
 BEGIN {
   
