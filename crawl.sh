f709f40eff5910febc63f252bfad79d74e54c7d8
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-22 20:54:58 -0700

    YES

--- crawl.sh
+++
@@ -1,5 +0,0 @@
-notes="unique notes"
-
-
-
-notes="unique notes" &&  nohup bundle exec rake scraper:scrape_all SCRAPE_PERCENTAGE=50 SCRAPE_PAGE_NUMBER=0 SCRAPE_JOB_NOTES=notes RAILS_ENV=production > /dev/null 2>&1 & && nohup bundle exec rake scraper:scrape_all SCRAPE_PERCENTAGE=50 SCRAPE_PAGE_NUMBER=1 SCRAPE_JOB_NOTES=notes RAILS_ENV=production > /dev/null 2>&1 &
---
+++ scrape.rb
@@ -0,0 +1,39 @@
+if(ARGV.length != 2)
+  puts "You need 2 arguments."
+  puts "1st argument: Notes (must be unique)"
+  puts "2nd argument: Max Number of Processes"
+  abort
+end
+
+notes = ARGV[0]
+num_processes = ARGV[1].to_i
+
+directory_name = friendly_filename(notes)
+
+directory_path = "/home/ubuntu/scrape_logs/#{directory_name}"
+
+# Dir.mkdir(directory_path)
+
+num_processes.times do |process_num|
+  
+  log_path = "#{directory_path}/#{process_num}.log"
+  command = "nohup bundle exec rake scraper:scrape_all SCRAPE_PROCESSES=#{num_processes} SCRAPE_PAGE_NUMBER=#{process_num} SCRAPE_JOB_NOTES=\"#{notes}\" RAILS_ENV=production > #{log_path} &"
+  
+  puts "log_path: #{log_path}"
+  puts "command: #{command}"
+  puts ""
+  
+  # `#{command}`
+  
+end
+
+BEGIN {
+  
+  def friendly_filename(filename)
+      filename.gsub(/[^\w\s_-]+/, '')
+              .gsub(/(^|\b\s)\s+($|\s?\b)/, '\\1\\2')
+              .gsub(/\s+/, '_')
+  end
+
+}
+
