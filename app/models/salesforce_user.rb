9d129f9b83d7b4f47ba8aaf5b7d0f323efd580ff
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-30 14:54:25 -0700

    can sign in with SF!

--- app/models/salesforce_user.rb
+++ app/models/salesforce_user.rb
@@ -1,7 +1,8 @@
 class SalesforceUser < ActiveRecord::Base
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      puts "inside where"
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
