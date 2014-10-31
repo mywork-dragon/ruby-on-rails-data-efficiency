63bca197623f52d2271c8f350cae19770e53294d
mywork-dragon <dragonblueyounger@gmail.com>
2014-10-30 22:30:46 -0700

    works from command line

--- app/services/restforce_service.rb
+++ app/services/restforce_service.rb
@@ -1,4 +1,20 @@
 class RestforceService
 
+  def client
+    client = Restforce.new :username => 'jason@mightysignal.com',
+      :password       => 'knKnsjnsansaf23764KSJANFssas',
+      :security_token => 'vZyFBHo9FHpqRWjDUhsIrjzdM',
+      :client_id      => '3MVG9fMtCkV6eLhfvfGZ559QaTiFUS_ZTpnvTn5pfL9_NAInaNgoW0AcvlslIJ1Xd6tOX7JfkJoo6bB55flRl',
+      :client_secret  => '3173051852013251576'
+  end
+  
+  class << self
+  
+    def client
+      RestforceService.new.client
+    end
+    
+  end
+
 
 end
