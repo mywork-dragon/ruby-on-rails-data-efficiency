2734a559b5d7411966513675e40580faec18bb29
mywork-dragon <dragonblueyounger@gmail.com>
2014-11-23 21:19:25 -0800

    gemfile

---
+++ app/services/restforce_service.rb
@@ -0,0 +1,30 @@
+class RestforceService
+
+  def client
+    client = Restforce.new :username => 'jason@mightysignal.com',
+      :password       => 'knKnsjnsansaf23764KSJANFssas',
+      :security_token => 'vZyFBHo9FHpqRWjDUhsIrjzdM',
+      :client_id      => '3MVG9fMtCkV6eLhfvfGZ559QaTiFUS_ZTpnvTn5pfL9_NAInaNgoW0AcvlslIJ1Xd6tOX7JfkJoo6bB55flRl',
+      :client_secret  => '3173051852013251576'
+  end
+
+  class << self
+
+    def client
+      RestforceService.new.client
+    end
+  
+    def run_test
+      client = self.client
+    
+      #@blah = client.create('Merchandise__c', Name: 'Dumdum', Price__c: '123', Quantity__c: '44')
+
+      out = client.query("select Id, Name from Merchandise__c").first.Name
+  
+      puts "query result: #{out}"
+    
+    end
+  
+  end
+  
+end
