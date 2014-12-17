49f3f98a249b3e28ccfc2c6fa01feffc41f45e3f
mywork-dragon <dragonblueyounger@gmail.com>
2014-12-16 17:42:45 -0800

    OAUTH SHIT WORKS LETS GO

--- app/models/salesforce_user.rb
+++ app/models/salesforce_user.rb
@@ -1,6 +1,8 @@
 class SalesforceUser < ActiveRecord::Base
 
   def self.from_omniauth(auth)
+    puts "auth: #{auth}"
+    
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
