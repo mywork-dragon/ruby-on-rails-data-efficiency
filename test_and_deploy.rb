f13dda609c7cfebf2fb69269ad24cedc59ed9b21
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-02 12:58:41 -0800

    fix

---
+++ test_and_deploy.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+test_output = %x(rake test)
+
+last_line = test_output.line.last
+last_line.split(", ")
+if !last_line.include?('0 failures')
+  puts "Tests failed."
+  abort
+end
+%x(cap production deploy)
