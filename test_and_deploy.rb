695833897b4d73a62092c07bbe369e53daedba17
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-02 12:53:54 -0800

    fix split

--- test_and_deploy.rb
+++ test_and_deploy.rb
@@ -1,7 +1,7 @@
 test_output = %x(rake_test)
 
 last_line = test_output.line.last
-last_line.split(",")
+last_line.split(", ")
 abort if !last_line.include?('0 failures')
 
 %x(cap production deploy)
