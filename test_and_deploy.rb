63e0353019f8265c73ed28d8348e182522c5bc41
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-02 12:54:51 -0800

    should fail

--- test_and_deploy.rb
+++ test_and_deploy.rb
@@ -2,6 +2,8 @@ test_output = %x(rake_test)
 
 last_line = test_output.line.last
 last_line.split(", ")
-abort if !last_line.include?('0 failures')
-
+if !last_line.include?('0 failures')
+  puts "Tests failed."
+  abort
+end
 %x(cap production deploy)
