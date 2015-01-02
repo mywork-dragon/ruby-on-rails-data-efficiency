9f4cc0c9edeb280d758564f1aaa1ac25a570930d
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-02 13:18:10 -0800

    new script

--- test_and_deploy.rb
+++ test_and_deploy.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/env ruby
 
-test_output = %x(rake test)
-
-last_line = test_output.lines.last
-
-puts test_output
-
-#puts ""
-#puts "last_line: #{last_line}"
+test_cmd = 'rake test'
+ 
+last_line = nil
+IO.popen(test_cmd).each do |line|
+  puts line
+  last_line = line
+end.close # Without close, you won't be able to access $?
+ 
+#puts "The command's exit code was: #{$?.exitstatus}"
 
 last_line.split(", ")
 if !last_line.include?('0 failures')
   abort
 end
+
 system('cap production deploy')
