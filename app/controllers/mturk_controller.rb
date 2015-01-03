2917defb3957c2191915703bbe2fabdf0365c55f
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-02 17:24:39 -0800

    fix

--- app/controllers/mturk_controller.rb
+++ app/controllers/mturk_controller.rb
@@ -3,12 +3,12 @@ class MturkController < ApplicationController
   def gochime
     installations = Installation.where(scrape_job_id: 43, service_id: 141).limit(10)
     
-    @company_names = []
+    @companies = []
     
     installations.each do |i|
       company_name = i.company.name
       
-      @company_names << company_name
+      @companies << company_name
       
     end
   end
