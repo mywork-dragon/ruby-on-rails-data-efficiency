f3a5d17c9e01b4168c83f537c3eead0d6ac4ec0e
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-06 22:32:36 -0800

    add debug

--- app/workers/ScraperWorker.rb
+++ app/workers/ScraperWorker.rb
@@ -3,6 +3,9 @@ class ScraperWorker
   include MyIp
 
   def perform(name, count)
+    log = Logger.new '/home/deploy/varys_current/logs/resque.log'
+    log.debug "before create"
     Company.create!(name: "dummy5432112345.edu", website: "http://dummy5432112345.edu", status: :active)
+    log.debug "after create"
   end
 end
