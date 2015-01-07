c6ed500a2eacb68bd445c95ae767c45b53e60638
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-06 22:42:30 -0800

    remove logging

--- app/workers/ScraperWorker.rb
+++ app/workers/ScraperWorker.rb
@@ -2,10 +2,8 @@ class ScraperWorker
   include Sidekiq::Worker
   include MyIp
 
+  # ScraperWorker.perform_async('bob', 5)
   def perform(name, count)
-    log = Logger.new '/home/deploy/varys_current/logs/resque.log'
-    log.debug "before create"
     Company.create!(name: "dummy5432112345.edu", website: "http://dummy5432112345.edu", status: :active)
-    log.debug "after create"
   end
 end
