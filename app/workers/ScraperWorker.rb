7b4e8d1a29d9a5926abf9dccc1d4baf61e58f9a6
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-07 15:54:50 -0800

    uncomment

--- app/workers/ScraperWorker.rb
+++ app/workers/ScraperWorker.rb
@@ -1,10 +1,10 @@
-# class ScraperWorker
-#   include Sidekiq::Worker
-#   include MyIp
-#
-#   # ScraperWorker.perform_async('bob', 5)
-#   #
-#   def perform(name, count)
-#     Company.create!(name: "dummy5432112345.edu", website: "http://dummy5432112345.edu", status: :active)
-#   end
-# end
+class ScraperWorker
+  include Sidekiq::Worker
+  include MyIp
+
+  # ScraperWorker.perform_async('bob', 5)
+  #
+  def perform(name, count)
+    Company.create!(name: "dummy5432112345.edu", website: "http://dummy5432112345.edu", status: :active)
+  end
+end
