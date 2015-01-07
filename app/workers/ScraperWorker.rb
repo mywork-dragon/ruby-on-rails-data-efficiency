aaeace0020605b08bc3f87d6d01b87971771ce1b
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-07 00:06:37 -0800

    test deploy

--- app/workers/ScraperWorker.rb
+++ app/workers/ScraperWorker.rb
@@ -3,6 +3,7 @@ class ScraperWorker
   include MyIp
 
   # ScraperWorker.perform_async('bob', 5)
+  #
   def perform(name, count)
     Company.create!(name: "dummy5432112345.edu", website: "http://dummy5432112345.edu", status: :active)
   end
