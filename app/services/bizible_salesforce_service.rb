c2a0a4df3cbbaf3bdc7223e7ab852ece3a40c12b
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-12 11:00:38 -0800

    syntax

--- app/services/bizible_salesforce_service.rb
+++ app/services/bizible_salesforce_service.rb
@@ -167,7 +167,7 @@ class BizibleSalesforceService
     
     if @demo
       salesforce_api_name_service_name_hash.each do |key, value|
-        salesforce_api_name_service_name_hash["MightySignalBiz__" + key] = hash.delete(key)
+        salesforce_api_name_service_name_hash["MightySignalBiz__" + key] = salesforce_api_name_service_name_hash.delete(key)
       end
       
       last_updated_api_name = "MightySignalBiz__MightySignal_Last_Updated__c"
