73269d95ed94bb622a77f3676a796cdae60d2127
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-21 23:14:13 -0800

    new code for Triggerman

--- app/services/triggermail_salesforce_service.rb
+++ app/services/triggermail_salesforce_service.rb
@@ -1,5 +1,5 @@
 # test
-class TriggermailDemoSalesforceService
+class TriggermailSalesforceService
 
   include SalesforceService
 
@@ -20,6 +20,14 @@ class TriggermailDemoSalesforceService
     @api_hash['Tag Management'] = {lead: 'Tag_Manager__c', opp: 'Tag_Manager__c'}
     @api_hash['General Marketing Tech'] = {lead: 'General_Marketing_Tech__c', opp: 'General_Marketing_Tech__c'}
     
+    @api_hash.each do |key, value|
+      value.each do |key2, value2|
+        @api_hash[key][key2] = "MightySignalTM__" + value2
+      end
+    end
+
+    puts "@api_hash: #{@api_hash}"
+
     sf_object_type = options[:object_type]
     
     @lead_services_hash = Hash.new
@@ -41,6 +49,14 @@ class TriggermailDemoSalesforceService
       :security_token => 'myKlAxydIqstRnXJlCwoU46c',
       :client_id      => '3MVG9fMtCkV6eLheWeuPjSg0j18ozUv5a1UaTfM9Fm5zoj3xJuPtrfum0nWZlK6LE62.jQLrR6Y_fnxJBS.7i',
       :client_secret  => '8577635590104229957'
+      
+    # su = OauthUser.find_by_email("max@triggermail.com")
+    #
+    # @client = Restforce.new :oauth_token => su.oauth_token,
+    #   :refresh_token => su.refresh_token,
+    #   :instance_url  => su.instance_url,
+    #   :client_id     => '3MVG9fMtCkV6eLheWeuPjSg0j18ozUv5a1UaTfM9Fm5zoj3xJuPtrfum0nWZlK6LE62.jQLrR6Y_fnxJBS.7i',  #need to fix
+    #   :client_secret => '8577635590104229957'
   end
 
   def hydrate_lead(options)
@@ -54,7 +70,18 @@ class TriggermailDemoSalesforceService
     hydrate_object(:lead, {id: options[:id], website: website, name: name})
   end
   
-  def hydrate_opp(options)
+  def hydrate_contact(options)
+    email = options[:email]
+    
+    name = email.split("@").last
+    website = "http://" + name
+
+    return if (website =~ URI::regexp).nil?
+    
+    hydrate_object(:contact, {id: options[:id], website: website, name: name})
+  end
+  
+  def hydrate_account(options)
     website = options[:website]
     
     name = UrlManipulator.url_with_base_only(website)
@@ -63,7 +90,7 @@ class TriggermailDemoSalesforceService
     
     return if (website =~ URI::regexp).nil?
     
-    hydrate_object(:opp, {id: options[:id], website: website, name: name})
+    hydrate_object(:account, {id: options[:id], website: website, name: name})
   end
 
   def hydrate_object(object_type, options={})
@@ -107,14 +134,14 @@ class TriggermailDemoSalesforceService
     
     
     
-    object_params = {Id: id, MightySignal_Last_Updated__c: current_date_time_sf_format}.merge(salesforce_api_name_service_name_hash)
-    client.update!(object_name, object_params)
+    object_params = {Id: id, MightySignalTM__MightySignal_Last_Updated__c: current_date_time_sf_format}.merge(salesforce_api_name_service_name_hash)
+    # client.update!(object_name, object_params)
     
   end
 
   def salesforce_api_name_service_name_hash(object_type, found_service_names)
 
-    ret = {"Marketing_Automation__c" => [], "Tag_Manager__c" => [], "General_Marketing_Tech__c" => [], "Other_Signals__c" => []}
+    ret = {"MightySignalTM__Marketing_Automation__c" => [], "MightySignalTM__Tag_Manager__c" => [], "MightySignalTM__General_Marketing_Tech__c" => [], "MightySignalTM__Other_Signals__c" => []}
 
     not_other = []
 
@@ -137,7 +164,7 @@ class TriggermailDemoSalesforceService
 
     end
     
-    ret["Other_Signals__c"] << found_service_names - not_other
+    ret["MightySignalTM__Other_Signals__c"] << found_service_names - not_other
     
     ret.each do |api_name, service_names|
       ret[api_name] = service_names.join(", ")
@@ -153,8 +180,12 @@ class TriggermailDemoSalesforceService
       self.new.hydrate_lead(options)
     end
     
-    def hydrate_opp(options={})      
-      self.new.hydrate_opp(options)
+    def hydrate_contact(options={})      
+      self.new.hydrate_contact(options)
+    end
+    
+    def hydrate_account(options={})      
+      self.new.hydrate_account(options)
     end
 
 
