d4c2b95a6f1eae65174fd7d7297038fbd50979c3
mywork-dragon <dragonblueyounger@gmail.com>
2015-01-30 16:35:46 -0800

    new pw

--- app/services/bizible_salesforce_service.rb
+++ app/services/bizible_salesforce_service.rb
@@ -83,7 +83,7 @@ class BizibleSalesforceService
     return @client if @client
     
     # @client = Restforce.new :username => 'jason@mightysignal.com',
-    #   :password       => 'knKnsjnsansaf23764KSJANFssas',
+    #   :password       => 'kajsbkjbPSLFPSkaj22424bb24b',
     #   :security_token => 'vZyFBHo9FHpqRWjDUhsIrjzdM',
     #   :client_id      => '3MVG9fMtCkV6eLhfvfGZ559QaTiFUS_ZTpnvTn5pfL9_NAInaNgoW0AcvlslIJ1Xd6tOX7JfkJoo6bB55flRl',
     #   :client_secret  => '3173051852013251576'
