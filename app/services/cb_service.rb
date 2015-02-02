8f11fc1a47df6ecf4f26f00346042bfe8d2f6fe1
mywork-dragon <dragonblueyounger@gmail.com>
2015-02-02 14:26:55 -0800

    can find company

---
+++ app/services/cb_service.rb
@@ -0,0 +1,33 @@
+class CbService
+
+  SITE = 'crunchbase.com/organization'
+  NUMBER_OF_RESULTS = 30
+  
+  
+
+  def initialize
+    
+  end
+  
+  def cb_url(company_name)
+
+    query = company_name
+    query_url_safe = CGI::escape(query)
+
+    url = "http://www.google.com/search?num=#{NUMBER_OF_RESULTS}&q=#{query_url_safe}+site:#{SITE}"
+    
+    puts "Google URL: #{url}"
+    
+    page = open(url)
+
+    html = Nokogiri::HTML page
+    
+    html.search("cite").each do |cite|
+      url = cite.inner_text
+
+      puts url
+    end
+  end 
+
+
+end
