b4160662e9358ff7d873a5b3b3d35a8c6ef4c7cd
mywork-dragon <dragonblueyounger@gmail.com>
2015-02-02 15:05:42 -0800

    start to try AL

--- app/services/cb_service.rb
+++ app/services/cb_service.rb
@@ -9,29 +9,64 @@ class CbService
     
   end
   
-  def cb_url(company_name)
 
-    query = company_name
-    query_url_safe = CGI::escape(query)
-
-    url = "http://www.google.com/search?num=#{NUMBER_OF_RESULTS}&q=#{query_url_safe}+site:#{SITE}"
+  class << self
     
-    puts "Google URL: #{url}"
+    def cb_url(company_name)
+
+      query = company_name
+      query_url_safe = CGI::escape(query)
+
+      url = "http://www.google.com/search?num=#{NUMBER_OF_RESULTS}&q=#{query_url_safe}+site:#{SITE}"
     
-    page = open(url)
-
-    html = Nokogiri::HTML page
+      puts "Google URL: #{url}"
     
-    html.search("cite").each do |cite|
-      url = cite.inner_text
+      page = open(url)
 
-      org_regex = /crunchbase.com\/organization\/[^\/]*\z/
+      html = Nokogiri::HTML(page)
+    
+      html.search("cite").each do |cite|
+        url = cite.inner_text
 
-      if(url.match(org_regex))
-        puts url
+        org_regex = /crunchbase.com\/organization\/[^\/]*\z/
+
+        if(url.match(org_regex))
+          puts url
+        end
       end
+    end 
+    
+    def cb_funding(cb_url)
+      page = open(cb_url)
+      html = Nokogiri::HTML(page)
+      
+      puts html
+      
+      funding_classes = html.css(".funding_amount")
+      
+      funding_class = funding_classes.first
+      
+      puts "funding_class: #{funding_class}"
+      
+      funding = funding_class.child
+      
+      puts funding
     end
-  end 
-
+    
+    
+    def test
+      companies = ["Instagram"]
+      
+      cb_urls = []
+      
+      companies.each do |company|
+        cb_urls << self.cb_url(company)
+      end
+      
+      
+      
+    end
+    
+  end
 
 end
