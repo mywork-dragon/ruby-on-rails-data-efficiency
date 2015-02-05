fad798fdc56f3e6ba034fd16d2f2590d88e10fd5
mywork-dragon <dragonblueyounger@gmail.com>
2015-02-04 16:12:42 -0800

    company_url works

--- app/services/app_store_service.rb
+++ app/services/app_store_service.rb
@@ -2,12 +2,22 @@ class AppStoreService
   
   class << self
   
-    def app_website(app_store_url)
+    def app_store_attributes(app_store_url)
+      ret = {}
+      
       url_cache = "http://webcache.googleusercontent.com/search?q=cache:#{app_store_url}"
       
       page = open(url_cache)
       html = Nokogiri::HTML(page)
       
+      ret[:company_url] = company_url(html)
+      
+      ret
+    end
+    
+    private
+    
+    def company_url(html)
       url = html.css(".app-links").children.first['href']
       
       UrlManipulator.url_with_http_only(url)
