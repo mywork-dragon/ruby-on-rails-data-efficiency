612f129199afaac40c701adcbc080479cf07fc42
mywork-dragon <dragonblueyounger@gmail.com>
2015-02-09 16:52:11 -0800

    sort

--- jobs/apptentive_fore_see_job.rb
+++ jobs/apptentive_fore_see_job.rb
@@ -6,14 +6,33 @@ class ApptentiveForeSeeJob
     
     srs = ScrapedResult.includes(:installations).where(scrape_job_id: 45, installations: {service_id: 226})
 
+    results = []
+    
+    srs.each do |sr|
+      company_name = sr.company.name
+      
+      result[:company] = company_name
+      
+      ranks = PageRankr.ranks(company_name)
+      result[:alexa] = ranks[:alexa_global]
+      result[:google] = ranks[:google]
+      
+      results << result
+      
+      puts result
+    end
+      
+    results.sort_by!(|result| result[:alexa])
       
     filename = "ForeSee.csv"
     
     CSV.open(directory_path + '/' + filename, "w+") do |csv|
-      srs.each do |sr|
-        company_name = sr.company.name
-        csv << [company_name]
-        puts company_name
+      csv << ['Company', 'Global Alexa Ranking', 'Google Page Rank']
+      
+      results.each do |result|
+        line = result[:company], result[:alexa], ranks[:google]]
+        csv << line
+        puts line
       end
     end
     
