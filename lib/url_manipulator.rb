9754e1932007871898a19d2bb6bab184bc63a663
mywork-dragon <dragonblueyounger@gmail.com>
2015-02-27 14:18:12 -0800

    change biz job

--- lib/url_manipulator.rb
+++ lib/url_manipulator.rb
@@ -5,21 +5,12 @@ class UrlManipulator
   class << self
     
     def url_with_http_only(url)
-      "http://" + self.url_with_base_only(url)
+      "http://" + url_with_base_only(url)
     end
     
     def url_with_base_only(url)
       regex = /^(http[s]*:\/\/)*(www.)*/
-
-      if url.match(regex)
-        name = url.gsub(regex, "")
-        url_with_http = url
-      else
-        name = url
-      end
-
-      name
-      
+      url.match(regex) ? url.gsub(regex, "") : url
     end
   
   end
