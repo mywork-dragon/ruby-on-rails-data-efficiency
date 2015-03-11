ca86a959d02f741d869e65e71c9c33d9eaa6637c
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-11 13:33:56 -0700

    new pw

--- app/services/mighty_signal_salesforce_service.rb
+++ app/services/mighty_signal_salesforce_service.rb
@@ -10,7 +10,7 @@ class MightySignalSalesforceService
     return @client if @client
     
     @client = Restforce.new :username => 'jasonlew@mightysignal.com',
-      :password       => 'saAHSaslfnKAJSNFKJ2147682647KJSAHKFJH128947',
+      :password       => 'asfklnSSLFKN1248LSKFN28asfknasf',
       :security_token => 'PN1W6fcEY2Pa7DI44l2Ubc5b',
       :client_id      => '3MVG9fMtCkV6eLhcIlf3UM3DhI0qHjleFYx1eiGwILdwEf8djU26Vnqjd3mu1Kxs0Z258R99eC0sfRJHG548g',
       :client_secret  => '6384884061761347258'
