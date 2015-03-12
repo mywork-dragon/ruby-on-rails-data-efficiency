47172b95b21435206bafa97d288a570c4d6584f3
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-12 00:58:49 -0700

    funding works

--- app/services/ios_app_service.rb
+++ app/services/ios_app_service.rb
@@ -2,19 +2,18 @@ class IosAppService
   
   class << self
   
-    def attributes(app_store_url)
+    def attributes(app_store_id)
       
-      attributes = AppStoreService.app_store_attributes(app_store_url)
+      attributes = AppStoreService.attributes(app_store_id)
       
       downloads_attributes = DownloadsService.downloads_attributes(attributes[:title])
       attributes.merge!(downloads_attributes)
       
       seller_url = attributes[:seller_url]
-      funding = CbService.cb_funding(seller_url) if seller_url
+      seller_domain = UrlManipulator.url_with_base_only(seller_url)
+      funding = CbService.attributes(seller_domain)
       
       attributes.merge!({funding: funding})
-      
-      
     end
     
     
