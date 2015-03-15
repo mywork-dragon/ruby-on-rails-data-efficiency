92d44e4590f2dd03c6fa619eef91f6485a7b54f2
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-14 20:43:18 -0700

    don't sidekiq on production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
   
-  Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
-  end
+  # Sidekiq.configure_server do |config|
+  #   config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
+  # end
+  #
+  # Sidekiq.configure_client do |config|
+  #   config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
+  # end
 
 elsif Rails.env.development?
   Sidekiq.configure_server do |config|
