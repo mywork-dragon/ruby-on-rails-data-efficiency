fd44c745bb4b7e80174c524e0ffd75c31d7a7074
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-14 20:39:52 -0700

    add reids

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   
   Sidekiq.configure_server do |config|
-    config.redis = { url: 'varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
+    config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
+    config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
   end
 
 elsif Rails.env.development?
