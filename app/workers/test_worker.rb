b9c1c54919e35e2c39a8721598b72327a4829179
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-16 23:18:44 -0700

    test li

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -4,4 +4,9 @@ class TestWorker
   def perform(string)
     SidekiqTester.create!(test_string: string, ip: MyIp.ip)
   end
+  
+  def test_li(string)
+    li "test_li, #{string}"
+    SidekiqTester.create!(test_string: string, ip: MyIp.ip)
+  end
 end
