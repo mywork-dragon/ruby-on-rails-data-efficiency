e9cba53fb36a13a9fe318fe859eed49d13a11547
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-16 23:23:50 -0700

    change perform

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -2,11 +2,8 @@ class TestWorker
   include Sidekiq::Worker
 
   def perform(string)
-    SidekiqTester.create!(test_string: string, ip: MyIp.ip)
-  end
-  
-  def test_li(string)
     li "test_li, #{string}"
     SidekiqTester.create!(test_string: string, ip: MyIp.ip)
   end
+  
 end
