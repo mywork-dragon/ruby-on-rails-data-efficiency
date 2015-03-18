657268bb0f0295c4162b0d4439112c263d0f8458
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-17 21:06:34 -0700

    run in background

--- jobs/ad_heroku_transfer.rb
+++ jobs/ad_heroku_transfer.rb
@@ -1,51 +1,62 @@
 class AdHerokuTransfer
 
-  def add_workers(json_file)
-    json = JSON.parse(IO.read(json_file))
+  class << self
+
+    def add_workers(json_file)
+      json = JSON.parse(IO.read(json_file))
     
-    json.each do |worker_hash|
-      aws_identifier = worker_hash['aws_worker_id']
-      li "aws_identifier: #{aws_identifier}"
+      json.each do |worker_hash|
+        aws_identifier = worker_hash['aws_worker_id']
+        li "aws_identifier: #{aws_identifier}"
       
-      worker = MTurkWorker.find_by_aws_identifier(aws_identifier)
+        worker = MTurkWorker.find_by_aws_identifier(aws_identifier)
 
-      li "worker: #{worker}"
+        li "worker: #{worker}"
       
-      if worker
-        li "Worker #{aws_identifier} already in DB"
-      else
-        worker = MTurkWorker.create!(
-          aws_identifier: aws_identifier,
-          age: worker_hash['age'].to_i,
-          gender: worker_hash['gender'],
-          city: worker_hash['city'],
-          state: worker_hash['state'],
-          country: worker_hash['country'],
-          iphone: worker_hash['iphone'],
-          ios_version: worker_hash['ios_version'],
-          heroku_identifier: worker_hash['heroku_id'].to_i
-        )
-        li "Created worker #{worker}"
+        if worker
+          li "Worker #{aws_identifier} already in DB"
+        else
+          worker = MTurkWorker.create!(
+            aws_identifier: aws_identifier,
+            age: worker_hash['age'].to_i,
+            gender: worker_hash['gender'],
+            city: worker_hash['city'],
+            state: worker_hash['state'],
+            country: worker_hash['country'],
+            iphone: worker_hash['iphone'],
+            ios_version: worker_hash['ios_version'],
+            heroku_identifier: worker_hash['heroku_id'].to_i
+          )
+          li "Created worker #{worker}"
 
+        end
+      
+        li ""
+      
       end
+    
+    end
+
+    def add_ads(json_file)
+      json = JSON.parse(IO.read(json_file))
+    
+      json.each do |ad_hash|
+        
+        delay(queue: 'critical').perform(ad_hash)
       
-      li ""
-      
+      end
+    
+      nil
     end
     
-  end
-
-  def add_ads(json_file)
-    json = JSON.parse(IO.read(json_file))
-    
-    json.each do |ad_hash|
+    def perform(ad_hash)
       aws_assignment_identifier = ad_hash['aws_assignment_id']
       ios_app_app_identifier = ad_hash['app_store_id']
-      
+    
       aa = FbAdAppearance.where(aws_assignment_identifier: aws_assignment_identifier, ios_app: IosApp.find_by_app_identifier(ios_app_app_identifier)).first
-      
+    
       puts "aa: #{aa}"
-      
+    
       if aa
         li "FbAdAppearance #{aa} already in DB"
       else
@@ -54,40 +65,26 @@ class AdHerokuTransfer
           hit_identifier: ad_hash['hit_id'],
           heroku_identifier: ad_hash['heroku_id'] 
         )
-        
-        
+      
+      
         aws_worker_identifier = ad_hash['aws_worker_id']
         m_turk_worker = MTurkWorker.find_by_aws_identifier(aws_worker_identifier)
         aa.m_turk_worker = m_turk_worker
-        
+      
         ios_app = IosApp.find_by_app_identifier(ios_app_app_identifier)
-        
+      
         if ios_app.nil?
-          
+        
           ios_app = IosApp.create!(app_identifier: ios_app_app_identifier) 
           app = App.create
           ios_app.app = app
           ios_app.save!
         end
-        
+      
         aa.ios_app = ios_app
-        
+      
         aa.save!
       end
-      
-    end
-    
-    nil
-  end
-
-  class << self
-    
-    def add_workers(json_file)
-      self.new.add_workers(json_file)
-    end
-    
-    def add_ads(json_file)
-      self.new.add_ads(json_file)
     end
     
     # run(ads_json_file: path_to_ads_json, workers_json_file: path_to_workers_json)
@@ -96,11 +93,11 @@ class AdHerokuTransfer
       if options[:ads_json_file].nil? || options[:workers_json_file].nil?
         raise "Need keys :ads_json_file and :workers_json_file"
       end
-      
+    
       options.each do |key, value|
         raise "File #{value} does not exist" if !File.exist?(value)
       end
-      
+    
       add_workers(options[:workers_json_file])
       add_ads(options[:ads_json_file])
     end
