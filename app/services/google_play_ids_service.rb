97e1009b5f803c2b7886c349f841b0ea352939c2
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-23 20:39:54 -0700

    Code Factored to Sidekick Worker

--- app/services/google_play_ids_service.rb
+++ app/services/google_play_ids_service.rb
@@ -26,8 +26,6 @@ class GooglePlayIdsService
     # scrapes Google Play store
     def scrape_app_store
 
-      # app_ids = Set.new # @patrick Ruby style usually uses underscore naming conventions for local vars :)
-
       # url string param for each category of app
       # @patrick You can use this syntax when you have a bunch a string literals with no spaces
       app_categories = %w(
@@ -67,70 +65,12 @@ class GooglePlayIdsService
 
       # adds alphabet nodes to array
       app_categories += ('A'..'Z').to_a
-      app_categories.select!{ |l| l == 'Z' } # for debug, only run letter A for now
+      app_categories.select!{ |l| l == 'A' || l == 'B' } # for debug, only run letter A for now
 
       # for each category of app
       app_categories.each do |app_category|
 
-        # logger.info "SCRAPING    CATEGORY: " + app_category + "..."
-
-        puts "SCRAPING    CATEGORY: " + app_category + "..."
-
-        # response    = server.Post (String.Format (https://play.google.com/store/search?q={0}&c=apps, searchField), ipf=1&xhr=1);
-
-        # Open initial page
-        apps_page_dom = open_url("https://play.google.com/store/search?q=" + app_category + "&c=apps&ipf=1&xhr=1")
-
-        if apps_page_dom != nil
-
-          pag_token = ""
-
-          while pag_token != nil
-
-            # wrapper for #selectedcontent columns
-            app_cards = apps_page_dom.css("div.card-list > div.card")
-
-            # iterate over each of the result wrapper elements
-            app_cards.each do |app_card|
-
-              # finds the href link inside the <a> and strips out the id
-              # Before: "/store/apps/details?id=com.hottrix.ibeerfree"
-              # After: "com.hottrix.ibeerfree"
-              #app_id = app_card.css("a.card-click-target")['href'].split('id=').last
-
-              app_id = app_card.css("a.card-click-target").first['href'].split('id=').last
-
-              puts "\n" + app_id
-
-            end
-
-            # Regex parses HTML document, finding the pagTok (page token)
-            pag_token_array = apps_page_dom.to_html.match(/GAEi+.+\:S\:.{11}\\42/)
-
-            puts pag_token_array.inspect
-
-            puts pag_token
-
-            if pag_token_array != nil
-              # Cleans up pagTok, returning ready-for-use string
-              pag_token = pag_token_array[0].gsub(':S:','%3AS%3A').gsub("\\42",'').gsub("\\u003d",'').gsub("\\", "=").split(',').last
-
-              puts pag_token
-
-              page_url = "https://play.google.com/store/search?q=" + app_category + "&c=apps&start=0&num=0&numChildren=0&pagTok=" + pag_token + "&ipf=1&xhr=1"
-
-              puts page_url
-
-              # Open initial page
-              apps_page_dom = open_url(page_url)
-
-            else
-              pag_token = nil
-            end
-
-          end
-
-        end
+        AppStoreIdsServiceWorker.perform_async(app_category)
 
       end
 
---
+++ app/workers/play_store_ids_service_worker.rb
@@ -0,0 +1,86 @@
+class PlayStoreIdsServiceWorker
+  include Sidekiq::Worker
+
+  def perform(app_category)
+
+    app_ids = Set.new # @patrick Ruby style usually uses underscore naming conventions for local vars :)
+
+    logger.info "SCRAPING    CATEGORY: " + app_category + "..."
+
+    # Open initial page
+    apps_page_dom = open_url("https://play.google.com/store/search?q=" + app_category + "&c=apps&ipf=1&xhr=1")
+
+    if apps_page_dom != nil
+
+      pag_token = ""
+
+      while pag_token != nil
+
+        # wrapper for #selectedcontent columns
+        app_cards = apps_page_dom.css("div.card-list > div.card")
+
+        # iterate over each of the result wrapper elements
+        app_cards.each do |app_card|
+
+          # finds the href link inside the <a> and strips out the id
+          # Before: "/store/apps/details?id=com.hottrix.ibeerfree"
+          # After: "com.hottrix.ibeerfree"
+          #app_id = app_card.css("a.card-click-target")['href'].split('id=').last
+
+          app_ids.add(app_card.css("a.card-click-target").first['href'].split('id=').last)
+
+        end
+
+        # Regex parses HTML document, finding the pagTok (page token)
+        pag_token_array = apps_page_dom.to_html.match(/GAEi+.+\:S\:.{11}\\42/)
+
+        if pag_token_array != nil
+          # Cleans up pagTok, returning ready-for-use string
+          pag_token = pag_token_array[0].gsub(':S:','%3AS%3A').gsub("\\42",'').gsub("\\u003d",'').gsub("\\", "=").split(',').last
+
+          page_url = "https://play.google.com/store/search?q=" + app_category + "&c=apps&start=0&num=0&numChildren=0&pagTok=" + pag_token + "&ipf=1&xhr=1"
+
+          # Open initial page
+          apps_page_dom = open_url(page_url)
+
+        else
+          pag_token = nil
+        end
+
+      end
+
+    end
+
+    add_to_db(app_ids.to_a)
+
+  end
+
+  # Pass array of app ids to add to db
+  def add_to_db(app_ids)
+
+    app_ids.each do |app_id|
+
+      android_app = AndroidApp.find_by_app_identifier(app_id)
+
+      if android_app.nil?
+        android_app = AndroidApp.new(app_identifier: app_id)
+        app = App.create
+        android_app.app = app
+        success = android_app.save
+
+        if success
+          logger.info "#{app_id} added to DB"
+        else
+          logger.error "Failed to save #{app_id} to DB"
+        end
+
+      else
+        logger.info "IosApp #{app_id} already in db"
+      end
+
+    end
+
+
+  end
+
+end
