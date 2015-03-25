cd61dd9f745e72ec3fe9bc69d0e5b1682ae08a15
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-25 14:32:29 -0700

    code ip and port

--- lib/tor_test.rb
+++ lib/tor_test.rb
@@ -13,10 +13,10 @@ class TorTest
       response.body
     end
     
-    def run_remote
+    def run_remote(ip='172.31.40.124', port=80)
       uri = URI.parse('http://wtfismyip.com/json/')
       response = nil
-      Net::HTTP.SOCKSProxy('172.31.40.124', 80).start(uri.host, uri.port) do |http|
+      Net::HTTP.SOCKSProxy('172.31.40.124', port).start(uri.host, uri.port) do |http|
         response = http.get(uri.path)
       end
       
