55318a8fc5b8c19b59a4d5f330989e3d83970ec3
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-25 16:25:21 -0700

    redirects?

--- lib/tor_test.rb
+++ lib/tor_test.rb
@@ -27,11 +27,51 @@ class TorTest
       uri = URI.parse(url)
       response = nil
       Net::HTTP.SOCKSProxy(ip, 9050).start(uri.host, uri.port) do |http|
+        http.use_ssl = true if uri.scheme == 'https'
+        
         response = http.get(uri.path)
       end
       
       response.body
     end
+    
+    def get2(url, ip='172.31.41.122', limit=10)
+      raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+      
+      uri = URI.parse(url)
+      response = nil
+      Net::HTTP.SOCKSProxy(ip, 9050).start(uri.host, uri.port) do |http|
+        http.use_ssl = true if uri.scheme == 'https'
+        
+        req = http.get(uri.path, 'User-Agent' => UserAgent.random_web)
+        
+        response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+        case response
+        when Net::HTTPSuccess     then response
+        when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+        else
+          response.error!
+        end
+      end
+      
+      response.body
+    end
+    
+    # def fetch(uri_str, limit = 10)
+    #   # You should choose better exception.
+    #   raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+    #
+    #   url = URI.parse(uri_str)
+    #   req = Net::HTTP::Get.new(url.path, { 'User-Agent' => ua })
+    #   response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+    #   case response
+    #   when Net::HTTPSuccess     then response
+    #   when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+    #   else
+    #     response.error!
+    #   end
+    # end
+    #
     
   end
   
