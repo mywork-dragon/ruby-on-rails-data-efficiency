3b18d7165ec2ae334fafd19d601dbee224ecbe2c
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-29 21:17:16 -0700

    typo

--- app/services/app_store_snapshot_service.rb
+++ app/services/app_store_snapshot_service.rb
@@ -6,7 +6,7 @@ class AppStoreSnapshotService
       
       j = IosAppSnapshotJob.create!(notes: notes)
       
-      IosApps.find_in_batches(batch_size: 100) do |ios_apps|
+      IosApp.find_in_batches(batch_size: 100) do |ios_apps|
         sleep 5
         puts ios_app_ids = ios_apps.map(&:id)
         # AppStoreSnapshotServiceWorker.perform_async
