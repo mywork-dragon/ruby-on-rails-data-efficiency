ee0015285c453dca0b8a664072d2557d162ae87f
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-29 22:59:53 -0700

    continue

--- app/workers/app_store_snapshot_service_worker.rb
+++ app/workers/app_store_snapshot_service_worker.rb
@@ -6,6 +6,9 @@ class AppStoreIdsServiceWorker
     ios_app_ids.each do |ios_app_id|
       next unless IosAppSnapshot.where(ios_app_snapshot_job_id: ios_app_snapshot_job_id, ios_app_id: ios_app_id).blank?
       
+      ios_app = IosApp.find(ios_app_id)
+      a = AppStoreService.attributes(ios_app.app_identifier)
+      
       
     end
     
