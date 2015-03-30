a282fea54177d611bf55dce6fce182c91fe849f7
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-29 20:41:27 -0700

    remove dupe migration

--- db/migrate/20150330015916_add_ios_app_snapshot_job_id_to_ios_app_snapshots.rb
+++
@@ -1,6 +0,0 @@
-class AddIosAppSnapshotJobIdToIosAppSnapshots < ActiveRecord::Migration
-  def change
-    add_column :ios_app_snapshots, :ios_snapshot_job_id, :integer
-    add_index :ios_app_snapshots, :ios_snapshot_job_id
-  end
-end
