e7522b9b4c42ed5a2e6447ee5dcffc6317cbc7c5
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-31 14:05:50 -0700

    add check for servers

--- lib/tor.rb
+++ lib/tor.rb
@@ -19,6 +19,33 @@ class Tor
       page
     end
     
+    # Checks all proxy servers to see if it can load a page
+    def check_servers(url='http://wtfismyip.com/json/')
+      es = []
+      
+      Proxy.all.each do |proxy|
+        single_server_result = check_server(url)
+        es << single_server_result unless single_server_result.nil?
+      end
+      
+      puts 'All proxies are fine!' if es.empty?
+      
+      es
+    end
+    
+    def check_server(url)
+      begin
+        page = get_using_proxy(url, proxy.private_ip) 
+      
+        puts ["Proxy id: #{proxy.id}", "Proxy public IP: #{proxy.public_ip}; Response: #{page}"].join(" | ")
+        puts ""
+      rescue => e
+        return {proxy: proxy, message: e.message, backtrace: e.backtrace}
+      end
+      
+      nil
+    end
+    
     def test(urls=['https://itunes.apple.com/us/app/dropbox/id327630330?mt=8','https://itunes.apple.com/us/app/tinder/id547702041?mt=8', 'https://itunes.apple.com/us/app/league-date-intelligently/id893653132?mt=8', 'https://play.google.com/store/apps/details?id=com.ubercab&hl=en', 'https://play.google.com/store/apps/details?id=com.supercell.clashofclans&hl=en'])
       o = []
       
