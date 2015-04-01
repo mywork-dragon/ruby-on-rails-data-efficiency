349b2348fd70b2fdea65bdee561db43c7c460eef
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-31 23:46:18 -0700

    can get updates

--- app/services/aa_service.rb
+++ app/services/aa_service.rb
@@ -22,8 +22,35 @@ class AaService
     end
     
     def updates
-      whats_new = @html.css('#app_content > div.app_content_section').children.find{ |c| c.text.strip == "What's New"}
-      whats_new.next_element
+      the_updates = @html.css('#app_content > div.app_content_section').children.find{ |c| c.text.strip == "What's New"}.next_element.children
+      
+      versions = the_updates.css('h5').select{|u| u.text.match(/.* (.*)/)}
+      notes = the_updates.css('.app-version-note').to_a
+      
+      ret = []
+      
+      versions.each_with_index do |v, i|
+        v_text = v.text
+        
+        version = v_text.gsub(/ \(.*\)/, '').strip
+        date = v_text.match(/\(.*\)/)[0].gsub('(', '').gsub(')', '').strip
+        
+        n_text = notes.to_a.fetch(i, nil)
+        
+        if n_text.nil?
+          text = nil
+        else
+          text = n_text.text 
+          text = text.strip.gsub(/Expand notes\z/, '').strip
+          text = I18n.transliterate(text)
+        end
+        
+        ret << {version: version, date: date, text: text}  
+      end
+      
+      ret
+      
+      
     end
   
     
