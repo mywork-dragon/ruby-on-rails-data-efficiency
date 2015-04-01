6d004bde3b0e9498834cc14a2a14c97992dd4f22
mywork-dragon <dragonblueyounger@gmail.com>
2015-03-31 17:07:00 -0700

    helper methods

--- app/services/app_store_snapshot_service.rb
+++ app/services/app_store_snapshot_service.rb
@@ -19,6 +19,25 @@ class AppStoreSnapshotService
       
     end
     
+    def apps_per_minute(ios_app_snapshot_job_id, sample_seconds=10)
+      IosAppSnapshot.where(ios_app_snapshot_job_id: ios_app_snapshot_job_id).count
+      sleep sample_seconds
+      b = IosAppSnapshot.where(ios_app_snapshot_job_id: ios_app_snapshot_job_id).count 
+      60.0/sample_seconds*(b-a)
+    end
+    
+    def apps_per_hour(ios_app_snapshot_job_id, sample_seconds=10)
+      apps_per_minute(ios_app_snapshot_job_id, sample_seconds)*60.0
+    end
+    
+    def apps_per_day(ios_app_snapshot_job_id, sample_seconds=10)
+      apps_per_hour(ios_app_snapshot_job_id, sample_seconds)*24.0
+    end
+    
+    def hours_per_job(ios_app_snapshot_job_id, sample_seconds=10)
+      IosApp.count * (1.0 / apps_per_hour(ios_app_snapshot_job_id, sample_seconds))
+    end
+    
     def test
       100.times{ AppStoreSnapshotServiceWorker.perform_async }
     end
