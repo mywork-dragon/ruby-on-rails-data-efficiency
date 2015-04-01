b86f4e56bb3b4d71f8a8bc2d9ee3354c5584fc64
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-01 14:28:30 -0700

    nil dates

--- app/services/aa_service.rb
+++ app/services/aa_service.rb
@@ -6,23 +6,31 @@ class AaService
     # @author Jason Lew
     # @param id The App Store identifier
     def attributes(id, options={})
-      @html = aa_html(id)
-
+      @html_updates = aa_html_updates(id)
+      #@html_countries = aa_html_countries(id)
+        
       ret = {}
     
       ret[:updates] = updates
+      #ret[:countries] = countries
     
       ret
     end
     
-    def aa_html(id)
+    def aa_html_updates(id)
       page = Tor.get("https://www.appannie.com/apps/ios/app/#{id}")
     
       Nokogiri::HTML(page)
     end
     
+    def aa_html_countries(id)
+      page = Tor.get("https://www.appannie.com/apps/ios/app/#{id}/app-ranking")
+    
+      Nokogiri::HTML(page)
+    end
+    
     def updates
-      the_updates = @html.css('#app_content > div.app_content_section').children.find{ |c| c.text.strip == "What's New"}.next_element.children
+      the_updates = @html_updates.css('#app_content > div.app_content_section').children.find{ |c| c.text.strip == "What's New"}.next_element.children
       
       versions = the_updates.css('h5').select{|u| u.text.match(/.* (.*)/)}
       notes = the_updates.css('.app-version-note').to_a
@@ -30,11 +38,18 @@ class AaService
       ret = []
       
       versions.each_with_index do |v, i|
-        v_text = v.text
+        puts v_text = v.text
         
         version = v_text.gsub(/ \(.*\)/, '').strip
-        date_as_text = v_text.match(/\(.*\)/)[0].gsub('(', '').gsub(')', '').strip
-        date = Date.parse(date_as_text)
+        
+        date_match = v_text.match(/\(.*\)/)
+        
+        if date_match.nil?
+          date = nil
+        else
+          date_as_text = date_match[0].gsub('(', '').gsub(')', '').strip
+          date = Date.parse(date_as_text)
+        end
         
         n_text = notes.to_a.fetch(i, nil)
         
@@ -52,6 +67,10 @@ class AaService
       ret
       
       
+    end
+    
+    def countries
+      ranks_node = @html_countries.css('ranks')
     end
   
     
