63b837871095b439e0b4b4541b61e5e966753557
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-02 02:13:09 -0700

    explicit SSHKit

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,7 +64,7 @@ end
 namespace :sidekiq do
   task :quiet do
     # Horrible hack to get PID without having to use terrible PID files
-    puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+    puts SSHKit.capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
   end
   task :restart do
     execute :sudo, :initctl, :restart, :workers
