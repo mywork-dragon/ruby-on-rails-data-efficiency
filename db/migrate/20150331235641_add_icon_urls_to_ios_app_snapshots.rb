75f9002b400993cf2c1ac726160e050eb895c3e6
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-02 13:57:58 -0700

    workaround

--- db/migrate/20150331235641_add_icon_urls_to_ios_app_snapshots.rb
+++ db/migrate/20150331235641_add_icon_urls_to_ios_app_snapshots.rb
@@ -1,6 +1,6 @@
 class AddIconUrlsToIosAppSnapshots < ActiveRecord::Migration
   def change
-    add_column :ios_app_snapshots, :icon_url_350x350, :string
-    add_column :ios_app_snapshots, :icon_url_175x175, :string 
+    add_column :ios_app_snapshots, :icon_url_350x350, :string unless ActiveRecord::Base.connection.column_exists?(:ios_app_snapshots, :icon_url_350x350)
+    add_column :ios_app_snapshots, :icon_url_175x175, :string unless ActiveRecord::Base.connection.column_exists?(:ios_app_snapshots, :icon_url_175x175)
   end
 end
