cada056a691bf31d04d10e3518f25ed1b510ac94
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-01 22:58:47 -0700

    move

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,19 +51,19 @@ namespace :deploy do
     end
   end
 
-end
-
-# For capistrano 3
-namespace :sidekiq do
-  task :quiet do
-    # Horrible hack to get PID without having to use terrible PID files
-    puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :") 
+  # For capistrano 3
+  namespace :sidekiq do
+    task :quiet do
+      # Horrible hack to get PID without having to use terrible PID files
+      puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :") 
+    end
+    task :restart do
+      execute :sudo, :initctl, :restart, :workers
+    end
   end
-  task :restart do
-    execute :sudo, :initctl, :restart, :workers
-  end
-end
 
-after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:reverted', 'sidekiq:restart'
-after 'deploy:published', 'sidekiq:restart'
+  after 'deploy:starting', 'sidekiq:quiet'
+  after 'deploy:reverted', 'sidekiq:restart'
+  after 'deploy:published', 'sidekiq:restart'
+
+end
