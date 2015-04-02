595279e6baf1ec7d0aff823142855f47a891e6b1
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-02 02:26:30 -0700

    execute instead

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,7 +66,8 @@ end
 namespace :sidekiq do
   task :quiet do
     # Horrible hack to get PID without having to use terrible PID files
-    puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+    # puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+    execute("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
   end
   task :restart do
     execute :sudo, :initctl, :restart, :workers
