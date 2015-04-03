6448bfabe932476e37a929eb85c2e83a4f7c387f
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-03 11:54:22 -0700

    scraper sidekiq

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,21 +55,21 @@ namespace :deploy do
 
 end
 
-# namespace :sidekiq do
-#   task :quiet do
-#     on roles(:scraper) do
-#       # Horrible hack to get PID without having to use terrible PID files
-#       puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")\
-#     end
-#
-#   end
-#   task :restart do
-#     on roles(:scraper) do
-#       execute :sudo, :initctl, :restart, :workers
-#     end
-#   end
-# end
-#
-# after 'deploy:starting', 'sidekiq:quiet'
-# after 'deploy:reverted', 'sidekiq:restart'
-# after 'deploy:published', 'sidekiq:restart'
+namespace :sidekiq do
+  task :quiet do
+    on roles(:scraper) do
+      # Horrible hack to get PID without having to use terrible PID files
+      puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")\
+    end
+
+  end
+  task :restart do
+    on roles(:scraper) do
+      execute :sudo, :initctl, :restart, :workers
+    end
+  end
+end
+
+after 'deploy:starting', 'sidekiq:quiet'
+after 'deploy:reverted', 'sidekiq:restart'
+after 'deploy:published', 'sidekiq:restart'
