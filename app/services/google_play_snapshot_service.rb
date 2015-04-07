0c8dd1a12d80c1206778afe4128bef509c27c746
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-07 07:50:57 -0500

    Create GPSS file

---
+++ app/services/google_play_snapshot_service.rb
@@ -0,0 +1,41 @@
+class GooglePlaySnapshotServce
+  
+  class << self
+  
+    def run(notes, options={})
+      
+      j = AndroidAppSnapshotJob.create!(notes: notes)
+  
+      AndroidApp.find_each.with_index do |android_app, index|
+        li "App ##{index}" if index%10000 == 0
+        AndroidAppSnapshotServiceWorker.perform_async(j.id, anr.id)
+      end
+      
+    end
+    
+    def apps_per_minute(ios_app_snapshot_job_id, sample_seconds=10)
+      IosAppSnapshot.where(ios_app_snapshot_job_id: ios_app_snapshot_job_id).count
+      sleep sample_seconds
+      b = IosAppSnapshot.where(ios_app_snapshot_job_id: ios_app_snapshot_job_id).count 
+      60.0/sample_seconds*(b-a)
+    end
+    
+    def apps_per_hour(ios_app_snapshot_job_id, sample_seconds=10)
+      apps_per_minute(ios_app_snapshot_job_id, sample_seconds)*60.0
+    end
+    
+    def apps_per_day(ios_app_snapshot_job_id, sample_seconds=10)
+      apps_per_hour(ios_app_snapshot_job_id, sample_seconds)*24.0
+    end
+    
+    def hours_per_job(ios_app_snapshot_job_id, sample_seconds=10)
+      IosApp.count * (1.0 / apps_per_hour(ios_app_snapshot_job_id, sample_seconds))
+    end
+    
+    def test
+      100.times{ AppStoreSnapshotServiceWorker.perform_async }
+    end
+  
+  end
+  
+end
