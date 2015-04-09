b3ea977be9a0cff891b6013ca904e795b6378d35
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-09 11:46:50 -0700

    remove download range

--- app/models/android_app_release.rb
+++ app/models/android_app_release.rb
@@ -2,7 +2,6 @@ class AndroidAppRelease < ActiveRecord::Base
 
   has_many :languages
   belongs_to :android_app
-  has_one :android_app_download_range
   has_one :android_app_review_snapshot
   
 end
