94486f5d8284a4f9b129c9de80cbe88878b390da
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-10 14:28:47 -0700

    iOS dedupe

--- app/models/android_app.rb
+++ app/models/android_app.rb
@@ -11,19 +11,20 @@ class AndroidApp < ActiveRecord::Base
   class << self
     
     def dedupe
-        # find all models and group them on keys which should be common
-        grouped = all.group_by{|model| [model.app_identifier] }
-        grouped.values.each do |duplicates|
-          # the first one we want to keep right?
-          first_one = duplicates.shift # or pop for last one
-          # if there are any more left, they are duplicates
-          # so delete all of them
-          duplicates.each do |double| 
-            puts "double: #{double.app_identifier}"
-            double.destroy # duplicates can now be destroyed
-          end
+      # find all models and group them on keys which should be common
+      grouped = all.group_by{|model| [model.app_identifier] }
+      grouped.values.each do |duplicates|
+        # the first one we want to keep right?
+        first_one = duplicates.shift # or pop for last one
+        # if there are any more left, they are duplicates
+        # so delete all of them
+        duplicates.each do |double| 
+          puts "double: #{double.app_identifier}"
+          double.destroy # duplicates can now be destroyed
         end
       end
+    end
+    
   end
 
 end
