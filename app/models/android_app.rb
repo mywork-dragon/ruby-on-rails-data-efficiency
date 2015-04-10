a14afddcd708f2d90e077a571d533629ca119d78
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-10 14:14:58 -0700

    dedupe android app

--- app/models/android_app.rb
+++ app/models/android_app.rb
@@ -8,4 +8,22 @@ class AndroidApp < ActiveRecord::Base
   has_many :android_apps_snapshots
   has_many :websites, through: :android_apps_snapshots
 
+  class << self
+    
+    def dedupe
+        # find all models and group them on keys which should be common
+        grouped = all.group_by{|model| [model.app_identifier] }
+        grouped.values.each do |duplicates|
+          # the first one we want to keep right?
+          first_one = duplicates.shift # or pop for last one
+          # if there are any more left, they are duplicates
+          # so delete all of them
+          duplicates.each do |double| 
+            puts "double: #{double}"
+            double.destroy # duplicates can now be destroyed
+          end
+        end
+      end
+  end
+
 end
