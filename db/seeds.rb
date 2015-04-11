9c8d0a71a4932be877e4b9cf528cc84ca9350f88
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-11 14:52:04 -0700

    seed data v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,32 @@
 
 if Rails.env.development?
 
-  puts "creating ios and android apps..."
-  for i in 1..500
-    IosApp.find_or_create_by(app_identifier: i)
-    AndroidApp.find_or_create_by(app_identifier: "com.company#{i}")
-  end
-
   puts "creating categories..."
   categories = ["Games", "Lifestyle", "Shopping", "Social", "Entertainment", "News", "Education", "Medical", "Productivity", "Music", "Photography"]
   categories.each do |cat|
     IosAppCategory.find_or_create_by(name: cat)
     AndroidAppCategory.find_or_create_by(name: cat)
   end
-  
-  puts "creating ios and android snapshots..."
-  for i in 1..200
+
+  puts "creating ios and android apps, and creating one snapshot for each..."
+  for i in 1..500
     name = Faker::App.name
-    ios_snapshot = IosAppSnapshot.create(name: name, released: Faker::Time.between(2.years.ago, Time.now), ios_app_id: IosApp.all.sample.id, icon_url_350x350: Faker::Avatar.image("#{name}#{i}350", "350x350"), icon_url_175x175: Faker::Avatar.image("#{name}#{i}175"))
+    
+    ios_app = IosApp.find_or_create_by(app_identifier: i)
+    ios_app_snapshot = IosAppSnapshot.create(name: name, released: Faker::Time.between(2.years.ago, Time.now), ios_app_id: IosApp.all.sample.id, icon_url_350x350: Faker::Avatar.image("#{name}#{i}350", "350x350"), icon_url_175x175: Faker::Avatar.image("#{name}#{i}175"), price: Faker::Commerce.price, size: rand(1000..1000000), version: Faker::App.version, description: Faker::Lorem.paragraph, release_notes: Faker::Lorem.paragraph, ratings_current_stars: rand(0..5), ratings_current_count: rand(0..100), ratings_all_stars: rand(0..5), ratings_all_count: rand(100..500))
+    ios_app.ios_app_snapshots << ios_app_snapshot
     ios_cat = IosAppCategory.all.sample
-    ios_cat.ios_app_snapshots << ios_snapshot
-    android_snapshot = AndroidAppSnapshot.create(name: name, released: Faker::Time.between(2.years.ago, Time.now), android_app_id: AndroidApp.all.sample.id, icon_url_300x300: Faker::Avatar.image("#{name}#{i}300", "300x300"))
+    ios_cat.ios_app_snapshots << ios_app_snapshot
+    
+    android_app = AndroidApp.find_or_create_by(app_identifier: "com.company#{i}")
+    android_app_snapshot = AndroidAppSnapshot.create(name: name, released: Faker::Time.between(2.years.ago, Time.now), android_app_id: AndroidApp.all.sample.id, icon_url_300x300: Faker::Avatar.image("#{name}#{i}300", "300x300"), price: Faker::Commerce.price, size: rand(1000..1000000), description: Faker::Lorem.paragraph, google_plus_likes: rand(10..1000), ratings_all_stars: rand(0..5), ratings_all_count: rand(10..100), installs_min: rand(0..100), installs_max: rand(100..1000000))
+    android_app.android_app_snapshots << android_app_snapshot
     android_cat = AndroidAppCategory.all.sample
-    android_cat.android_app_snapshots << android_snapshot
+    android_cat.android_app_snapshots << android_app_snapshot
   end
 
   puts "creating companies..."
-  for i in 1..3000
+  for i in 1..1500
     if i <= 1000
       Company.create(name: Faker::Company.name, fortune_1000_rank: i, street_address: Faker::Address.street_address, city: Faker::Address.city, zip_code: Faker::Address.zip_code, state: Faker::Address.state_abbr, country: Faker::Address.country)
     else
@@ -42,7 +42,7 @@ if Rails.env.development?
   end
 
   puts "creating websites, and linking them to companies, ios apps, and android apps..."
-  for i in 1..6000
+  for i in 1..2000
     site = Website.create(url: Faker::Internet.domain_name, kind: rand(0..1))
     company = Company.all.sample
     ios_app1 = IosApp.all.sample
@@ -55,5 +55,8 @@ if Rails.env.development?
     ios_app2.websites << site
     android_app1.websites << site
     android_app2.websites << site
+    if i % 100 == 0
+      puts "#{i/200}/20 of the way done"
+    end
   end
 end
