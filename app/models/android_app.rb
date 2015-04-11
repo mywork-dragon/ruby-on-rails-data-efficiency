892102c0d4cd42512af70ed0cbd3cc0aabc53679
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-11 15:47:23 -0700

    removed newest_download_snapshot method from android app model

--- app/models/android_app.rb
+++ app/models/android_app.rb
@@ -10,15 +10,10 @@ class AndroidApp < ActiveRecord::Base
 
   def get_newest_app_snapshot
     self.android_app_snapshots.max_by do |snapshot|
-      snapshot.updated_at
+      snapshot.created_at
     end
   end
   
-  def get_newest_download_snapshot
-    self.android_app_download_snapshots.max_by do |snapshot|
-      snapshot.updated_at
-    end
-  end
   
   def get_company
     self.websites.each do |w|
