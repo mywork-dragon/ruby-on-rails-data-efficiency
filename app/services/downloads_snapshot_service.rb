466d86f6603d97305484f0f3285b47560942fa54
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-11 18:34:04 -0700

    downloads should work

--- app/services/downloads_snapshot_service.rb
+++ app/services/downloads_snapshot_service.rb
@@ -13,6 +13,25 @@ class DownloadsSnapshotService
 
     end
     
+    def apps_per_minute(ios_app_download_snapshot_job_id=IosAppDownloadSnapshotJob, sample_seconds=10)
+      a = IosAppDownloadSnapshot.where(ios_app_download_snapshot_job_id: ios_app_download_snapshot_job_id:).count
+      sleep sample_seconds
+      b = IosAppDownloadSnapshot.where(ios_app_download_snapshot_job_id: ios_app_download_snapshot_job_id:).count
+      60.0/sample_seconds*(b-a)
+    end
+
+    def apps_per_hour(ios_app_download_snapshot_job_id=IosAppDownloadSnapshotJob.last.id, sample_seconds=10)
+      apps_per_minute(ios_app_download_snapshot_job_id, sample_seconds)*60.0
+    end
+
+    def apps_per_day(ios_app_download_snapshot_job_id=IosAppDownloadSnapshotJob.last.id, sample_seconds=10)
+      apps_per_hour(ios_app_download_snapshot_job_id, sample_seconds)*24.0
+    end
+
+    def hours_per_job(ios_app_download_snapshot_job_id=IosAppDownloadSnapshotJob.last.id, sample_seconds=10)
+      IosApp.count * (1.0 / apps_per_hour(ios_app_download_snapshot_job_id, sample_seconds))
+    end
+    
   end
   
 end
--- app/workers/downloads_snapshot_service_worker.rb
+++ app/workers/downloads_snapshot_service_worker.rb
@@ -14,15 +14,33 @@ class DownloadsSnapshotServiceWorker
 
   def save_attributes(options={})
     ios_app = IosApp.find(options[:ios_app_id])
-    ios_app_snapshot_job_id = options[:ios_app_snapshot_job_id]
+    ios_app_download_snapshot_job_id = options[:ios_app_download_snapshot_job_id]
 
     s = IosAppDownloadSnapshot.create(ios_app: ios_app, ios_app_download_snapshot_job_id: ios_app_download_snapshot_job_id)
 
     try = 0
 
     begin
-
-      a = DownloadsService.attributes(app_identifier: ios_app.app_identifier, description: ios_app.description, name: ios_app.name)
+      
+      name = nil
+      
+      recent_snapshots = IosAppSnapshot.where(ios_app: ios_app).order(created_at: :desc).limit(5)
+      
+      recent_snapshots.each do |rss| 
+        if rss_name = rss.name
+          name = rss_name
+          break
+        end
+      end
+      
+      app_identifier = ios_app.app_identifier
+      
+      # name = 'Dropbox'
+      # app_identifier = 327630330
+      
+      raise "Couldn't find name in any recent snapshots" if name.nil?
+      
+      a = DownloadsService.attributes(app_identifier: app_identifier, name: name)
 
       raise 'DownloadsService.attributes is empty' if a.empty?
       
@@ -33,7 +51,7 @@ class DownloadsSnapshotServiceWorker
       s.save!
 
     rescue => e
-      ise = IosAppDownloadSnapshotException.create(ios_app_snapshot: s, name: e.message, backtrace: e.backtrace, try: try, ios_app_download_snapshot_job_id: ios_app_download_snapshot_job_id)
+      ise = IosAppDownloadSnapshotException.create(ios_app_download_snapshot: s, name: e.message, backtrace: e.backtrace, try: try, ios_app_download_snapshot_job_id: ios_app_download_snapshot_job_id)
       if (try += 1) < MAX_TRIES
         retry
       else
@@ -46,14 +64,6 @@ class DownloadsSnapshotServiceWorker
     end
 
     s
-  end
-
-  def test_save_attributes
-    ids = [389377362, 801207885, 509978909, 946286572, 355074115]
-
-    android_app_ids = ids.map{ |id| AndroidApp.find_or_create_by(app_identifier: id) }
-
-    perform(-1, ios_app_ids)
   end
   
 end
