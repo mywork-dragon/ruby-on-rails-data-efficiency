fac45d8d9ea7aa8802249c98cb7d51543e9b6986
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-13 11:23:00 -0700

    for the rest

--- app/services/add_websites_from_companies_service.rb
+++ app/services/add_websites_from_companies_service.rb
@@ -1,16 +1,34 @@
 class AddWebsitesFromCompanies
   
-  def run_fortune_1000
-    cs = Company.where.not(fortune_1000_rank: nil)
+  class << self
     
-    cs.each do |c|
+    def run_fortune_1000
+      cs = Company.where.not(fortune_1000_rank: nil)
+    
+      cs.each do |c|
+        w = Website.find_or_create_by(url: c.website)
+      
+        w.company = c
+        w.save
+      
+      end
+    
+    end
+  
+    def run_rest
+      Company.find_each.with_index do |c, index|
+        li "Company ##{c}" if index%10000 == 0
+        self.delay.run_rest_add(c.id)
+      end
+    end
+    
+    def run_rest_add(company_id)
+      c = Company.find(company_id)
       w = Website.find_or_create_by(url: c.website)
-      
-      w.company = c
-      w.save
-      
     end
     
   end
+  
+
   
 end
