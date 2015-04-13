0df6b58b238fbb3c8230bdc253d93e540bf7de92
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-13 11:09:01 -0700

    script

--- app/services/add_websites_from_companies_service.rb
+++ app/services/add_websites_from_companies_service.rb
@@ -1,3 +1,19 @@
 class AddWebsitesFromCompanies
   
+  def run_fortune_1000
+    cs = Company.where.not(fortune_1000_rank: nil)
+    
+    cs.each do |c|
+      w = Website.new(url: url)
+      success = w.save
+      
+      if success
+        w.company = c
+        w.save
+      end
+      
+    end
+    
+  end
+  
 end
