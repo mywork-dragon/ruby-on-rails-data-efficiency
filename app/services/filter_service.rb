dc357bd3e844db69ecf3b57c5329e00e87295e6f
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-16 14:56:14 -0700

    try this

--- app/services/filter_service.rb
+++ app/services/filter_service.rb
@@ -10,16 +10,20 @@ class FilterService
     end
     
     def filter_ios_apps(app_filters)
-      results = IosApp.includes(:ios_fb_ad_appearances, newest_ios_app_snapshot: :ios_app_categories, websites: :company)
+      limit = 10 #TODO: pass this as parameter later
+      queries = []
+      first_object = IosApp
+      
+      queries << 'includes(:ios_fb_ad_appearances, newest_ios_app_snapshot: :ios_app_categories, websites: :company)'
       if app_filters[:mobilePriority]
         mobile_priorities = []
-        mobile_priorities << :high if app_filters[:mobilePriority].include?("High")
-        mobile_priorities << :medium if app_filters[:mobilePriority].include?("Medium")
-        mobile_priorities << :low if app_filters[:mobilePriority].include?("Low")
-        results = results.where(mobile_priority: mobile_priorities)
+        mobile_priorities << IosApp.mobile_priorities[:high] if app_filters[:mobilePriority].include?("High")
+        mobile_priorities << IosApp.mobile_priorities[:medium] if app_filters[:mobilePriority].include?("Medium")
+        mobile_priorities << IosApp.mobile_priorities[:low] if app_filters[:mobilePriority].include?("Low")
+        queries << "where(mobile_priority: mobile_priorities)"
       end
       
-      results = results.joins(:ios_fb_ad_appearances) if app_filters[:adSpend]
+      queries << 'joins(:ios_fb_ad_appearances)' if app_filters[:adSpend]
       
       if app_filters[:userBases]
         user_bases = []
@@ -27,16 +31,48 @@ class FilterService
         user_bases << IosApp.user_bases[:strong] if app_filters[:userBases].include?("Strong")
         user_bases << IosApp.user_bases[:moderate] if app_filters[:userBases].include?("Moderate")
         user_bases << IosApp.user_bases[:weak] if app_filters[:userBases].include?("Weak")
-        results = results.where(user_base: user_bases)
+        queries << 'where(user_base: user_bases)'
       end
       
       if app_filters[:updatedMonthsAgo]
-        results = results.joins(:newest_ios_app_snapshot).where('released > ?', app_filters[:updatedMonthsAgo].to_i.months.ago.to_date)
+        queries << "joins(:newest_ios_app_snapshot).where('released > ?', app_filters[:updatedMonthsAgo].to_i.months.ago.to_date)"
       end
       if app_filters[:categories]
-        results = results.joins(newest_ios_app_snapshot: {ios_app_categories_snapshots: :ios_app_category}).where('ios_app_categories.name IN (?)', app_filters[:categories].join(','))
+        queries << "joins(newest_ios_app_snapshot: {ios_app_categories_snapshots: :ios_app_category}).where('ios_app_categories.name IN (?)', app_filters[:categories].join(','))"
       end
-      results
+      
+      query = queries.join('.')
+      puts "Query: #{query}"
+      first_object.instance_eval("self.#{query}.limit(#{limit})")
+      
+      
+      # results = IosApp.includes(:ios_fb_ad_appearances, newest_ios_app_snapshot: :ios_app_categories, websites: :company)
+      # if app_filters[:mobilePriority]
+      #   mobile_priorities = []
+      #   mobile_priorities << :high if app_filters[:mobilePriority].include?("High")
+      #   mobile_priorities << :medium if app_filters[:mobilePriority].include?("Medium")
+      #   mobile_priorities << :low if app_filters[:mobilePriority].include?("Low")
+      #   results = results.where(mobile_priority: mobile_priorities)
+      # end
+      #
+      # results = results.joins(:ios_fb_ad_appearances) if app_filters[:adSpend]
+      #
+      # if app_filters[:userBases]
+      #   user_bases = []
+      #   user_bases << IosApp.user_bases[:elite] if app_filters[:userBases].include?("Elite")
+      #   user_bases << IosApp.user_bases[:strong] if app_filters[:userBases].include?("Strong")
+      #   user_bases << IosApp.user_bases[:moderate] if app_filters[:userBases].include?("Moderate")
+      #   user_bases << IosApp.user_bases[:weak] if app_filters[:userBases].include?("Weak")
+      #   results = results.where(user_base: user_bases)
+      # end
+      #
+      # if app_filters[:updatedMonthsAgo]
+      #   results = results.joins(:newest_ios_app_snapshot).where('released > ?', app_filters[:updatedMonthsAgo].to_i.months.ago.to_date)
+      # end
+      # if app_filters[:categories]
+      #   results = results.joins(newest_ios_app_snapshot: {ios_app_categories_snapshots: :ios_app_category}).where('ios_app_categories.name IN (?)', app_filters[:categories].join(','))
+      # end
+      # results
     end
     
     def apps_with_keywords(keywords)
