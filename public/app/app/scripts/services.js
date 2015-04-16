c994681705d20c65f6d15658e5577b0378a0069a
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-16 10:42:11 -0700

    Dashboard Search POST Skeleton Added

---
+++ public/app/app/scripts/services.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**************************
+ App ui Services
+
+ **************************/
+
+angular.module("appApp").factory("apiService", ['$http', function($http) {
+
+  return {
+    postDashboardSearch: function(searchRequest) {
+      return searchRequest;
+      /*
+      $http({
+        method: 'POST',
+        url: 'https://www.example.com/api/v1/page',
+        params: 'limit=10, sort_by=created:desc'
+      });
+      },
+      getApp: function() {
+        return $http({
+          method: 'GET',
+          url: 'https://www.example.com/api/v1/page',
+          params: 'limit=10, sort_by=created:desc'
+        });
+      },
+      getCompany: function() {
+        return $http({
+          method: 'GET',
+          url: 'https://www.example.com/api/v1/page',
+          params: 'limit=10, sort_by=created:desc'
+        });
+       */
+    }
+  }
+}]);
