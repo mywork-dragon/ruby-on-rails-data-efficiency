4812c5b7530e031eeadbb1198c14b8dc07c9c9e8
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-16 17:28:32 -0700

    Rails End Point Testing

--- public/app/app/scripts/services.js
+++ public/app/app/scripts/services.js
@@ -9,28 +9,33 @@ angular.module("appApp").factory("apiService", ['$http', function($http) {
 
   return {
     postDashboardSearch: function(text) {
-      console.log(text);
-      /*
-       postDashboardSearch: $http({
+      return $http({
         method: 'POST',
-        url: 'https://www.example.com/api/v1/page',
-        params: 'limit=10, sort_by=created:desc'
+        headers: {
+          'Content-Type': 'json'
+        },
+        url: 'http://mightysignal.com/api/filter_ios_apps',
+        data: {"app": {"adSpend": "true"}}
+      }).success(function(data) {
+        console.log(data);
       });
-      },
-      getApp: function() {
-        return $http({
-          method: 'GET',
-          url: 'https://www.example.com/api/v1/page',
-          params: 'limit=10, sort_by=created:desc'
-        });
-      },
-      getCompany: function() {
-        return $http({
-          method: 'GET',
-          url: 'https://www.example.com/api/v1/page',
-          params: 'limit=10, sort_by=created:desc'
-        });
-       */
     }
-  }
+  };
 }]);
+
+/*
+ },
+ getApp: function() {
+ return $http({
+ method: 'GET',
+ url: 'https://www.example.com/api/v1/page',
+ params: 'limit=10, sort_by=created:desc'
+ });
+ },
+ getCompany: function() {
+ return $http({
+ method: 'GET',
+ url: 'https://www.example.com/api/v1/page',
+ params: 'limit=10, sort_by=created:desc'
+ });
+ */
