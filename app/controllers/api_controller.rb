d62c0ac3335d40ee7154d8bce7d2e2379d8cda1e
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-18 14:10:54 -0700

    try without sorting / ordering

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -39,7 +39,7 @@ class ApiController < ApplicationController
     
     results_count = IosApp.instance_eval("#{query}.length")
     query += ".limit(#{pageSize}).offset(#{(pageNum-1) * pageSize})"
-    query += ".#{FilterService.ios_sort_order_query(sort_by, order_by)}"
+    # query += ".#{FilterService.ios_sort_order_query(sort_by, order_by)}"
     # query += ".#{order_query}"
     results = IosApp.instance_eval(query)
     results_json = []
