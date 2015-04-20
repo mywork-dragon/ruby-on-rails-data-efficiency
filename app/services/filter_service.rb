cdfe35399abb229263bf33e887f9ed0023cd91a9
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-20 13:52:58 -0700

    double quotes around categories to avoid single quote bug

--- app/services/filter_service.rb
+++ app/services/filter_service.rb
@@ -47,7 +47,7 @@ class FilterService
       end
       
       if app_filters[:categories]
-        cats_with_quotes = app_filters[:categories].map{|c| "\'#{c}\'"}
+        cats_with_quotes = app_filters[:categories].map{|c| "\"#{c}\""}
         queries << "joins(newest_ios_app_snapshot: {ios_app_categories_snapshots: :ios_app_category}).where('ios_app_categories.name IN (?)', #{cats_with_quotes.join(',')})"
       end
       
