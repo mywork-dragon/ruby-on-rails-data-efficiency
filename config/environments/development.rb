3df2e9ddb08b0ec17ac07604306df185ad957cad
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-19 18:03:35 -0700

    remove sign up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,6 @@ Rails.application.configure do
     password:             'iamthemailmanofmightysignalyo',
     authentication:       'plain',
     enable_starttls_auto: true  }
+    
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 end
