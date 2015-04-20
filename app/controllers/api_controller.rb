0e6437fd73b47034ec16c1b1907e5915ea894715
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-20 13:31:55 -0700

    only get categories with ios_app_snapshots

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -245,7 +245,7 @@ class ApiController < ApplicationController
   end
 
   def get_ios_categories
-    render json: IosAppCategory.select(:name).all.order('name asc').to_a.map{|cat| cat.name}
+    render json: IosAppCategory.select(:name).joins(:ios_app_categories_snapshots).group('ios_app_categories.id').order('name asc').to_a.map{|cat| cat.name}
   end
 
 end
