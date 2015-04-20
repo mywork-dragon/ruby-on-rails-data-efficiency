73afbf508bfd74a4b0903ffe3531929f6c0a7725
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-20 11:58:51 -0700

    try something else

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'devise'
+
 module Varys
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
