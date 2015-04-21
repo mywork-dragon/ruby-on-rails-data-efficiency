c18df576fef9c5e5f2b5d89bc2a8c80ca1c35187
mywork-dragon <dragonblueyounger@gmail.com>
2015-04-20 17:56:46 -0700

    fix encoding

--- lib/auth_token.rb
+++ lib/auth_token.rb
@@ -1,7 +1,9 @@
 class AuthToken
+  
   def self.encode(payload, exp=1.month.from_now)
-  payload[:exp] = exp.to_i
-  JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
 
   def self.decode(token)
     payload = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
@@ -9,4 +11,5 @@ class AuthToken
   rescue
     nil # It will raise an error if it is not a token that was generated with our secret key or if the user changes the contents of the payload
   end
+  
 end
