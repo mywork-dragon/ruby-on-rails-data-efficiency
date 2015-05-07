86f768fea13036aeda3c5fd9bb566e6dc858be6b
mywork-dragon <dragonblueyounger@gmail.com>
2015-05-06 23:00:46 -0700

    comment out test

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -13,9 +13,22 @@ class ApiControllerTest < ActionController::TestCase
   #   puts "TURRDOWN"
   # end
   
-  test "Should filter correctly for Mobile Priority" do
-    post(:filter_ios_apps, {'app' => {'mobilePriority' => "H"}})
-    puts "response: #{@response.body}"
-    assert true
-  end
+  # test "Should filter correctly for Mobile Priority" do
+  #   puts "cols: #{IosApp.column_names}"
+  #   ios_app = IosApp.create!(app_identifier: 123, mobile_priority: :medium)
+  #
+  #   ios_app_snapshot = IosAppSnapshot.create
+  #
+  #   ios_app.newest_ios_app_snapshot = ios_app_snapshot
+  #   ios_app.save
+  #
+  #   post(:filter_ios_apps, {'app' => {'mobilePriority' => "M"}})
+  #   puts "response: #{@response.body}"
+  #
+  #   puts "ios_app: #{IosApp.find_by_app_identifier(123).app_identifier}"
+  #
+  #   assert true
+  # end
+  
+    
 end
