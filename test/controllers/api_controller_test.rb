384fbd167dba2eb6f48fcc333c61a3e009272317
mywork-dragon <dragonblueyounger@gmail.com>
2015-05-06 22:16:04 -0700

    test works

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
+require 'action_controller'
+
 class ApiControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+  
+  # def setup
+  #   puts "SETUP API"
   # end
+  #
+  # # called after every single test
+  # def teardown
+  #   puts "TURRDOWN"
+  # end
+  
+  test "Should filter correctly for Mobile Priority" do
+    post(:filter_ios_apps, {'app' => {'mobilePriority' => "H"}})
+    puts "response: #{@response}"
+    assert true
+  end
 end
