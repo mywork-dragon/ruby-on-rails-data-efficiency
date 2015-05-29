3708462b55a66024f404dceccede70263cfc98d4
mywork-dragon <dragonblueyounger@gmail.com>
2015-05-29 14:44:16 -0700

    dummy model

---
+++ db/migrate/20150529214401_dummy_model.rb
@@ -0,0 +1,4 @@
+class DummyModel < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529063445) do
+ActiveRecord::Schema.define(version: 20150529214401) do
 
   create_table "android_app_categories", force: true do |t|
     t.string   "name"
