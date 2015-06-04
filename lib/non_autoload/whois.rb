7fc0e7b0b48cc6b1b531df96d0f6604530773df5
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-04 00:07:44 -0700

    Tor might be working

--- lib/non_autoload/whois.rb
+++ lib/non_autoload/whois.rb
@@ -1,9 +1,14 @@
 Whois::Server::SocketHandler.class_eval do
   
+  alias_method :old_execute, :execute
+  
   def execute(query, *args)
-    puts "EXECUTION!!!"
+    #puts "EXECUTION!!!"
 
-    #super
+    TCPSocket::socks_server = "127.0.0.1"
+    TCPSocket::socks_port = 9050
+
+    old_execute(query, *args)
   end
   
 end
