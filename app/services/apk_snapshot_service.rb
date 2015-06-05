f8ecfb5c9979d6ad767e962f35c5e98949a8336d
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-05 15:19:33 -0700

    background

--- app/services/apk_snapshot_service.rb
+++ app/services/apk_snapshot_service.rb
@@ -27,7 +27,7 @@ class ApkSnapshotService
       j = ApkSnapshotJob.create!(notes: notes)
       
       aa = AndroidApp.find_by_app_identifier('com.pinterest')
-      ApkSnapshotServiceWorker.new.perform(j.id, aa.id)
+      ApkSnapshotServiceWorker.perform_async(j.id, aa.id)
       
     end
   
