eb58fa739eeee04055027319e3f5de7080792f26
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-05 15:10:13 -0700

    run 100 method

--- app/services/apk_snapshot_service.rb
+++ app/services/apk_snapshot_service.rb
@@ -6,11 +6,19 @@ class ApkSnapshotService
       
       j = ApkSnapshotJob.create!(notes: notes)
       
-      AndroidApp.select(:id).joins(:newest_android_app_snapshot).where("android_app_snapshots.price = ?", 0).limit(100).find_each do |app|
+      AndroidApp.select(:id).joins(:newest_android_app_snapshot).where("android_app_snapshots.price = ?", 0).find_each.with_index do |app, index|
         ApkSnapshotServiceWorker.perform_async(j.id, app.id)
-        # ApkSnapshotServiceWorker.new.perform(j.id, app.id)
       end
       
+    end 
+  
+    def run_100(notes)
+      j = ApkSnapshotJob.create!(notes: notes)
+      
+      AndroidApp.select(:id).joins(:newest_android_app_snapshot).where("android_app_snapshots.price = ?", 0).limit(100).each_with_index do |app, index|
+        li "App #{index}"
+        ApkSnapshotServiceWorker.perform_async(j.id, app.id)
+      end
     end
   
     # For testing
