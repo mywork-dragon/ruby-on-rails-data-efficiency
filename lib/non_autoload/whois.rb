1f93ee2f26b941c370d7d0e6edd1b149457c7982
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-09 12:44:25 -0700

    always use local tor

--- lib/non_autoload/whois.rb
+++ lib/non_autoload/whois.rb
@@ -3,11 +3,20 @@ Whois::Server::SocketHandler.class_eval do
   alias_method :old_execute, :execute
   
   def execute(query, *args)
-    proxy = Tor.next_proxy
-    proxy.last_used = DateTime.now
-    proxy.save
     
-    TCPSocket::socks_server = proxy.private_ip
+    if Rails.env.production?
+      proxy = Tor.next_proxy
+      proxy.last_used = DateTime.now
+      ip = proxy.private_ip
+      proxy.save
+    else
+      ip = '127.0.0.1'
+    end
+      
+    
+    
+    
+    TCPSocket::socks_server = ip
     TCPSocket::socks_port = 9050
 
     old_execute(query, *args)
