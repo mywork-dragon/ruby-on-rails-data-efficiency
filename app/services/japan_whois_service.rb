4ea1957ef4d815eb30241e5bd79c251bcd4e1112
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-08 23:17:55 -0700

    final fix

--- app/services/japan_whois_service.rb
+++ app/services/japan_whois_service.rb
@@ -7,7 +7,7 @@ class JapanWhoisService
       JpIosAppSnapshot.find_each.with_index do |ss, index|
         li "App ##{index}" if index%10000 == 0
       
-        JapanWhoisServiceWorker.perform(ss.id)
+        JapanWhoisServiceWorker.perform_async(ss.id)
       end
       
     end
