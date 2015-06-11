8cea31ca6c2d646b9545075f55bb62b14dda98f1
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-11 14:09:14 -0700

    add wildcard to beginning of iOS app search as well

--- app/services/filter_service.rb
+++ app/services/filter_service.rb
@@ -202,7 +202,7 @@ class FilterService
     def ios_app_keywords_query(keywords)
       # name_query_array = keywords.map{|k| "ios_app_snapshots.name LIKE \"%#{k}%\""}
       name_query_array = keywords.map{|k| "ios_app_snapshots.name LIKE ?"}
-      keywords_with_quotes = keywords.map{|k| "\"#{k}%\""}
+      keywords_with_quotes = keywords.map{|k| "\"%#{k}%\""}
       "joins(:newest_ios_app_snapshot).where(\'#{name_query_array.join(' OR ')}\', #{keywords_with_quotes.join(',')})"
     end
     
