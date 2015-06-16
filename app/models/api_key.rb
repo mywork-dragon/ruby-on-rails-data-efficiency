01a5b75770a66ce5652cecccbe1b1647264b3cff
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-15 17:58:41 -0700

    API Key

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,6 +1,26 @@
 class ApiKey < ActiveRecord::Base
 
+  validates :key, presence: true
+  validates :key, uniqueness: true
+
   has_many :accounts_api_keys
   has_many :accounts, through: :accounts_api_keys
+
+  class << self
+    
+    # Create a unique API key
+    # @author Jason Lew
+    def create!
+      loop do 
+        key = SecureRandom.urlsafe_base64
+        if !ApiKey.find_by_key(key)
+          super(key: key)
+          break
+        end
+      end 
+      
+    end
+    
+  end
 
 end
