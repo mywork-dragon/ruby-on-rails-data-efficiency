b1747b6d09fd2908a374567175b5a1ac52c8ded9
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-15 18:07:00 -0700

    stub customer api controller

---
+++ app/controllers/customer_api_controller.rb
@@ -0,0 +1,14 @@
+# The controller for the API that we give to our customers
+# @author Jason Lew
+class CustomerApiController < ApplicationController
+
+  def ios_apps
+  end
+  
+  def android_apps
+  end
+  
+  def companies
+  end
+
+end
---
+++ test/helpers/customer_api_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CustomerApiHelperTest < ActionView::TestCase
+end
