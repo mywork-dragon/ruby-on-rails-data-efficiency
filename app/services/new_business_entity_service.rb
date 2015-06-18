9c65d84b561c0ec749e7c260a27bfca957c3ecb9
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-18 00:23:47 -0700

    separate method

--- app/services/new_business_entity_service.rb
+++ app/services/new_business_entity_service.rb
@@ -121,17 +121,22 @@ class NewBusinessEntityService
         next if dasi.blank?
         
         if dasi
-          ios_developer = IosDeveloper.find_or_create_by_identifier(developer_app_store_identifier)
+          ios_developer = IosDeveloper.find_by_identifier(developer_app_store_identifier)
+          ios_developer = IosDeveloper.create(identifier: dasi, name: ss.seller) if ios_developer.nil?
           c = Company.find_by_app_store_identifier(dasi)
         end 
         
         # 2. Link the app to the developer if it's not already
         ios_app.ios_developer = ios_developer if ios_app.ios_developer.blank?
         
-        
-        
-        
+        # 3.
+        link_developer_to_company_and_add_websites
       end
+      
+    end
+    
+    def link_developer_to_company_and_add_websites
+      # 1
       
     end
     
