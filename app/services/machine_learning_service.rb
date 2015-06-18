eaa2cb0a01cb4719afdf814d2aa1ceb741155345
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-17 17:14:26 -0700

    doesn't work yet

--- app/services/machine_learning_service.rb
+++ app/services/machine_learning_service.rb
@@ -41,11 +41,18 @@ class MachineLearningService
     training_ss_ids = [3756292, 3635886, 3690092, 3312993, 3596641, 3390829, 3547939, 2961514, 3474788, 2969821]
     labels = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
     
-    examples = training_ss_ids.map do |id|
+    examples_a = training_ss_ids.map do |ss_id|
       vector_from_ss_id(ss_id)
     end
     
+    # puts "labels: #{labels}"
+    # puts "examples: #{examples}"
+    
+    examples = examples_a.map {|ary| Libsvm::Node.features(ary) }
+    
     problem.set_examples(labels, examples)
+    
+    return
     
     model = Libsvm::Model.train(problem, parameter)
     
@@ -57,18 +64,24 @@ class MachineLearningService
   end
   
   def vector_from_ss_id(ss_id)
-    ss = IosAppSnapshot.find(id)
+    ss = IosAppSnapshot.find(ss_id)
     
     appearances = IosAppSnapshot.where(ios_app_snapshot_job_id: ss.ios_app_snapshot_job_id, seller_url: ss.seller_url, developer_app_store_identifier: ss.developer_app_store_identifier).count
     total = IosAppSnapshot.where(ios_app_snapshot_job_id: ss.ios_app_snapshot_job_id, developer_app_store_identifier: ss.developer_app_store_identifier).count
     percentage_other_apps_same_website = (appearances.to_f)/(total.to_f)
     
-    contact_support_link_same = UrlHelper.url_with_domain_only(ss.seller_url) == UrlHelper.url_with_domain_only(ss.support_url) ? 1 : 0
+    contact_support_link_same = nil
+    if ss.seller_url.blank? || ss.support_url.blank?
+      contact_support_link_same = 0
+    else
+      contact_support_link_same = UrlHelper.url_with_domain_only(ss.seller_url) == UrlHelper.url_with_domain_only(ss.support_url) ? 1 : 0
+    end
     
     reviews = ss.ratings_all_count/5.0e6
     
     white = Text::WhiteSimilarity.new
-    company_name_in_domain_percentage = white.similarity(ss.seller, UrlHelper.url_with_domain_only(ss.seller_url))
+    company_name_in_domain_percentage = 0
+    company_name_in_domain_percentage = white.similarity(ss.seller, UrlHelper.url_with_domain_only(ss.seller_url)) unless ss.seller_url.blank?
     
     create_vector(percentage_other_apps_same_website: percentage_other_apps_same_website, reviews: reviews, contact_support_link_same: contact_support_link_same, company_name_in_domain_percentage: company_name_in_domain_percentage)
   end
@@ -77,7 +90,7 @@ class MachineLearningService
   #scale reviews down by 5M
   
   def create_vector(percentage_other_apps_same_website:, reviews:, contact_support_link_same:, company_name_in_domain_percentage:)
-    [percentage_other_apps_same_website, reviews, contact_support_link_same, company_name_in_domain_percentage]
+    [percentage_other_apps_same_website, reviews, contact_support_link_same, company_name_in_domain_percentage].map{ |x| x.to_f }
   end
 
   class << self
