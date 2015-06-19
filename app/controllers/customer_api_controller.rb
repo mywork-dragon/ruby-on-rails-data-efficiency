ce4ea0fa9fbc0a18c59f5d5a6f00ee1806e63500
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-18 22:03:55 -0700

    2 endpoints

--- app/controllers/customer_api_controller.rb
+++ app/controllers/customer_api_controller.rb
@@ -13,53 +13,83 @@ class CustomerApiController < ApplicationController
   # end
 
   def ios_apps
-    # appId = params['id']
-    # ios_app = IosApp.find(appId)
-    # company = ios_app.get_company #could be nil, if no websites, or websites don't have company
-    # newest_app_snapshot = ios_app.newest_ios_app_snapshot
-    # newest_download_snapshot = ios_app.get_newest_download_snapshot
-    # app_json = {
-    #   id: appId,
-    #   name: newest_app_snapshot.present? ? newest_app_snapshot.name : nil,
-    #   mobilePriority: ios_app.mobile_priority,
-    #   adSpend: ios_app.ios_fb_ad_appearances.present?,
-    #   countriesDeployed: nil, #not part of initial launch
-    #   # downloads: newest_download_snapshot.present? ? newest_download_snapshot.downloads : nil,
-    #   userBase: ios_app.user_base,
-    #   lastUpdated: newest_app_snapshot.present? ? newest_app_snapshot.released.to_s : nil,
-    #   appIdentifier: ios_app.app_identifier,
-    #   appIcon: {
-    #     large: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_350x350 : nil,
-    #     small: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_175x175 : nil
-    #   },
-    #   company: {
-    #     name: company.present? ? company.name : nil,
-    #     id: company.present? ? company.id : nil,
-    #     fortuneRank: company.present? ? company.fortune_1000_rank : nil,
-    #     funding: company.present? ? company.funding : nil,
-    #     websites: ios_app.get_website_urls,
-    #     location: {
-    #       streetAddress: company.present? ? company.street_address : nil,
-    #       city: company.present? ? company.city : nil,
-    #       zipCode: company.present? ? company.zip_code : nil,
-    #       state: company.present? ? company.state : nil,
-    #       country: company.present? ? company.country : nil
-    #     }
-    #   }
-    # }
-    # render json: app_json
+    id = params['id']
     
-    puts params['id']
-    
-    render json: {'dummy' => 'show'}
+    ios_app = IosApp.find(id)
+    company = ios_app.get_company #could be nil, if no websites, or websites don't have company
+    newest_app_snapshot = ios_app.newest_ios_app_snapshot
+    newest_download_snapshot = ios_app.get_newest_download_snapshot
+    app_json = {
+      id: id,
+      name: newest_app_snapshot.present? ? newest_app_snapshot.name : nil,
+      mobilePriority: ios_app.mobile_priority,
+      adSpend: ios_app.ios_fb_ad_appearances.present?,
+      countriesDeployed: nil, #not part of initial launch
+      # downloads: newest_download_snapshot.present? ? newest_download_snapshot.downloads : nil,
+      userBase: ios_app.user_base,
+      lastUpdated: newest_app_snapshot.present? ? newest_app_snapshot.released.to_s : nil,
+      appIdentifier: ios_app.app_identifier,
+      appIcon: {
+        large: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_350x350 : nil,
+        small: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_175x175 : nil
+      },
+      company: {
+        name: company.present? ? company.name : nil,
+        id: company.present? ? company.id : nil,
+        fortuneRank: company.present? ? company.fortune_1000_rank : nil,
+        funding: company.present? ? company.funding : nil,
+        websites: ios_app.get_website_urls,
+        location: {
+          streetAddress: company.present? ? company.street_address : nil,
+          city: company.present? ? company.city : nil,
+          zipCode: company.present? ? company.zip_code : nil,
+          state: company.present? ? company.state : nil,
+          country: company.present? ? company.country : nil
+        }
+      }
+    }
+    render json: app_json
   end
   
   def android_apps
-    render json: {'dummy' => 'show'}
+    id = params['id']
+    android_app = AndroidApp.find(id)
+    company = android_app.get_company
+    newest_app_snapshot = android_app.newest_android_app_snapshot
+    
+    app_json = {
+      id: id,
+      name: newest_app_snapshot.present? ? newest_app_snapshot.name : nil,
+      mobilePriority: android_app.mobile_priority, 
+      adSpend: android_app.android_fb_ad_appearances.present?, 
+      countriesDeployed: nil, #not part of initial launch
+      downloads: newest_app_snapshot.present? ? "#{newest_app_snapshot.downloads_min}-#{newest_app_snapshot.downloads_max}" : nil,
+      lastUpdated: newest_app_snapshot.present? ? newest_app_snapshot.released : nil,
+      appIdentifier: android_app.app_identifier,
+      appIcon: {
+        large: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_300x300 : nil
+        # 'small' => newest_app_snapshot.present? ? newest_app_snapshot.icon_url_175x175 : nil
+      },
+      company: {
+        name: company.present? ? company.name : nil,
+        id: company.present? ? company.id : nil,
+        fortuneRank: company.present? ? company.fortune_1000_rank : nil, 
+        funding: company.present? ? company.funding : nil,
+        websites: android_app.get_website_urls, #this is an array
+        location: {
+          streetAddress: company.present? ? company.street_address : nil,
+          city: company.present? ? company.city : nil,
+          zipCode: company.present? ? company.zip_code : nil,
+          state: company.present? ? company.state : nil,
+          country: company.present? ? company.country : nil
+        }
+      }
+    }
+    render json: app_json
   end
   
   def companies
-    render json: {'dummy' => 'show'}
+    
   end
 
 end
