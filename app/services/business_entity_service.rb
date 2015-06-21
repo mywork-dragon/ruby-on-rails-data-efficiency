d9d61e6d8cf080a215f26b7ae2debe14cac2db19
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-21 00:03:00 -0700

    run all apps

--- app/services/business_entity_service.rb
+++ app/services/business_entity_service.rb
@@ -10,7 +10,14 @@ class BusinessEntityService
         BusinessEntityIosServiceWorker.new.perform(ids)
     end
 
-
+    def run_ios_by_app
+      IosApp.find_in_batches(batch_size: 1000).with_index do |batch, index|
+        li "Batch #{index}"
+        ios_app_snapshot_ids = batch.map{|ia| ia.newest_ios_app_snapshot_id}.select{ |id| id.present?}
+        
+        BusinessEntityIosServiceWorker.perform_async(ios_app_snapshot_ids)
+      end
+    end
   
     def run_ios(ios_app_snapshot_job_ids)
       IosAppSnapshot.where(ios_app_snapshot_job_id: ios_app_snapshot_job_ids).find_in_batches(batch_size: 1000).with_index do |batch, index|
