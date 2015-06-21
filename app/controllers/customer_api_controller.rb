0e3010d21072163b731faf68cc9fb7220ab0e300
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-20 21:00:26 -0700

    require mix panel gem

--- app/controllers/customer_api_controller.rb
+++ app/controllers/customer_api_controller.rb
@@ -1,3 +1,5 @@
+require 'mixpanel-ruby'
+
 # The controller for the API that we give to our customers
 # Not to be confused with ApiController, which is our internal API to communicated with the frontend
 # @author Jason Lew
