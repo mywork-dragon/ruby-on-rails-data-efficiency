845c1493fbdcb301c6bc1899898177a1693deb79
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-21 18:18:34 -0700

    test critical queue

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -1,9 +1,13 @@
 class TestWorker
   include Sidekiq::Worker
+  
+  sidekiq_options :queue => :critical
 
   def perform(string)
-    li "test_li, #{string}"
-    SidekiqTester.create!(test_string: string, ip: MyIp.ip)
+    # li "test_li, #{string}"
+#     SidekiqTester.create!(test_string: string, ip: MyIp.ip)
+    
+    SidekiqTester.create!(test_string: "critical")
   end
   
 end
