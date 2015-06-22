3f3e48603d500a2931e098f9d411b4b1900525f6
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-22 16:14:41 -0700

    fix

--- app/controllers/customer_api_controller.rb
+++ app/controllers/customer_api_controller.rb
@@ -27,9 +27,9 @@ class CustomerApiController < ApplicationController
     begin
       app_identifier = params['appStoreId']
       properties = {'app_identifier' => app_identifier.to_s}
-    
+
       ios_app = IosApp.find_by_app_identifier(app_identifier)
-      
+
       if ios_app.blank?
         app_json = {}
       else
@@ -70,29 +70,29 @@ class CustomerApiController < ApplicationController
     ensure
       track('ios_apps', properties)
     end
-    
+
   end
-  
+
   def android_apps
     begin
       app_identifier = params['googlePlayId']
       properties = {'app_identifier' => app_identifier.to_s}
-      
+
       track('android_apps', 'app_identifier' => app_identifier.to_s)
-    
+
       android_app = AndroidApp.find_by_app_identifier(app_identifier)
-      
+
       if android_app.blank?
         app_json = {}
       else
         company = android_app.get_company
         newest_app_snapshot = android_app.newest_android_app_snapshot
-    
+
         app_json = {
           mightySignalId: android_app.id,
           name: newest_app_snapshot.present? ? newest_app_snapshot.name : nil,
-          mobilePriority: android_app.mobile_priority, 
-          adSpend: android_app.android_fb_ad_appearances.present?, 
+          mobilePriority: android_app.mobile_priority,
+          adSpend: android_app.android_fb_ad_appearances.present?,
           downloadsEstimate: newest_app_snapshot.present? ? (newest_app_snapshot.downloads_max -  newest_app_snapshot.downloads_min)/2.0 : nil,
           downloadsMin: newest_app_snapshot.present? ? newest_app_snapshot.downloads_min : nil,
           downloadsMax: newest_app_snapshot.present? ? newest_app_snapshot.downloads_max : nil,
@@ -102,8 +102,8 @@ class CustomerApiController < ApplicationController
           company: {
             name: company.present? ? company.name : nil,
             mightySignalId: company.present? ? company.id : nil,
-            fortuneRank: company.present? ? company.fortune_1000_rank : nil, 
-            websites: android_app.get_website_urls, #this is an array
+            fortuneRank: company.present? ? company.fortune_1000_rank : nil,
+            websites: android_app.get_website_urls,
             location: {
               streetAddress: company.present? ? company.street_address : nil,
               city: company.present? ? company.city : nil,
@@ -124,25 +124,25 @@ class CustomerApiController < ApplicationController
     ensure
       track('android_apps', properties)
     end
-    
+
   end
-  
+
   def companies
     begin
       url = params['website']
       properties = {'website' => url.to_s}
-    
+
       website = Website.find_by_url(url)
-      
+
       if website.blank?
         company_h = {}
       else
         company = website.company
-    
+
         company_h = {
           name: company.present? ? company.name : nil,
           mightySignalId: company.present? ? company.id : nil,
-          fortuneRank: company.present? ? company.fortune_1000_rank : nil, 
+          fortuneRank: company.present? ? company.fortune_1000_rank : nil,
           location: {
             streetAddress: company.present? ? company.street_address : nil,
             city: company.present? ? company.city : nil,
@@ -151,15 +151,15 @@ class CustomerApiController < ApplicationController
             country: company.present? ? company.country : nil
           }
         }
-    
+
         # jlew -- look at all sibling websites
-    
+
         #ios_apps = IosAppsWebsite.where(website_id: website.id).map(&:ios_app_id).map{ |ios_app_id| IosApp.find(ios_app_id)}
         ios_apps = company.websites.map{ |website| website.ios_apps} #goes up to company, then down to all apps
-    
+
         ios_apps_a = ios_apps.map do |ios_app|
           newest_app_snapshot = ios_app.newest_ios_app_snapshot
-      
+
           {
             mightySignalId: ios_app.id,
             name: newest_app_snapshot.present? ? newest_app_snapshot.name : nil,
@@ -168,37 +168,34 @@ class CustomerApiController < ApplicationController
             userBase: ios_app.user_base,
             lastUpdated: newest_app_snapshot.present? ? newest_app_snapshot.released.to_s : nil,
             appIdentifier: ios_app.app_identifier,
-            appIcon: {
-              large: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_350x350 : nil,
-              small: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_175x175 : nil
-            }
+            appIconLarge: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_350x350 : nil,
+            appIconSmall: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_175x175 : nil
           }
         end
-    
-        #android_apps = AndroidAppsWebsite.where(website_id: website.id).map(&:android_app_id).map{ |android_app_id| AndroidApp.find(android_app_id)} 
+
+        #android_apps = AndroidAppsWebsite.where(website_id: website.id).map(&:android_app_id).map{ |android_app_id| AndroidApp.find(android_app_id)}
         android_apps = company.websites.map{ |website| website.android_apps}  #goes up to company, then down to all apps
-    
+
         android_apps_a = android_apps.map do |android_app|
           newest_app_snapshot = android_app.newest_android_app_snapshot
-      
+
           {
             mightySignalId: android_app.id,
             name: newest_app_snapshot.present? ? newest_app_snapshot.name : nil,
-            mobilePriority: android_app.mobile_priority, 
-            adSpend: android_app.android_fb_ad_appearances.present?, 
+            mobilePriority: android_app.mobile_priority,
+            adSpend: android_app.android_fb_ad_appearances.present?,
             downloadsEstimate: newest_app_snapshot.present? ? (newest_app_snapshot.downloads_max -  newest_app_snapshot.downloads_min)/2.0 : nil,
             downloadsMin: newest_app_snapshot.present? ? newest_app_snapshot.downloads_min : nil,
             downloadsMax: newest_app_snapshot.present? ? newest_app_snapshot.downloads_max : nil,
             lastUpdated: newest_app_snapshot.present? ? newest_app_snapshot.released : nil,
             googlePlayId: android_app.app_identifier,
             appIconLarge: newest_app_snapshot.present? ? newest_app_snapshot.icon_url_300x300 : nil
-            }
           }
         end
-    
+
         company_json = {company: company_h, apps: {ios_apps: ios_apps_a, android_apps: android_apps_a}}
       end
-    
+
     rescue => e
       render json: json_failure
       merge_failure!(properties, company_json, e)
@@ -208,7 +205,7 @@ class CustomerApiController < ApplicationController
     ensure
       track('companies', properties)
     end
-    
+
   end
   
   protected
@@ -216,7 +213,7 @@ class CustomerApiController < ApplicationController
   def merge_failure!(properties, response, exception)
     properties.merge!('status_code' => '500', 'exception' => {'message' => exception.message, 'backtrace' => exception.backtrace}, 'response' => response)
   end
-  
+
   def merge_success!(properties, response)
     properties.merge!('status_code' => '400', 'response' => response)
   end
