c963431a9914cb0c1c3fa7ff042dd6e35af477a5
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-25 01:38:50 -0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621025144) do
+ActiveRecord::Schema.define(version: 20150625083704) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
@@ -265,6 +265,16 @@ ActiveRecord::Schema.define(version: 20150621025144) do
   add_index "companies", ["google_play_identifier"], name: "index_google_play_identifier", using: :btree
   add_index "companies", ["status"], name: "index_companies_on_status", using: :btree
   add_index "companies", ["website"], name: "index_companies_on_website", unique: true, using: :btree
+
+  create_table "dupes", force: true do |t|
+    t.string   "app_identifier"
+    t.boolean  "duped"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "dupes", ["app_identifier"], name: "index_dupes_on_app_identifier", using: :btree
+  add_index "dupes", ["duped"], name: "index_dupes_on_duped", using: :btree
 
   create_table "google_accounts", force: true do |t|
     t.string   "email"
