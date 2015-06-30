9c74ae47f952127b3869f40092ee4e156726e83e
mywork-dragon <dragonblueyounger@gmail.com>
2015-06-30 10:12:49 -0700

    Company Page Table Wired Up

--- public/app/app/scripts/controllers/company-details.js
+++ public/app/app/scripts/controllers/company-details.js
@@ -2,56 +2,60 @@
 
 angular.module('appApp').controller("CompanyDetailsCtrl", ["$scope", "$http", "$routeParams", "$window", "pageTitleService", "$rootScope",
   function($scope, $http, $routeParams, $window, pageTitleService, $rootScope) {
-  $scope.load = function() {
+    $scope.load = function() {
 
-    return $http({
-      method: 'GET',
-      url: API_URI_BASE + 'api/get_company',
-      params: {id: $routeParams.id}
-    }).success(function(data) {
-      $scope.companyData = data;
-      /*
-      console.log(data.iosApps, data.androidApps);
-      if(data.iosApps) {$rootScope.apps.push(data.iosApps);}
-      if(data.androidApps) {$rootScope.apps.push(data.androidApps);}
+      $scope.queryInProgress = true;
 
-      /* Sets html title attribute */
-      pageTitleService.setTitle($scope.companyData.name);
-    });
-  };
+      return $http({
+        method: 'GET',
+        url: API_URI_BASE + 'api/get_company',
+        params: {id: $routeParams.id}
+      }).success(function(data) {
+        pageTitleService.setTitle(data.name);
+        $scope.companyData = data;
+        var companyApps = data.iosApps.concat(data.androidApps);
+        $scope.apps = companyApps;
+        $scope.numApps = companyApps.length;
+        $scope.queryInProgress = false;
+        console.log('YO', companyApps, $rootScope.apps);
+        /* Sets html title attribute */
+      }).error(function() {
+        $scope.queryInProgress = false;
+      });
+    };
 
-  /* LinkedIn Link Button Logic */
-  $scope.onLinkedinButtonClick = function(linkedinLinkType) {
-    var linkedinLink = "";
+    /* LinkedIn Link Button Logic */
+    $scope.onLinkedinButtonClick = function(linkedinLinkType) {
+      var linkedinLink = "";
 
-    if (linkedinLinkType == 'company') {
-      linkedinLink = "https://www.linkedin.com/vsearch/c?keywords=" + encodeURI($scope.companyData.name);
-    } else {
-      linkedinLink = "https://www.linkedin.com/vsearch/p?keywords=" + linkedinLinkType + "&company=" + encodeURI($scope.companyData.name);
-    }
+      if (linkedinLinkType == 'company') {
+        linkedinLink = "https://www.linkedin.com/vsearch/c?keywords=" + encodeURI($scope.companyData.name);
+      } else {
+        linkedinLink = "https://www.linkedin.com/vsearch/p?keywords=" + linkedinLinkType + "&company=" + encodeURI($scope.companyData.name);
+      }
+
+      /* -------- Mixpanel Analytics Start -------- */
+      mixpanel.track(
+        "LinkedIn Link Clicked", {
+          "companyName": $scope.companyData.name,
+          "companyPosition": linkedinLinkType
+        }
+      );
+      /* -------- Mixpanel Analytics End -------- */
+
+      $window.open(linkedinLink, '_blank');
+    };
+
+    $scope.load();
 
     /* -------- Mixpanel Analytics Start -------- */
     mixpanel.track(
-      "LinkedIn Link Clicked", {
-        "companyName": $scope.companyData.name,
-        "companyPosition": linkedinLinkType
+      "Page Viewed", {
+        "pageType": "Company",
+        "companyid": $routeParams.id,
+        "appPlatform": APP_PLATFORM
       }
     );
     /* -------- Mixpanel Analytics End -------- */
-
-    $window.open(linkedinLink, '_blank');
-  };
-
-  $scope.load();
-
-  /* -------- Mixpanel Analytics Start -------- */
-  mixpanel.track(
-    "Page Viewed", {
-      "pageType": "Company",
-      "companyid": $routeParams.id,
-      "appPlatform": APP_PLATFORM
-    }
-  );
-  /* -------- Mixpanel Analytics End -------- */
-}
+  }
 ]);
