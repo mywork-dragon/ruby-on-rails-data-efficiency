8d706f798d18806e489e806f45defa4eca88addc
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-01 16:36:26 -0700

    disabled timeout and increase open_timeout to 30 seconds

--- lib/non_autoload/apk_downloader.rb
+++ lib/non_autoload/apk_downloader.rb
@@ -65,7 +65,7 @@ if defined?(ApkDownloader)
       login_http.use_ssl = true
       login_http.ssl_version="SSLv3"
       login_http.verify_mode  = OpenSSL::SSL::VERIFY_NONE
-      login_http.open_timeout = 10
+      login_http.open_timeout = 30
 
       post = Net::HTTP::Post.new LoginUri.to_s
       post.set_form_data params
@@ -98,52 +98,52 @@ if defined?(ApkDownloader)
       http.use_ssl = (url.scheme == 'https')
       http.ssl_version="SSLv3"
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      http.open_timeout = 10
+      http.open_timeout = 30
 
       req = Net::HTTP::Get.new url.to_s
       req['Accept-Encoding'] = ''
       req['User-Agent'] = 'AndroidDownloadManager/4.1.1 (Linux; U; Android 4.1.1; Nexus S Build/JRO03E)'
       req['Cookie'] = [cookie.name, cookie.value].join('=')
 
-      max time
-      response = http.request_head(url)
-      file_size = response['content-length'].to_i
-      max_time = if file_size > 0 then (file_size/10000) / 10 else 300 end
+      # max time
+      # response = http.request_head(url)
+      # file_size = response['content-length'].to_i
+      # max_time = if file_size > 0 then (file_size/10000) / 10 else 300 end
 
       resp = http.request req
 
-      # case resp
-      # when Net::HTTPSuccess
-      #   return resp
-      # when Net::HTTPRedirection
-      #   return recursive_apk_fetch(URI(resp['Location']), cookie, tries - 1)
-      # else
-      #   resp.error!
-      # end
-
-      begin
-        timeout(max_time) do
-          req = Net::HTTP::Get.new url.to_s
-          req['Accept-Encoding'] = ''
-          req['User-Agent'] = 'AndroidDownloadManager/4.1.1 (Linux; U; Android 4.1.1; Nexus S Build/JRO03E)'
-          req['Cookie'] = [cookie.name, cookie.value].join('=')
-
-          resp = http.request req
-
-          case resp
-          when Net::HTTPSuccess
-            return resp
-          when Net::HTTPRedirection
-            return recursive_apk_fetch(URI(resp['Location']), cookie, tries - 1)
-          else
-            resp.error!
-          end
-        end
-      rescue => e
-        # ApkSnapshotException.create(name: e.message, backtrace: e.backtrace, try: @try)
-        http.finish
+      case resp
+      when Net::HTTPSuccess
+        return resp
+      when Net::HTTPRedirection
         return recursive_apk_fetch(URI(resp['Location']), cookie, tries - 1)
+      else
+        resp.error!
       end
+
+      # begin
+      #   timeout(max_time) do
+      #     req = Net::HTTP::Get.new url.to_s
+      #     req['Accept-Encoding'] = ''
+      #     req['User-Agent'] = 'AndroidDownloadManager/4.1.1 (Linux; U; Android 4.1.1; Nexus S Build/JRO03E)'
+      #     req['Cookie'] = [cookie.name, cookie.value].join('=')
+
+      #     resp = http.request req
+
+      #     case resp
+      #     when Net::HTTPSuccess
+      #       return resp
+      #     when Net::HTTPRedirection
+      #       return recursive_apk_fetch(URI(resp['Location']), cookie, tries - 1)
+      #     else
+      #       resp.error!
+      #     end
+      #   end
+      # rescue => e
+      #   # ApkSnapshotException.create(name: e.message, backtrace: e.backtrace, try: @try)
+      #   http.finish
+      #   return recursive_apk_fetch(URI(resp['Location']), cookie, tries - 1)
+      # end
 
 
     end
@@ -158,7 +158,7 @@ if defined?(ApkDownloader)
         @http.use_ssl = true
         @http.ssl_version="SSLv3"
         @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        @http.open_timeout = 10
+        @http.open_timeout = 30
       end
 
       api_headers = {
