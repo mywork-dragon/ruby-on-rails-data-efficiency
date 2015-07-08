cfbdad5cc045334674346b92254bf6fd61ad7810
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-08 13:46:38 -0700

    no https and reasonable open_timeout for login and api_request

--- lib/non_autoload/apk_downloader.rb
+++ lib/non_autoload/apk_downloader.rb
@@ -97,7 +97,7 @@ if defined?(ApkDownloader)
       # login_http.use_ssl = true
       # login_http.ssl_version="SSLv3"
       # login_http.verify_mode  = OpenSSL::SSL::VERIFY_NONE
-      # login_http.open_timeout = 500
+      login_http.open_timeout = 30
 
       post = Net::HTTP::Post.new LoginUri.to_s
       post.set_form_data params
@@ -160,7 +160,7 @@ if defined?(ApkDownloader)
         # @http.use_ssl = true
         # @http.ssl_version="SSLv3"
         # @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        # @http.open_timeout = 500
+        @http.open_timeout = 30
       end
 
       api_headers = {
