2e719c2ae79c5abde294bb4249309d8adad9c895
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-07 22:30:32 -0700

    save

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -646,6 +646,7 @@ class ApiController < ApplicationController
               if website
                 clearbit_contact = ClearbitContact.create(website_id: website.id)
                 clearbit_contact.update(website_id: website.id, clearbit_id: contact_id, given_name: contact_given_name, family_name: contact_family_name, full_name: contact_full_name, title: contact_title, email: contact_email, linkedin: contact_linkedin)
+                clearbit_contact.save
               end
             end
 
