e7e943f5b101ef6167d8c653a8c123dde3747f49
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-08 23:47:34 -0700

    35 on super scraper

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :sidekiq_log, '/home/deploy/sidekiq.log'
 set :sidekiq_pid, '/home/deploy/sidekiq.pid'
 
 set :scraper_concurrency, 50
-set :super_scraper, 50
+set :super_scraper_concurrency, 35
 
 set :sidekiq_queue, %w(critical default low)
 
