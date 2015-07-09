89a79395b19914e094d8061dd02d0f4a7ca220bc
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-09 03:10:03 -0700

    concurrency 50

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ set :sidekiq_role, [:scraper, :super_scraper]
 set :sidekiq_log, '/home/deploy/sidekiq.log'
 set :sidekiq_pid, '/home/deploy/sidekiq.pid'
 
-set :sidekiq_concurrency, 3
+set :sidekiq_concurrency, 50
 
 # set :scraper_concurrency, 50
 # set :super_scraper_concurrency, 35
