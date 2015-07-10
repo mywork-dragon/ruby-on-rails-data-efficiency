c51848288b27c1ea97392a99868390d17c148cf7
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-10 15:12:00 -0700

    http patch

--- lib/non_autoload/http.rb
+++ lib/non_autoload/http.rb
@@ -1,82 +1,62 @@
-# require 'net/protocol'
-# require 'uri'
+require 'net/protocol'
+require 'uri'
 
-# module Net
+module Net
 
-#   class HTTP < Protocol
+  class HTTP < Protocol
 
-#     def connect
-#       if proxy? then
-#         conn_address = proxy_address
-#         conn_port    = proxy_port
-#       else
-#         conn_address = address
-#         conn_port    = port
-#       end
+    def initialize(address, port = nil)
+      @address = address
+      @port    = (port || HTTP.default_port)
+      @local_host = nil
+      @local_port = nil
+      @curr_http_version = HTTPVersion
+      @keep_alive_timeout = 2
+      # I changed last_communicated from `nil` to `false`
+      @last_communicated = false
+      @close_on_empty_response = false
+      @socket  = nil
+      @started = false
+      @open_timeout = nil
+      @read_timeout = 60
+      @continue_timeout = nil
+      @debug_output = nil
 
-#       D "opening connection to #{conn_address}:#{conn_port}..."
-#       s = Timeout.timeout(@open_timeout, Net::OpenTimeout) {
-#         TCPSocket.open(conn_address, conn_port, @local_host, @local_port)
-#       }
-#       s.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
-#       D "opened"
-#       if use_ssl?
-#         ssl_parameters = Hash.new
-#         iv_list = instance_variables
-#         SSL_IVNAMES.each_with_index do |ivname, i|
-#           if iv_list.include?(ivname) and
-#             value = instance_variable_get(ivname)
-#             ssl_parameters[SSL_ATTRIBUTES[i]] = value if value
-#           end
-#         end
-#         @ssl_context = OpenSSL::SSL::SSLContext.new
-#         @ssl_context.set_params(ssl_parameters)
-#         D "starting SSL for #{conn_address}:#{conn_port}..."
-#         s = OpenSSL::SSL::SSLSocket.new(s, @ssl_context)
-#         s.sync_close = true
-#         D "SSL established"
-#       end
-#       @socket = BufferedIO.new(s)
-#       @socket.read_timeout = @read_timeout
-#       @socket.continue_timeout = @continue_timeout
-#       @socket.debug_output = @debug_output
-#       if use_ssl?
-#         begin
-#           if proxy?
-#             buf = "CONNECT #{@address}:#{@port} HTTP/#{HTTPVersion}\r\n"
-#             buf << "Host: #{@address}:#{@port}\r\n"
-#             if proxy_user
-#               credential = ["#{proxy_user}:#{proxy_pass}"].pack('m')
-#               credential.delete!("\r\n")
-#               buf << "Proxy-Authorization: Basic #{credential}\r\n"
-#             end
-#             buf << "\r\n"
-#             @socket.write(buf)
-#             HTTPResponse.read_new(@socket).value
-#           end
-#           s.session = @ssl_session if @ssl_session
-#           # Server Name Indication (SNI) RFC 3546
-#           s.hostname = @address if s.respond_to? :hostname=
-#           # Timeout.timeout(@open_timeout, Net::OpenTimeout) { s.connect }
-#           s.connect
-#           if @ssl_context.verify_mode != OpenSSL::SSL::VERIFY_NONE
-#             s.post_connection_check(@address)
-#           end
-#           @ssl_session = s.session
-#         rescue => exception
-#           D "Conn close because of connect error #{exception}"
-#           @socket.close if @socket and not @socket.closed?
-#           raise exception
-#         end
-#       end
-#       on_connect
-#     end
-#     private :connect
+      @proxy_from_env = false
+      @proxy_uri      = nil
+      @proxy_address  = nil
+      @proxy_port     = nil
+      @proxy_user     = nil
+      @proxy_pass     = nil
 
-#     def on_connect
-#     end
-#     private :on_connect
+      @use_ssl = false
+      @ssl_context = nil
+      @ssl_session = nil
+      @enable_post_connection_check = true
+      @sspi_enabled = false
+      SSL_IVNAMES.each do |ivname|
+        instance_variable_set ivname, nil
+      end
+    end
 
-#   end
+    def end_transport(req, res)
+      @curr_http_version = res.http_version
+      # I changed last_communicated from `nil` to `false`
+      @last_communicated = false
+      if @socket.closed?
+        D 'Conn socket closed'
+      elsif not res.body and @close_on_empty_response
+        D 'Conn close'
+        @socket.close
+      elsif keep_alive?(req, res)
+        D 'Conn keep-alive'
+        @last_communicated = Time.now
+      else
+        D 'Conn close'
+        @socket.close
+      end
+    end
 
-# end
+  end
+
+end
