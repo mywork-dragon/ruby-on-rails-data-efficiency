6db13671dd7fbeaab663523bc5599b292df5369c
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-13 12:56:50 -0700

    add self

--- config/deploy/mighty_deployer.rb
+++ config/deploy/mighty_deployer.rb
@@ -9,67 +9,61 @@ module MightyDeployer
 
   puts "laksfnlaknsflkansfklansf!!!!"
 
-  module << self
+  def self.define_web_api_servers
+    web_server = '54.85.3.24'
 
-    def test
-    end
+    api_server = '52.6.191.250'
+  
+    server web_server, user: 'deploy'
 
-    def define_web_api_servers
-      web_server = '54.85.3.24'
+    server api_server, user: 'deploy'
+  
+    @app_roles += [web_server, api_server]
+    @web_roles << web_server
+    @db_roles << web_server
+    @api_roles << api_server
+  
+  end
 
-      api_server = '52.6.191.250'
-    
-      server web_server, user: 'deploy'
+  def self.define_scraper_servers
+    scraper_servers = %w(
+      52.2.192.44
+    )
+  
+    role :scraper, scraper_servers
 
-      server api_server, user: 'deploy'
-    
-      @app_roles += [web_server, api_server]
-      @web_roles << web_server
-      @db_roles << web_server
-      @api_roles << api_server
-    
+    scraper_servers.each do |scraper_server|
+      server scraper_server, user: 'deploy'
     end
   
-    def define_scraper_servers
-      scraper_servers = %w(
-        52.2.192.44
-      )
-    
-      role :scraper, scraper_servers
+    @app_roles += scraper_servers
+    @scraper_roles += scraper_servers
+  end
 
-      scraper_servers.each do |scraper_server|
-        server scraper_server, user: 'deploy'
-      end
-    
-      @app_roles += scraper_servers
-      @scraper_roles += scraper_servers
+  def self.define_sdk_scraper_servers
+    sdk_scraper_servers = %w(
+      54.164.24.87
+      54.88.39.109
+      54.86.80.102
+    )
+  
+    role :sdk_scraper, sdk_scraper_servers
+  
+    sdk_scraper_servers.each do |sdk_scraper_server|
+      server sdk_scraper_server, user: 'deploy'
     end
   
-    def define_sdk_scraper_servers
-      sdk_scraper_servers = %w(
-        54.164.24.87
-        54.88.39.109
-        54.86.80.102
-      )
-    
-      role :sdk_scraper, sdk_scraper_servers
-    
-      sdk_scraper_servers.each do |sdk_scraper_server|
-        server sdk_scraper_server, user: 'deploy'
-      end
-    
-      @app_roles += sdk_scraper_servers
-      @sdk_scraper_roles += sdk_scraper_servers
-    end
-  
-    def define_roles
-      role :app, @app_roles
-      role :web, @web_roles
-      role :api, @api_roles
-      role :db,  @db_roles #must have this do migrate db
-      role :sdk_scraper, @sdk_scraper_roles
-      role :scraper, @scraper_roles
-    end
+    @app_roles += sdk_scraper_servers
+    @sdk_scraper_roles += sdk_scraper_servers
+  end
+
+  def self.define_roles
+    role :app, @app_roles
+    role :web, @web_roles
+    role :api, @api_roles
+    role :db,  @db_roles #must have this do migrate db
+    role :sdk_scraper, @sdk_scraper_roles
+    role :scraper, @scraper_roles
   end
 
 
