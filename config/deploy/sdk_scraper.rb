a68929cf31f01404e3ffae0bf9fe68998d18812f
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-13 17:43:02 -0700

    another deploy fix

--- config/deploy/sdk_scraper.rb
+++ config/deploy/sdk_scraper.rb
@@ -1,4 +1,4 @@
 require_relative 'mighty_deployer'
 
 set :branch, 'sdk_scraper'
-MightyDeployer.deploy_to(:web_api, :scraper, :sdk_scraper)
+MightyDeployer.deploy_to([:web_api, :scraper, :sdk_scraper])
