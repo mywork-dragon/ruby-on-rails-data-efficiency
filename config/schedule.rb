339a6a8e91d4865a995197486ecf7898a06a826c
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-15 02:21:00 -0700

    remove from crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,9 +21,10 @@
 
 set :output, "/home/deploy/cron.log"
 
-every 1.minutes, roles: [:scraper] do
-  runner "SidekiqTester.say_hi"
-end
+#this works! keep as a sample
+# every 1.minutes, roles: [:scraper] do
+#   runner "SidekiqTester.say_hi"
+# end
 
 # 2.times do |i|
 #   every 1.day, :at => '1:00am' do
