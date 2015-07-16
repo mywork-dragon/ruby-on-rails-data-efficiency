28ca0484ca7a273bb7f31fb47145fb2c55c7d313
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-15 23:44:50 -0700

    cron jobs for sidekiq and cron backups

--- config/schedule.rb
+++ config/schedule.rb
@@ -26,6 +26,14 @@ set :output, "/home/deploy/cron.log"
 #   runner "SidekiqTester.say_hi"
 # end
 
+every :day, :at => '6:00am', roles: [:scraper, :sdk_scraper] do
+  command 's3cmd put /home/deploy/cron.log s3://varys-backup/cron_logs/cron_"`hostname -I`"_` date +\'%Y_%m_%d_%H_%M_%S\' `.log; cat /dev/null > /home/deploy/cron.log'
+end
+
+every :day, :at => '6:05am', roles: [:scraper, :sdk_scraper] do
+  command 's3cmd put /home/deploy/sidekiq.log s3://varys-backup/sidekiq_logs/sidekiq_"`hostname -I`"_` date +\'%Y_%m_%d_%H_%M_%S\' `.log; cat /dev/null > /home/deploy/sidekiq.log'
+end
+
 # 2.times do |i|
 #   every 1.day, :at => '1:00am' do
 #     rake "scraper:scrape_all SCRAPE_PROCESSES=1 SCRAPE_PAGE_NUMBER=#{i}"
