88df7146399cb592fe5ae4e4051aa72da16fc719
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-17 16:37:22 -0700

    data

--- app/services/slack_notification_service.rb
+++ app/services/slack_notification_service.rb
@@ -9,6 +9,13 @@ class SlackNotificationService
       HTTParty.post('https://hooks.slack.com/services/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', body: json)
     end
     
+    def done(status, options)
+      text = status.data
+    
+      json = {text: text}.to_json
+      HTTParty.post('https://hooks.slack.com/services/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', body: json)
+    end
+    
   end
   
 end
