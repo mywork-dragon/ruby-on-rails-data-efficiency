9bcad4ae17ba1ab5e66dd369792587b1414fda9f
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-17 16:54:46 -0700

    instance instead

--- app/services/slack_notification_service.rb
+++ app/services/slack_notification_service.rb
@@ -1,21 +1,19 @@
 class SlackNotificationService
   
-  class << self
-    def test
-    
-      text = "Hello SLACK!"
-    
-      json = {text: text}.to_json
-      HTTParty.post('https://hooks.slack.com/services/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', body: json)
-    end
-    
-    def done(status, options)
-      text = status.data
-    
-      json = {text: text}.to_json
-      HTTParty.post('https://hooks.slack.com/services/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', body: json)
-    end
-    
+  def test
+  
+    text = "Hello SLACK!"
+  
+    json = {text: text}.to_json
+    HTTParty.post('https://hooks.slack.com/services/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', body: json)
   end
+  
+  def done(status, options)
+    text = status.data
+  
+    json = {text: text}.to_json
+    HTTParty.post('https://hooks.slack.com/services/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', body: json)
+  end
+    
   
 end
