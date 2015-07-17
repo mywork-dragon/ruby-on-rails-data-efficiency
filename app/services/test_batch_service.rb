c39807762af0223c468d1bd04f43e57291e961ef
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-17 15:28:12 -0700

    oops

--- app/services/test_batch_service.rb
+++ app/services/test_batch_service.rb
@@ -7,7 +7,7 @@ class TestBatchService
       batch.description = "Batch description (this is optional)"
       batch.on(:complete, 'SlackNotificationService#test')
       batch.jobs do
-        test_worker.perform_async('hello!')
+        TestWorker.perform_async('hello!')
       end
       puts "Just started Batch #{batch.bid}"
     end
