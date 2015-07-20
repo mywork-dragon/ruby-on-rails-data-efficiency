0f95724d3c9088948347e283226d2e593f08bfa2
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-20 13:44:28 -0700

    patch fix

--- lib/non_autoload/apk_downloader.rb
+++ lib/non_autoload/apk_downloader.rb
@@ -1,7 +1,7 @@
 # Patch for 1.1.5
-if defined?(ApkDownloader)
-  ApkDownloader.module_eval do
-  	def download! package, destination, apk_snap_id
+module ApkDownloader
+  class << self
+    def download! package, destination, apk_snap_id
       @api ||= Api.new
       data = @api.fetch_apk_data package, apk_snap_id
       File.open(destination, 'wb') { |f| f.write data }
