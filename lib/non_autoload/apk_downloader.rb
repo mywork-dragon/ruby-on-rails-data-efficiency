b1eae574775cfa093c709765d486ad363782d51d
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-20 13:38:31 -0700

    calling correct class in patch

--- lib/non_autoload/apk_downloader.rb
+++ lib/non_autoload/apk_downloader.rb
@@ -1,6 +1,6 @@
 # Patch for 1.1.5
 if defined?(ApkDownloader)
-  ApkDownloader::Api.module_eval do
+  ApkDownloader.module_eval do
   	def download! package, destination, apk_snap_id
       @api ||= Api.new
       data = @api.fetch_apk_data package, apk_snap_id
