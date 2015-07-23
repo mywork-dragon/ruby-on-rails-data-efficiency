416d803834061ae4bbb85d56c259ec8ef1c877f7
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-23 15:07:12 -0700

    jason's migration hack

--- db/migrate/20150723205918_add_apk_access_forbidden_to_android_app_snapshot.rb
+++ db/migrate/20150723205918_add_apk_access_forbidden_to_android_app_snapshot.rb
@@ -1,6 +1,6 @@
 class AddApkAccessForbiddenToAndroidAppSnapshot < ActiveRecord::Migration
   def change
-  	add_column :android_app_snapshots, :apk_access_forbidden, :boolean
-  	add_index :android_app_snapshots, :apk_access_forbidden, name: 'index_apk_access_forbidden'
+  	add_column :android_app_snapshots, :apk_access_forbidden, :boolean unless column_exists?(:android_app_snapshots, :apk_access_forbidden)
+  	add_index :android_app_snapshots, :apk_access_forbidden, name: 'index_apk_access_forbidden' unless index_exists?(:android_app_snapshots, :apk_access_forbidden, name: 'index_apk_access_forbidden')
   end
 end
