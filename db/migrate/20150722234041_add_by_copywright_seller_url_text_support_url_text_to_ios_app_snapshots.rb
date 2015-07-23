5973cb5796bce4df83970d82b5d2714528d5ec8c
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-22 17:37:48 -0700

    oops

--- db/migrate/20150722234041_add_by_copywright_seller_url_text_support_url_text_to_ios_app_snapshots.rb
+++ db/migrate/20150722234041_add_by_copywright_seller_url_text_support_url_text_to_ios_app_snapshots.rb
@@ -1,8 +1,8 @@
 class AddByCopywrightSellerUrlTextSupportUrlTextToIosAppSnapshots < ActiveRecord::Migration
   def change
     add_column :ios_app_snapshots, :by, :string unless column_exists?(:ios_app_snapshots, :by)
-    add_column :ios_app_snapshots, :copywright, :string column_exists?(:ios_app_snapshots, :copywright)
-    add_column :ios_app_snapshots, :seller_url_text, :string column_exists?(:ios_app_snapshots, :seller_url_text)
-    add_column :ios_app_snapshots, :support_url_text, :string column_exists?(:ios_app_snapshots, :support_url_text)
+    add_column :ios_app_snapshots, :copywright, :string unless column_exists?(:ios_app_snapshots, :copywright)
+    add_column :ios_app_snapshots, :seller_url_text, :string unless column_exists?(:ios_app_snapshots, :seller_url_text)
+    add_column :ios_app_snapshots, :support_url_text, :string unless column_exists?(:ios_app_snapshots, :support_url_text)
   end
 end
