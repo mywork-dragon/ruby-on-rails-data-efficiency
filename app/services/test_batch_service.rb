16bb621fe8e5aeba52094b98e4c88e53092f2a3c
mywork-dragon <dragonblueyounger@gmail.com>
2015-07-27 14:45:12 -0700

    add notification

--- app/services/test_batch_service.rb
+++ app/services/test_batch_service.rb
@@ -6,7 +6,6 @@ class TestBatchService
       batch = Sidekiq::Batch.new
       description = 'Batch description (this is optional)'
       batch.description = description 
-      batch.on(:complete, 'SlackNotificationService#done')
       batch.on(:complete, self, description: description)
       batch.jobs do
         TestWorker.perform_async('hello!')
@@ -15,7 +14,7 @@ class TestBatchService
     end
   
     def on_complete(status, options)
-      Slackiq.notify(:main, options[:description], status, 'extra0' => 'hi', 'extra2' => 'bye')
+      Slackiq.notify(webhook_name: :main, description: options[:description], status: status, 'extra0' => 'hi', 'extra2' => 'bye')
     end
   
   end
