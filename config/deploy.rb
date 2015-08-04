c406f21acdb77401ceb7846d8a19be3f01c0bd2e
mywork-dragon <dragonblueyounger@gmail.com>
2015-08-04 00:41:50 -0700

    fix concurrency

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,13 +47,14 @@ set :sidekiq_log, '/home/deploy/sidekiq.log'
 set :sidekiq_pid, '/home/deploy/sidekiq.pid'
 
 set :sdk_scraper_concurrency, 50
+set :scraper_master_concurrencye, 50
 set :scraper_concurrency, 50
 
 # set :sidekiq_queue, %w(critical default low)
 
 set :sdk_scraper_queue, %w(sdk)
-set :scraper_queue, %w(critical default low)
 set :scraper_master_queue, %w(critical scraper_master default low)
+set :scraper_queue, %w(critical default low)
 
 set :whenever_roles, [:scraper, :sdk_scraper]
 
