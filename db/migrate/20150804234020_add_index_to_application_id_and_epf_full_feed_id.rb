dbac68c9dbe9c837811b25c16061f15ec927e111
mywork-dragon <dragonblueyounger@gmail.com>
2015-08-04 16:42:03 -0700

    add index

---
+++ db/migrate/20150804234020_add_index_to_application_id_and_epf_full_feed_id.rb
@@ -0,0 +1,5 @@
+class AddIndexToApplicationIdAndEpfFullFeedId < ActiveRecord::Migration
+  def change
+    add_index :ios_app_epf_snapshots, [:application_id, :epf_full_feed_id], name: index_application_id_and_epf_full_feed_id
+  end
+end
