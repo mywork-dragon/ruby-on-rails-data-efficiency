a3405c6f9c745591039998ff1494fed3eedd019a
mywork-dragon <dragonblueyounger@gmail.com>
2015-08-03 19:21:41 -0700

    can read lines

--- app/workers/epf_service_worker.rb
+++ app/workers/epf_service_worker.rb
@@ -18,28 +18,23 @@ class EpfServiceWorker
     record = ''
     File.foreach(file, encoding: 'UTF-8:UTF-8').with_index do |line, line_num|
        
-      if line.include?(RS)
+      record += line
+       
+      if line.end_with?(RS)
         
-        line_split = line.split(RS)
-        
-        record += line_split.first
-          
         save_application(record)
-          
-        record = line_split.last
         
-      else
-        record += line
+        record = ''
+        
       end
-      
     end
+    
+    records
   end
   
   def save_application(record)
     puts record
     puts ""
-    
-    sleep(0.5)
   end
   
 end
