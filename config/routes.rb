5878b7b3a429e817113ef4aec95a6913e51282a4
mywork-dragon <dragonblueyounger@gmail.com>
2015-08-11 12:56:49 -0700

    Routes Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'welcome#index'
-  get '/team', to: 'welcome#team'
   post 'contact_us' => 'welcome#contact_us', as: :contact_us
   post 'try_it_out' => 'welcome#try_it_out', as: :try_it_out
-  
-  # get 'demo' => 'welcome#demo'
-  # post 'submit_demo' => "welcome#submit_demo"
-  #
-  # get 'demo_you_are' => 'welcome#demo_you_are'
-  # get 'demo3' => 'welcome#demo3'
-  # get 'demo4' => 'welcome#demo4'
-  # get 'demo_services' => 'welcome#demo_services'
-  # get 'demo_companies' => 'welcome#demo_companies'
-  # get 'demo_get_services' => 'welcome#demo_get_services'
-  # get 'demo_get_companies' => 'welcome#demo_get_companies'
   
   match 'auth/:provider/callback', to: 'salesforce_sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
@@ -30,17 +15,14 @@ Rails.application.routes.draw do
   get 'customer_salesforce_credentials' => 'customer_salesforce#salesforce_credentials'
   
   post 'triggermail_demo_hydrate_lead'=> 'triggermail_demo_salesforce#hydrate_lead'
-  
   post 'triggermail_hydrate_lead'=> 'triggermail_salesforce#hydrate_lead'
-  
   post 'lead_hydration_demo_hydrate_lead'=> 'lead_hydration_demo_salesforce#hydrate_lead'
-  
   post 'mighty_signal_hydrate_lead'=> 'mighty_signal_salesforce#hydrate_lead'
   post 'mighty_signal_hydrate_opp'=> 'mighty_signal_salesforce#hydrate_opp'
   
   get 'mturk' => 'mturk#gochime'
   
-  #api for endpoints
+  # API Endpoints (for Front-End)
   post 'api/filter_ios_apps' => 'api#filter_ios_apps'
   post 'api/filter_android_apps' => 'api#filter_android_apps'
   get 'api/get_ios_app' => 'api#get_ios_app'
@@ -74,70 +56,22 @@ Rails.application.routes.draw do
   get 'api/android_sdks' => 'api#android_sdks_for_app'
   get 'api/android_sdks_exist' => 'api#android_sdks_for_app_exist'
 
+  # Auth Endpoints (for Front-End)
+  post 'auth/login' => 'auth#authenticate'
+  post 'auth/validate_token' => 'auth#validate_token'
+  get 'auth/permissions' => 'auth#permissions'
+
   # API for customers
   get 'ping' => 'customer_api#ping', constraints: { subdomain: 'api' }
   get 'ios_apps' => 'customer_api#ios_apps', constraints: { subdomain: 'api' }
   get 'android_apps' => 'customer_api#android_apps', constraints: { subdomain: 'api' }
   get 'companies' => 'customer_api#companies', constraints: { subdomain: 'api' }
 
-  
   if Rails.env.development?
     get 'app_info' => 'app#app_info'
     get 'app_info_get_signals' => 'app#app_info_get_signals'
   end
-  
-  post 'auth/login' => 'auth#authenticate'
-  post 'auth/validate_token' => 'auth#validate_token'
-  get 'auth/permissions' => 'auth#permissions'
-  
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
   get 'ping' => 'application#ping'
   get 'companies' => 'results#companies'
   get 'services' => 'results#services'
