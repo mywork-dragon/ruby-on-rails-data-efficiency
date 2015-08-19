96874a64cf71e970f4589523979a0d6576fc96a1
mywork-dragon <dragonblueyounger@gmail.com>
2015-08-19 14:45:47 -0700

    deploy to web

--- config/deploy/web.rb
+++ config/deploy/web.rb
@@ -3,4 +3,4 @@ require_relative 'mighty_deployer'
 set :branch, 'master'
 set :rails_env, 'production'
 # MightyDeployer.deploy_to([:web_api])
-MightyDeployer.deploy_to([:api])
+MightyDeployer.deploy_to([:web])
