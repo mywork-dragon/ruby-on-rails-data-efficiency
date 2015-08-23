52e255f2a564d9b41599fa31bb8abcf57f6b7392
mywork-dragon <dragonblueyounger@gmail.com>
2015-08-23 13:46:36 -0700

    gem

--- config/initializers/slackiq.rb
+++ config/initializers/slackiq.rb
@@ -1,1 +1,1 @@
-# Slackiq.configure( sdk_scraper: 'https://hooks.slack.com/services/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')
+Slackiq.configure( sdk_scraper: 'https://hooks.slack.com/services/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')
