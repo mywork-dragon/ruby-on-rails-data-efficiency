21748455d65210c4dad6c15a35bb4bcf72b26c8a
mywork-dragon <dragonblueyounger@gmail.com>
2015-08-31 12:21:21 -0700

    ElasticSearch Index Bug Fix

--- app/chewy/apps_index.rb
+++ app/chewy/apps_index.rb
@@ -12,13 +12,13 @@ class AppsIndex < Chewy::Index
   define_type IosAppSnapshot do
     field :name, :seller_url
     # field :seller_url, value: ->(url) {UrlHelper.url_with_domain_only(url)}
-    field :company_name, value: ->(ios_app_snapshot) {!ios_app_snapshot.ios_app.get_company.nil? ? ios_app_snapshot.ios_app.get_company.name : ''}
+    field :company_name, value: ->(ios_app_snapshot) {!(ios_app_snapshot.ios_app.nil? || ios_app_snapshot.ios_app.get_company.nil?) ? ios_app_snapshot.ios_app.get_company.name : ''}
   end
 
   define_type AndroidAppSnapshot do
     field :name, :seller_url
     # field :seller_url, value: ->(url) {UrlHelper.url_with_domain_only(url)}
-    field :company_name, value: ->(android_app_snapshot) {!android_app_snapshot.android_app.get_company.nil? ? android_app_snapshot.android_app.get_company.name : ''}
+    field :company_name, value: ->(android_app_snapshot) {!(android_app_snapshot.android_app.nil? || android_app_snapshot.android_app.get_company.nil?) ? android_app_snapshot.android_app.get_company.name : ''}
     # Seller URL
     # Seller
   end
