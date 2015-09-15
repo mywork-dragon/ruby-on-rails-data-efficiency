b46f3f9a719e4e0fb3a9084e6234a2d5d5860046
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-15 12:13:00 -0700

    iOS class service

--- app/services/test_service.rb
+++ app/services/test_service.rb
@@ -2,36 +2,6 @@ class TestService
 
 	class << self
 
-		def parse_ios
-
-			class_dump = File.open('../Zinio.classdump.txt').read
-
-			clss = class_dump.scan(/(@property|@protocol|@interface|struct|_Bool|\(void\)|\(id\)|\(int\))(.*?)\n/m)
-
-			exclude_words = %w(ns ui categories table tab scroll search page library button zoom issue index image server theme purchase cl zinio)
-
-			clss.each do |k, v|
-
-				str = v[/[^<]+/].strip
-
-				unless exclude_words.any?{ |s| str.downcase.include?(s) }
-
-					str = str.gsub(/\((.*?)\)/m,'').gsub(/[^a-z0-9\s]/i,'').gsub('Bool','').gsub('Init','').gsub('init','')
-
-					# TestServiceWorker.perform_async(str)
-
-					str = str.strip.split(' ').first
-
-					words = str.split(/(?=[A-Z])/).map(&:capitalize).join(' ').strip unless str.present?
-
-				end
-
-			end
-
-			nil
-
-		end
-
 
 	end
 
--- app/workers/test_service_worker.rb
+++ app/workers/test_service_worker.rb
@@ -4,15 +4,7 @@ class TestServiceWorker
 
   sidekiq_options queue: :sdk
 
-  def perform(q)
-
-  	KnownIosWords.find_or_create_by(word: q) if in_app_docs?(q)
-
-  end
-
-  def in_app_docs?(q)
-
-  	JSON.parse(open("https://developer.apple.com/search/search_data.php?q=#{q}&style=flat&results=500").read).any?{ |res| res['description'].downcase.include?(q) }
+  def perform()
 
   end
 
