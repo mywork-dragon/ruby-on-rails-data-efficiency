46f18bec56971f86a3b8043dbc8d459da34eab1f
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-14 17:11:38 -0700

    ElasticSearch Query Refactor

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1069,9 +1069,7 @@ class ApiController < ApplicationController
             operator: 'and',
             fields: [:name, :seller_url, :seller],
             type: 'most_fields',
-            minimum_should_match: '3<75%',
-            fuzziness: '1',
-            prefix_length: '3'
+            minimum_should_match: '3<75%'
         }
     ).limit(num_per_page).offset((page - 1) * num_per_page)
     total_apps_count = result_ids.total_count # the total number of potential results for query (independent of paging)
