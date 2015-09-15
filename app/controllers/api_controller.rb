f5e139dc228503c29115787258be33593e6292e8
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-15 15:28:07 -0700

    Old Queries Restored

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1074,12 +1074,23 @@ class ApiController < ApplicationController
     num_per_page = !params['numPerPage'].nil? ? params['numPerPage'].to_i : 100
 
     result_ids = AppsIndex::IosApp.query(
+=begin
         multi_match: {
             query: query,
             operator: 'and',
             fields: [:name, :seller_url, :seller],
             type: 'cross_fields',
             fuzziness: 1
+        }
+=end
+        multi_match: {
+            query: query,
+            operator: 'and',
+            fields: [:name, :seller_url, :seller], # , :ratings_all],
+            type: 'most_fields',
+            minimum_should_match: '3<75%',
+            fuzziness: '1',
+            prefix_length: '3'
         }
     ).limit(num_per_page).offset((page - 1) * num_per_page)
     total_apps_count = result_ids.total_count # the total number of potential results for query (independent of paging)
@@ -1130,7 +1141,7 @@ class ApiController < ApplicationController
         multi_match: {
             query: query,
             operator: 'and',
-            fields: [:name, :seller_url], # , :seller, :ratings_all],
+            fields: [:name, :seller_url, :seller], # , :ratings_all],
             type: 'most_fields',
             minimum_should_match: '3<75%',
             fuzziness: '1',
