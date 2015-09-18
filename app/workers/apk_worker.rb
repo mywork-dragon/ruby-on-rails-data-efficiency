49b7043a1588e625488a522f1e7d9d033690d24f
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-17 19:28:35 -0700

    inspect best account

--- app/workers/apk_worker.rb
+++ app/workers/apk_worker.rb
@@ -42,6 +42,8 @@ module ApkWorker
 
       best_account = optimal_account(apk_snapshot_job_id, bid, apk_snap.id)
 
+      li "best_account: #{best_account.inspect}"
+
       raise "no best account" if best_account.nil?
 
       apk_snap.google_account_id = best_account.id
