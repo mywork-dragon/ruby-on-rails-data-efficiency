0b2ebcba47f8fea921f15ee7ac42bb55af8ee62e
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-23 15:29:22 -0700

    Create New List Field Focus Directive Added

--- public/app/app/scripts/directives.js
+++ public/app/app/scripts/directives.js
@@ -463,4 +463,19 @@ angular.module("app.directives", []).directive("imgHolder", [
           });
         }
       };
-    }]);
+    }])
+    .directive('focusMe', function($timeout) {
+      return {
+        link: function(scope, element, attrs) {
+          scope.$watch(attrs.focusMe, function(value) {
+            if(value === true) {
+              console.log('value=',value);
+              $timeout(function() {
+                element[0].focus();
+                scope[attrs.focusMe] = false;
+              }, 550);
+            }
+          });
+        }
+      };
+    });
