b9181f9ea277e0e6b9b0578c112c6b66d465bfa3
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-23 18:18:38 -0700

    android_wrong_icon_test_service.rb

---
+++ app/services/android_wrong_icon_test_service.rb
@@ -0,0 +1,33 @@
+class AndroidWrongIconTestService
+
+  class << self
+    
+    def run
+
+      batch = Sidekiq::Batch.new
+      batch.description = "AndroidWrongIconTestService" 
+      batch.on(:complete, 'AndroidWrongIconTestService#on_complete_run')
+  
+      Slackiq.message('AndroidWrongIconTestService: Queueing...', webhook_name: :main)
+
+      batch.jobs do
+
+        10e3.times do |n|
+          AndroidWrongIconTestServiceWorker.perform_async
+        end  
+      
+      end
+
+      
+
+      Slackiq.message('AndroidWrongIconTestService: Done queueing.' webhook_name: :main)
+
+    end
+
+  end
+
+  def on_complete_run(status, options)
+    Slackiq.notify(webhook_name: :main, status: status, title: 'AndroidWrongIconTestService Completed')
+  end
+
+end
