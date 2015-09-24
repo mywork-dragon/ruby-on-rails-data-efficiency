b08cf378a366d01658a4240798d58a69f486c1e6
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-23 18:19:06 -0700

    new migration

--- db/migrate/20150921203154_add_uniqueness_to_android_sdk_companies_apk_snapshot.rb
+++ db/migrate/20150921203154_add_uniqueness_to_android_sdk_companies_apk_snapshot.rb
@@ -1,7 +1,7 @@
 class AddUniquenessToAndroidSdkCompaniesApkSnapshot < ActiveRecord::Migration
   def change
 
-  	# remove_index :android_sdk_companies_apk_snapshots, name: 'index_apk_snapshot_id_android_sdk_company_id'
+  	remove_index :android_sdk_companies_apk_snapshots, name: 'index_apk_snapshot_id_android_sdk_company_id'
 
     add_index :android_sdk_companies_apk_snapshots, [:apk_snapshot_id, :android_sdk_company_id], name: 'index_apk_snapshot_id_android_sdk_company_id2' if !index_exists?(:android_sdk_companies_apk_snapshots, [:apk_snapshot_id, :android_sdk_company_id])
 
