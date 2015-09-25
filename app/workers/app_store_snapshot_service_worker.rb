c5cd3a203564051cdde8672460ddca372e9f5133
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-25 14:11:23 -0700

    Add lock for iOS too

--- app/workers/app_store_snapshot_service_worker.rb
+++ app/workers/app_store_snapshot_service_worker.rb
@@ -21,167 +21,171 @@ class AppStoreSnapshotServiceWorker
     ios_app = IosApp.find(options[:ios_app_id])
     
     ios_app_snapshot_job_id = options[:ios_app_snapshot_job_id]
-    s = IosAppSnapshot.create(ios_app: ios_app, ios_app_snapshot_job_id: ios_app_snapshot_job_id)
-    
-    try = 0
-    
-    begin
+
+    IosAppSnapshot.transaction do
+
+      s = IosAppSnapshot.lock.create(ios_app: ios_app, ios_app_snapshot_job_id: ios_app_snapshot_job_id)
       
-      a = AppStoreService.attributes(ios_app.app_identifier)
+      try = 0
       
-      raise 'AppStoreService.attributes is empty' if a.empty?
-    
-      single_column_attributes = %w(
-        name
-        description
-        release_notes
-        version
-        price
-        size
-        seller
-        recommended_age
-        required_ios_version
-        released
-        editors_choice
-        developer_app_store_identifier
-        icon_url_350x350
-        icon_url_175x175
-        first_released
-        by
-        copywright
-        seller_url_text
-        support_url_text
-      )
-    
-      single_column_attributes.each do |sca|
-        value = a[sca.to_sym]
-        s.send("#{sca}=", value) if value
-      end
-    
-      # Categories
-      if categories = a[:categories]
-        categories_snapshot_primary = IosAppCategoriesSnapshot.new
-        categories_snapshot_primary.ios_app_snapshot = s
-        categories_snapshot_primary.ios_app_category = IosAppCategory.find_or_create_by(name: categories[:primary])
-        categories_snapshot_primary.kind = :primary
-        categories_snapshot_primary.save!
-    
-        categories_snapshot_secondary = IosAppCategoriesSnapshot.new
-        categories[:secondary].each do |secondary_category|
-          categories_snapshot_secondary.ios_app_snapshot = s
-          categories_snapshot_secondary.ios_app_category = IosAppCategory.find_or_create_by(name: secondary_category)
-          categories_snapshot_secondary.kind = :secondary
+      begin
+        
+        a = AppStoreService.attributes(ios_app.app_identifier)
+        
+        raise 'AppStoreService.attributes is empty' if a.empty?
+      
+        single_column_attributes = %w(
+          name
+          description
+          release_notes
+          version
+          price
+          size
+          seller
+          recommended_age
+          required_ios_version
+          released
+          editors_choice
+          developer_app_store_identifier
+          icon_url_350x350
+          icon_url_175x175
+          first_released
+          by
+          copywright
+          seller_url_text
+          support_url_text
+        )
+      
+        single_column_attributes.each do |sca|
+          value = a[sca.to_sym]
+          s.send("#{sca}=", value) if value
         end
-        categories_snapshot_secondary.save!
-      end
-    
-      if ratings = a[:ratings]
-        if ratings_current = ratings[:current]
-          ratings_current_count = ratings_current[:count].to_i
-          s.ratings_current_count = ratings_current_count
-          s.ratings_current_stars = ratings_current[:stars]
-          
-          if released = a[:released]
-            days_ago = (Date.tomorrow - released).to_i
-            ratings_per_day_current_release = ratings_current_count/(days_ago.to_f)
-            s.ratings_per_day_current_release = ratings_per_day_current_release
+      
+        # Categories
+        if categories = a[:categories]
+          categories_snapshot_primary = IosAppCategoriesSnapshot.new
+          categories_snapshot_primary.ios_app_snapshot = s
+          categories_snapshot_primary.ios_app_category = IosAppCategory.find_or_create_by(name: categories[:primary])
+          categories_snapshot_primary.kind = :primary
+          categories_snapshot_primary.save!
+      
+          categories_snapshot_secondary = IosAppCategoriesSnapshot.new
+          categories[:secondary].each do |secondary_category|
+            categories_snapshot_secondary.ios_app_snapshot = s
+            categories_snapshot_secondary.ios_app_category = IosAppCategory.find_or_create_by(name: secondary_category)
+            categories_snapshot_secondary.kind = :secondary
+          end
+          categories_snapshot_secondary.save!
+        end
+      
+        if ratings = a[:ratings]
+          if ratings_current = ratings[:current]
+            ratings_current_count = ratings_current[:count].to_i
+            s.ratings_current_count = ratings_current_count
+            s.ratings_current_stars = ratings_current[:stars]
+            
+            if released = a[:released]
+              days_ago = (Date.tomorrow - released).to_i
+              ratings_per_day_current_release = ratings_current_count/(days_ago.to_f)
+              s.ratings_per_day_current_release = ratings_per_day_current_release
+            end
+            
           end
           
+        
+          if ratings_all = ratings[:all]
+            ratings_all_count = ratings_all[:count].to_i #store in memory bc we need it later
+            s.ratings_all_count = ratings_all_count
+            
+            s.ratings_all_stars = ratings_all[:stars]
+          end
+        end
+      
+        if seller_url = a[:seller_url]
+          s.seller_url = seller_url
+          #TODO: add logic around company
+        end
+      
+      
+        if support_url = a[:support_url]
+           s.support_url = support_url
+           #TODO: add logic around company
+        end
+     
+      
+        if languages = a[:languages]
+          languages.each do |language_name|
+            s.ios_app_languages << IosAppLanguage.find_or_create_by(name: language_name)
+          end
+        end
+      
+        if in_app_purchases = a[:in_app_purchases]
+          in_app_purchases.each do |in_app_purchase|
+            IosInAppPurchase.create(name: in_app_purchase[:name], price: in_app_purchase[:price], ios_app_snapshot: s)
+          end
         end
         
-      
-        if ratings_all = ratings[:all]
-          ratings_all_count = ratings_all[:count].to_i #store in memory bc we need it later
-          s.ratings_all_count = ratings_all_count
+        if icon_urls = a[:icon_urls]
           
-          s.ratings_all_stars = ratings_all[:stars]
+          if size_350x350 = icon_urls[:size_350x350]
+            s.icon_url_350x350 = size_350x350
+          end
+          if size_175x175 = icon_urls[:size_175x175]
+            s.icon_url_175x175 = size_175x175
+          end
         end
-      end
-    
-      if seller_url = a[:seller_url]
-        s.seller_url = seller_url
-        #TODO: add logic around company
-      end
-    
-    
-      if support_url = a[:support_url]
-         s.support_url = support_url
-         #TODO: add logic around company
-      end
-   
-    
-      if languages = a[:languages]
-        languages.each do |language_name|
-          s.ios_app_languages << IosAppLanguage.find_or_create_by(name: language_name)
-        end
-      end
-    
-      if in_app_purchases = a[:in_app_purchases]
-        in_app_purchases.each do |in_app_purchase|
-          IosInAppPurchase.create(name: in_app_purchase[:name], price: in_app_purchase[:price], ios_app_snapshot: s)
-        end
-      end
       
-      if icon_urls = a[:icon_urls]
+        s.save!
         
-        if size_350x350 = icon_urls[:size_350x350]
-          s.icon_url_350x350 = size_350x350
+        #set user base
+        if defined?(ratings_all_count) && defined?(ratings_per_day_current_release)
+          if ratings_per_day_current_release >= 7 || ratings_all_count >= 50e3
+            user_base = :elite
+          elsif ratings_per_day_current_release >= 1 || ratings_all_count >= 10e3
+            user_base = :strong
+          elsif ratings_per_day_current_release >= 0.1 || ratings_all_count >= 100
+            user_base = :moderate
+          else
+            user_base = :weak
+          end
+          
+          ios_app.user_base = user_base
         end
-        if size_175x175 = icon_urls[:size_175x175]
-          s.icon_url_175x175 = size_175x175
+        
+        
+        #set mobile priority
+        if released = a[:released]
+          if ios_app.ios_fb_ad_appearances.present? || released > 2.months.ago
+            mobile_priority = :high
+          elsif released > 4.months.ago
+            mobile_priority = :medium
+          else
+            mobile_priority = :low
+          end
+          
+          ios_app.mobile_priority = mobile_priority
         end
-      end
-    
-      s.save!
+        
+        #update newest snapshot
+        ios_app.newest_ios_app_snapshot_id = s.id #make sure s has been saved already
+        
+        ios_app_save_success = ios_app.save
       
-      #set user base
-      if defined?(ratings_all_count) && defined?(ratings_per_day_current_release)
-        if ratings_per_day_current_release >= 7 || ratings_all_count >= 50e3
-          user_base = :elite
-        elsif ratings_per_day_current_release >= 1 || ratings_all_count >= 10e3
-          user_base = :strong
-        elsif ratings_per_day_current_release >= 0.1 || ratings_all_count >= 100
-          user_base = :moderate
+      rescue => e
+        ise = IosAppSnapshotException.create(ios_app_snapshot: s, name: e.message, backtrace: e.backtrace, try: try, ios_app_snapshot_job_id: ios_app_snapshot_job_id)
+        if (try += 1) < MAX_TRIES
+          retry
         else
-          user_base = :weak
+          s.status = :failure
+          s.save!
         end
-        
-        ios_app.user_base = user_base
-      end
-      
-      
-      #set mobile priority
-      if released = a[:released]
-        if ios_app.ios_fb_ad_appearances.present? || released > 2.months.ago
-          mobile_priority = :high
-        elsif released > 4.months.ago
-          mobile_priority = :medium
-        else
-          mobile_priority = :low
-        end
-        
-        ios_app.mobile_priority = mobile_priority
-      end
-      
-      #update newest snapshot
-      ios_app.newest_ios_app_snapshot_id = s.id #make sure s has been saved already
-      
-      ios_app_save_success = ios_app.save
-    
-    rescue => e
-      ise = IosAppSnapshotException.create(ios_app_snapshot: s, name: e.message, backtrace: e.backtrace, try: try, ios_app_snapshot_job_id: ios_app_snapshot_job_id)
-      if (try += 1) < MAX_TRIES
-        retry
       else
-        s.status = :failure
+        s.status = :success
         s.save!
       end
-    else
-      s.status = :success
-      s.save!
+      
+      s
     end
-    
-    s
   end
   
   def test_save_attributes
