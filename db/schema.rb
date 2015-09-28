6f1031bb29962a9599d60ed400d89feb6ddf2eb8
mywork-dragon <dragonblueyounger@gmail.com>
2015-09-28 15:02:07 -0700

    change name column to text in ios_app_snapshot_exceptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924025216) do
+ActiveRecord::Schema.define(version: 20150928215917) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
