c6e2c1aa4c5cdc31663fe0600ff4fef73d8d1992
mywork-dragon <dragonblueyounger@gmail.com>
2015-10-19 12:57:33 -0700

    flagging logic

--- lib/non_autoload/apk_downloader_api.rb
+++ lib/non_autoload/apk_downloader_api.rb
@@ -161,8 +161,6 @@ if defined?(ApkDownloader)
         curb.timeout = 90
       end
 
-      # TestModel.create(text0: response)
-
       if [200,302].include? response.status
 
         mp = ApkSnapshot.find(apk_snap_id).micro_proxy
@@ -192,19 +190,22 @@ if defined?(ApkDownloader)
           if response.body.include? "This item cannot be installed in your country"
 
             aa = ApkSnapshot.find_by_id(apk_snap_id).android_app
-
-            aa.in_america = false
+            aa.display_type = :foreign
             aa.save
 
+          elsif response.body.include? "Your device is not compatible with this item"
+            
+            aa = ApkSnapshot.find_by_id(apk_snap_id).android_app
+            aa.display_type = :device_incompatible
+            aa.save
+
+          else
+
+            ga = GoogleAccount.joins(apk_snapshots: :google_account).where('apk_snapshots.id = ?', apk_snap_id).first
+            ga.flags = ga.flags + 1
+            ga.save
+
           end
-
-          ga = GoogleAccount.joins(apk_snapshots: :google_account).where('apk_snapshots.id = ?', apk_snap_id).first
-          ga.flags = ga.flags + 1
-          ga.save
-
-          as = ApkSnapshot.find_by_id(apk_snap_id).android_app.newest_android_app_snapshot
-          as.apk_access_forbidden = true
-          as.save
 
           snap = ApkSnapshot.find_by_id(apk_snap_id)
           snap.status = :forbidden
@@ -212,9 +213,9 @@ if defined?(ApkDownloader)
 
         elsif response.status == 404
 
-          as = ApkSnapshot.find_by_id(apk_snap_id).android_app
-          as.taken_down = true
-          as.save
+          aa = ApkSnapshot.find_by_id(apk_snap_id).android_app
+          aa.display_type = :taken_down
+          aa.save
 
           snap = ApkSnapshot.find_by_id(apk_snap_id)
           snap.status = :taken_down
