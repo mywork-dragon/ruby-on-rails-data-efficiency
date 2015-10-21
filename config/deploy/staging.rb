7da881457a996988debea9cf2d81a77db1bc8919
mywork-dragon <dragonblueyounger@gmail.com>
2015-10-21 12:11:05 -0700

    Temp Deployment Change for Staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,8 @@
 require_relative 'mighty_deployer'
 
-set :branch, 'staging'
+set :branch, 'angular-1'
 set :rails_env, 'production'
 set :bundle_without, 'scraper_only'
 MightyDeployer.deploy_to([:staging])
+
+# cap staging deploy
