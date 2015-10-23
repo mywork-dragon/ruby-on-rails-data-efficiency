abbc58654929ab4fd3f29a8d707f0866d3ec4558
mywork-dragon <dragonblueyounger@gmail.com>
2015-10-22 20:51:59 -0700

    CSV Export Fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1139,7 +1139,7 @@ class ApiController < ApplicationController
     android_apps = # ANDROID APPS HERE ----------------------------------------
     apps = []
 
-    header = ['MightySignal App ID', 'Android App ID', 'App Name', 'Company Name', 'Fortune Rank', 'Mobile Priority', 'Ad Spend', 'User Base', 'Categories', 'Released Date', 'Total Ratings', 'Min Downloads', 'Max Downloads']
+    header = ['MightySignal App ID', 'Android App ID', 'App Name', 'Company Name', 'Fortune Rank', 'Mobile Priority', 'Ad Spend', 'User Base', 'Categories', 'Released Date', 'Total Ratings'] #, 'Min Downloads', 'Max Downloads']
     # Android
     # results = AndroidApp.includes(:android_fb_ad_appearances, newest_android_app_snapshot: :android_app_categories, websites: :company).joins(:newest_android_app_snapshot).where('android_app_snapshots.name IS NOT null').joins(websites: :company).where(mobile_priority: [0]).where(user_base: [0]).joins(newest_android_app_snapshot: {android_app_categories_snapshots: :android_app_category}).where('android_app_categories.name IN (?)', ["Travel & Local", "Lifestyle", "Sports", "Health & Fitness", "Entertainment", "Photography"]).group('android_apps.id').order('android_app_snapshots.name ASC').to_a
 
@@ -1163,9 +1163,9 @@ class ApiController < ApplicationController
           app.user_base,
           newest_snapshot.present? ? IosAppCategoriesSnapshot.where(ios_app_snapshot: newest_snapshot, kind: IosAppCategoriesSnapshot.kinds[:primary]).map{|iacs| iacs.ios_app_category.name}.join(", ") : nil,
           app.released,
-          newest_snapshot.present? ? newest_snapshot.ratings_all_count : nil,
-          newest_snapshot.present? ? newest_snapshot.downloads_min : nil,
-          newest_snapshot.present? ? newest_snapshot.downloads_max : nil
+          newest_snapshot.present? ? newest_snapshot.ratings_all_count : nil
+          #newest_snapshot.present? ? newest_snapshot.downloads_min : nil,
+          #newest_snapshot.present? ? newest_snapshot.downloads_max : nil
       ]
 
       apps << app_hash
