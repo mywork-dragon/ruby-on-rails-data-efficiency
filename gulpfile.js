45338864d7b28eb3a29d1383c57ebc9662880c0d
mywork-dragon <dragonblueyounger@gmail.com>
2015-10-23 14:51:27 -0700

    Merge

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
 var gulp = require('gulp');
+var rev = require('gulp-rev');
+var revReplace = require('gulp-rev-replace');
 
-gulp.task('cache-buster', function() {
-    // place code for your default task here
+
+gulp.task('revision', function() {
+    return gulp
+        .src([
+                './public/app/app/scripts/**/*.js',
+                './public/app/app/styles/**/*.css'
+            ],
+            {
+                base: './public/app'
+            })
+        .pipe(rev()) // app.js --> app-1j8889jr.js
+        .pipe(revReplace())
+        .pipe(gulp.dest('./public/app/dist'));
+});
+
+gulp.task('revreplace', function() {
+    return gulp
+        .src([
+            './public/app/app/scripts/**/*.js',
+            './public/app/app/styles/**/*.css'
+        ],
+        {
+            base: './public/app'
+        })
+        .pipe(rev()) // app.js --> app-1j8889jr.js
+        .pipe(revReplace())
+        .pipe(gulp.dest('./dist'));
 });
