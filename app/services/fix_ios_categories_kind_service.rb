82f502ecd8565f03a1dbfcf001636a0346575471
mywork-dragon <dragonblueyounger@gmail.com>
2015-10-24 22:11:25 -0700

    use batches!

--- app/services/fix_ios_categories_kind_service.rb
+++ app/services/fix_ios_categories_kind_service.rb
@@ -3,21 +3,42 @@ class FixIosCategoriesKindService
   class << self
 
     def fix_primary
-      IosAppCategoriesSnapshot.where(kind: 'primary').find_in_batches(batch_size: 1000).with_index do |batch, index|
-        li "##{index}"
-        ids = batch.map{ |iac| iac.id}
-        FixIosCategoriesWorker.perform_async(ids, 0)
+      batch = Sidekiq::Batch.new
+      batch.description = "fix_primary" 
+      batch.on(:complete, 'FixIosCategoriesKindService#on_complete_fix_primary')
+
+      batch.jobs do
+        IosAppCategoriesSnapshot.where(kind: 'primary').find_in_batches(batch_size: 1000).with_index do |batch, index|
+          li "##{index}"
+          ids = batch.map{ |iac| iac.id}
+          FixIosCategoriesWorker.perform_async(ids, 0)
+        end
       end
     end
 
     def fix_secondary
-      IosAppCategoriesSnapshot.where(kind: 'primary').find_in_batches(batch_size: 1000).with_index do |batch, index|
-        li "##{index}"
-        ids = batch.map{ |iac| iac.id}
-        FixIosCategoriesWorker.perform_async(ids, 1)
+      batch = Sidekiq::Batch.new
+      batch.description = "fix_secondary" 
+      batch.on(:complete, 'FixIosCategoriesKindService#on_complete_fix_secondary')
+
+      batch.jobs do
+        IosAppCategoriesSnapshot.where(kind: 'primary').find_in_batches(batch_size: 1000).with_index do |batch, index|
+          li "##{index}"
+          ids = batch.map{ |iac| iac.id}
+          FixIosCategoriesWorker.perform_async(ids, 1)
+        end
       end
     end
-    
+
+
+    def on_complete_fix_primary(status, options)
+      Slackiq.notify(webhook_name: :main, status: status, title: 'fix_primary')
+    end
+
+    def on_complete_fix_secondary(status, options)
+      Slackiq.notify(webhook_name: :main, status: status, title: 'fix_secondary')
+    end
+
   end
 
 
