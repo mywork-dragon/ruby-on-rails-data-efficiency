304a88202a6c505279344c5c5047344bc0f60bc7
mywork-dragon <dragonblueyounger@gmail.com>
2015-10-28 18:46:06 -0700

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,8 +85,10 @@ namespace :deploy do
     # run bower install to get bower updates
     on roles(:web, :staging) do
       execute '(cd /home/webapps/varys/current/public/app && bower install)'
-      execute '(cd /home/webapps/varys/releases/$(ls -t /home/webapps/varys/releases | head -n1) && npm install)'
-      execute '(cd /home/webapps/varys/releases/$(ls -t /home/webapps/varys/releases | head -n1) && npm run gulp-build)'
+      execute '(cd /home/deploy/varys_current && npm install)'
+      execute '(cd /home/deploy/varys_current && npm run gulp-build)'
+      # execute '(cd /home/webapps/varys/releases/$(ls -t /home/webapps/varys/releases | head -n1) && npm install)'
+      # execute '(cd /home/webapps/varys/releases/$(ls -t /home/webapps/varys/releases | head -n1) && npm run gulp-build)'
     end
   end
 end
