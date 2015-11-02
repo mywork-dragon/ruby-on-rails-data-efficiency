54ab8dd7cdda892d2895b6ab8f8f4219a743bf65
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-02 14:51:10 -0800

    get repo data

---
+++ app/services/github_service.rb
@@ -0,0 +1,21 @@
+class GithubService
+
+  # Repo can be URL or user/repo
+  def get_repo_data(repo)
+    repos_api_url = 
+      if repo.match(/\Ahttps?:\/\//)  # if it looks like a url
+        repo.sub('github.com/', 'api.github.com/repos/')
+      elsif repo.split('/').count == 2
+        'https://api.github.com/repos/' + repo
+      end
+
+    ap repos_api_url
+  end
+
+  class << self
+    def get_repo_data(repo)
+      self.new.get_repo_data(repo)
+    end
+  end
+
+end
