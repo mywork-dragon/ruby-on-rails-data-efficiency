835890adb9d888498393f4119298484a259df0d2
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-11 14:01:36 -0800

    batch the job

--- app/services/cocoapod_metrics_service.rb
+++ app/services/cocoapod_metrics_service.rb
@@ -2,16 +2,29 @@ class CocoapodMetricsService
 
   class << self
 
-    # get all the cocoapods
-    pod_sdks = IosSdk.find(Cocoapod.select(:ios_sdk_id).map{|x| x.ios_sdk_id}.compact.uniq)
-    # filter deprecated?
-    
-    pod_sdks.each do |sdk|
+    def update_metrics
+
+      pod_sdks = IosSdk.find(Cocoapod.select(:ios_sdk_id).map{|x| x.ios_sdk_id}.compact.uniq)
+
       if Rails.env.production?
-        CocoapodMetricsServiceWorker.perform(sdk.id)
+        batch = Sidekiq::Batch.new
+        batch.description = "updating metrics for cocoapods" 
+        batch.on(:complete, 'CocoapodMetricsService#on_complete')
+
+        batch.jobs do
+          pod_sdks.each do |sdk|
+            CocoapodMetricsServiceWorker.perform_async(sdk.id)
+          end
+        end
       else
-        CocoapodMetricsServiceWorker.new.perform(sdk.id)
+        pod_sdks.each do |sdk|
+          CocoapodMetricsServiceWorker.new.perform(sdk.id)
+        end
       end
     end
+  end
+
+  def on_complete(status, options)
+    Slackiq.notify(webhook_name: :main, status: status, title: 'updated_metrics')
   end
 end
