05ce101c5501a420e4a46c1f04e48bf7264d2090
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-10 16:56:17 -0800

    Filter Adding Logic Refactored Into Service

---
+++ public/app/app/scripts/services/filter.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular.module("appApp")
+  .factory("filterService", ["$rootScope",
+    function($rootScope) {
+      return {
+        addFilter: function(parameter, value, displayName, limitToOneFilter) {
+          console.log('Param:', parameter, 'Val:', value, 'DisName:', displayName);
+          /* -------- Mixpanel Analytics Start -------- */
+          var mixpanelProperties = {};
+          mixpanelProperties['parameter'] = parameter;
+          mixpanelProperties[parameter] = value;
+          mixpanel.track(
+            "Filter Added",
+            mixpanelProperties
+          );
+          /* -------- Mixpanel Analytics End -------- */
+
+          var duplicateTag = false;
+          var oneTagUpdated = false;
+
+          $rootScope.tags.forEach(function (tag) {
+
+            // Determine if tag is a duplicate
+            if (tag.parameter == parameter && tag.value == value) {
+              duplicateTag = true;
+            }
+
+            if(limitToOneFilter && !duplicateTag) {
+              // If replacing pre existing tag of limitToOneFilter = true category
+              if (tag.parameter == parameter) {
+                tag.value = value;
+                tag.text = displayName + ': ' + value;
+                oneTagUpdated = true;
+              }
+            }
+          });
+
+          if(limitToOneFilter && !duplicateTag && !oneTagUpdated) {
+            // If first tag of limitToOneFilter = true category
+            $rootScope.tags.push({
+              parameter: parameter,
+              value: value,
+              text: displayName + ': ' + value
+            });
+          }
+
+          if(!limitToOneFilter && !duplicateTag || $rootScope.tags.length < 1) {
+            $rootScope.tags.push({
+              parameter: parameter,
+              value: value,
+              text: displayName + ': ' + value
+            });
+          }
+        }
+      };
+    }
+  ]);
