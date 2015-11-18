2c024ba6f300b65ba2c51b47d6edba87773b221a
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-17 17:26:26 -0800

    bug fixes and remove old code

--- app/services/sdk_service.rb
+++ app/services/sdk_service.rb
@@ -13,7 +13,7 @@ class SdkService
 			packages = packages.uniq
 
 			matches = packages.reduce({}) do |memo, package|
-				put "Searching for package: #{package}".yellow
+				puts "Searching for package: #{package}".yellow
 				match = existing_sdk_from_package(package: package, platform: platform)
 				memo[package] = match if match
 				memo
@@ -89,6 +89,7 @@ class SdkService
 				end
 				memo
 			end
+
 
 			results = query_hash.keys.map do |query|
 				sdk = google_sdk(query: query, platform: platform) || google_github(query: query, platform: platform)
@@ -276,7 +277,7 @@ class SdkService
 		end
 
 		def github_query_valid?(query)
-			query.downcase!
+			query = query.downcase
 
 			invalid_queries = %w(
 				apple
@@ -316,117 +317,6 @@ class SdkService
 		def camel_split(str)
 			str.split(/(?=[A-Z])/).map(&:capitalize).join(' ').strip
 		end
-
-		# def g_words(package)
-		# 	words = %w(ads maps wallet analytics drive admob doubleclick plus)
-		# 	words.each do |g| 
-		# 		return 'google ' + g if package.include? g
-		# 	end
-		# 	nil
-		# end
-
-		# ## OLD
-		# def create_sdk_with_query(proposed:, platform:, query:)
-		# 	raise "Not implemented Android create sdk" if platform !== :ios
-
-		# 	begin
-		# 		favicon = proposed[:favicon] || WWW::Favicon.new.find(proposed[:website])
-		# 	rescue
-		# 		favicon = nil
-		# 	end
-
-		# 	begin
-		# 		sdk = IosSdk.create!(name: proposed[:company], website: proposed[:url], favicon: favicon, open_source: proposed[:kind] == :open_source, github_repo_identifier: proposed[:repo_id])
-		# 	rescue ActiveRecord::RecordNotUnique => e
-		# 		sdk = IosSdk.find_by_name(proposed[:company])
-		# 	end
-
-		# 	# Link future similar queries to this same SDK
-		# 	begin
-		# 		prefix = IosSdkPackagePrefix.create!(prefix: query.capitalize, ios_sdk_id: sdk.id)
-		# 	rescue ActiveRecord::RecordNotUnique => e
-		# 		nil # was already created...?
-		# 	end
-
-		# 	sdk
-		# end
-
-
-		# Given the queries to search for, will find SDKs
-		# When you already know what the queries will be
-		# @author Jason Lew
-		# @param The queries to run
-		# @platform :ios or :android
-		# @note Will only search unique queries
-		# def find_from_queries(queries:, platform:)
-		# 	queries = queries.uniq.compact
-			
-		# 	return {} if queries.empty?
-
-		# 	sdks = []
-
-		# 	queries.each do |query|
-		# 		puts "Query: #{query}".green
-		# 		sdk = find_existing_prefix(query: query, platform: platform) || google_sdk(query: query, platform: platform) || google_github(query: query, platform: platform)
-
-		# 		next if !sdk
-
-
-
-		# 		sdk = find_existing_sdk(proposed: sdk, platform: platform) || sdk
-
-		# 		if sdk.class != IosSdk || sdk.class != AndroidSdkCompany
-		# 			# create the sdk
-		# 			sdk = create_sdk_with_query(proposed: sdk, platform: platform, query: query)
-		# 		end
-		# 		ap sdk
-		# 		puts ""
-		# 		sdks << sdk
-		# 	end
-
-			
-		# 	existing_sdks = sdks.select { |sdk| sdk.class == IosSdk}.uniq
-		# 	new_sdks = sdks - existing_sdks
-
-		# 	company_sdks = sdks.select{ |sdk| sdk[:kind] == :company}
-		# 	company_sdks.uniq!{ |x| [x[:company], x[:url]] }
-
-		# 	open_source_sdks = sdks.select{ |sdk| sdk[:kind] == :open_source if sdk.class != IosSdk}
-		# 	open_source_sdks.uniq{ |x| x[:repo_id]}
-
-		# 	company_sdks + open_source_sdks
-		# end
-
-		# Checks to see if the sdk already exists for a given query. If it does, returns the sdk
-		# def find_existing_prefix(query:, platform:)
-		# 	# see if the prefix has already been searched
-		# 	raise "Not implemented Android check existing" if platform !== :ios
-		# 	existing_prefix = IosSdkPackagePrefix.find_by_prefix(query.capitalize)
-		# 	return existing_prefix.ios_sdk if existing_prefix
-
-		# end
-
-		# def sdks_from_queries(queries:, platform:)
-
-		# end
-
-		# # Given a query ("Crashlytics"), a platform (:ios or :android), create an SDK if can find a suitable one. If optional read_only flag is true, don't actually write to the database
-		# def create_sdk_with_query(query:, platform:, read_only: false)
-		# 	sdk = google_sdk(query: query, platform: platform) || google_github(query: query, platform: platform)
-
-		# 	return nil if sdk.nil?
-
-		# 	sdk = find_sdk_from_proposed(proposed: sdk, platform: platform) || sdk
-
-		# 	puts "Creating SDK"
-		# 	ap sdk
-
-		# 	return nil if read_only
-
-		# 	sdk = create_sdk_from_proposed(proposed: sdk, platform: platform)
-
-
-		# end
 
 	end
 
