e52e3e47e2cdbb9724714631542ad1887b2c0d90
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-20 11:23:48 -0800

    bug fixes on ios live scan service

--- app/services/ios_live_scan_service.rb
+++ app/services/ios_live_scan_service.rb
@@ -13,6 +13,8 @@ class IosLiveScanService
       else
         IosLiveScanServiceWorker.new.perform(job.id, ios_app_id)
       end
+
+      job.id
     end
 
     def check_status(job_id: job_id)
@@ -34,34 +36,32 @@ class IosLiveScanService
 
       job = IpaSnapshotJob.find(job_id)
 
-      byebug
-
       return nil if job.nil? || job.job_type != 'one_off'
       
       snapshot = job.ipa_snapshots.first
 
       # first set of checks: validation stage
       status = if %w(validating not_available paid unchanged device_incompatible).include?(job.live_scan_status)
-        result_map[job.status.to_sym]
-      elsif job.status != :initiated
+        result_map[job.live_scan_status.to_sym]
+      elsif job.live_scan_status !="initiated"
         result_map[:validating]
       end
 
       return status if !status.nil?
 
       # second set of checks: download and scan stages
-      stage = if snapshot.nil?
+      status = if snapshot.nil?
         result_map[:preparing]
-      elsif snapshot.scan_status == :scanned
+      elsif snapshot.scan_status == "scanned"
         result_map[:complete]
-      elsif snapshot.scan_status == :scanning
+      elsif snapshot.scan_status == "scanning"
         result_map[:scanning]
-      elsif snapshot.download_status == :complete && snapshot.success == false
+      elsif snapshot.download_status == "complete" && snapshot.success == false
         exception = snapshot.ipa_snapshot_exceptions.last
         exception && exception.error_code == "devices_busy" ? result_map[:devices_busy] : result_map[:failed]
-      elsif snapshot.download_status == :starting
+      elsif snapshot.download_status == "starting" || snapshot.download_status == "cleaning"
         result_map[:downloading]
-      elsif snapshot.download_status == :retrying
+      elsif snapshot.download_status == "retrying"
         result_map[:retrying]
       else
         result_map[:failed]
