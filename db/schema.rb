244fd71f43ecffbb2f9f87898b6dc20fcf26f185
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-19 19:57:28 -0800

    Add iOS version to iOS devices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118182401) do
+ActiveRecord::Schema.define(version: 20151120035318) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
@@ -885,6 +885,7 @@ ActiveRecord::Schema.define(version: 20151118182401) do
     t.datetime "updated_at"
     t.boolean  "in_use"
     t.datetime "last_used"
+    t.string   "ios_version"
   end
 
   add_index "ios_devices", ["ip"], name: "index_ios_devices_on_ip", using: :btree
