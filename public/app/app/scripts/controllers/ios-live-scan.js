dd691393b24daccc07e7ce767571c3855b9df0a7
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-23 09:38:51 -0800

    Template for iOS Live Scan Ctrl Built

--- public/app/app/scripts/controllers/ios-live-scan.js
+++ public/app/app/scripts/controllers/ios-live-scan.js
@@ -1,12 +1,35 @@
 'use strict';
 
-angular.module('appApp').controller("IosLiveScanCtrl", ["$scope", "$http", "$routeParams", "$window", "pageTitleService", "listApiService", "loggitService", "$rootScope", "apiService", "authService", "appDataService",
-  function($scope, $http, $routeParams, $window, pageTitleService, listApiService, loggitService, $rootScope, apiService, authService, appDataService) {
+angular.module('appApp').controller("IosLiveScanCtrl", ["$scope", "$http", "$routeParams", "$window", "pageTitleService", "listApiService", "loggitService", "$rootScope", "apiService", "authService", "appDataService", "sdkLiveScanService",
+  function($scope, $http, $routeParams, $window, pageTitleService, listApiService, loggitService, $rootScope, apiService, authService, appDataService, sdkLiveScanService) {
+
     var iosLiveScanCtrl = this;
 
-    // GET ios_sdks_exist
-    // GET ios_scan_status?id
-    // POST ios_start_scan?id
-    
+    var userInfo = {}; // User info set
+    authService.userInfo().success(function(data) { userInfo['email'] = data.email; });
+
+    $scope.$on('EVENT_ON_APP_DETAILS_LOAD_COMPLETION', function () {
+
+      iosLiveScanCtrl.appData = appDataService.appData; // Service to share data between both controllers
+      iosLiveScanCtrl.displayStatus = appDataService.appData.displayStatus;
+
+      sdkLiveScanService.checkForIosSdks()
+        .success(function (data) {
+          iosLiveScanCtrl.sdkData = data;
+        });
+
+    });
+
+    iosLiveScanCtrl.getSdks = function(appId) {
+      sdkLiveScanService.startIosSdkScan(appId)
+        .success(function(data) {
+
+        })
+        .error(function(err) {
+
+        });
+    }
+
   }
+
 ]);
--- public/app/app/scripts/services/sdk-live-scan.js
+++ public/app/app/scripts/services/sdk-live-scan.js
@@ -16,6 +16,27 @@ angular.module("appApp")
           url: API_URI_BASE + 'api/scan_android_sdks',
           params: {appId: appId}
         })
+      },
+      checkForIosSdks: function(appId) {
+        return $http({
+          method: 'GET',
+          url: API_URI_BASE + 'api/ios_sdks_exist',
+          params: {appId: appId}
+        })
+      },
+      startIosSdkScan: function(appId) {
+        return $http({
+          method: 'POST',
+          url: API_URI_BASE + 'api/ios_start_scan',
+          data: {id: appId}
+        })
+      },
+      getIosScanStatus: function(statusId) {
+        return $http({
+          method: 'GET',
+          url: API_URI_BASE + 'api/ios_scan_status',
+          params: {id: statusId}
+        })
       }
     };
   }]);
