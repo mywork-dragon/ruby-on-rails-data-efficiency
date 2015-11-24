13ca72929b08a9767dad0b0d4b829f3e63e1d904
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-24 14:10:36 -0800

    Helper method to create_with_proxy

--- app/models/ios_device.rb
+++ app/models/ios_device.rb
@@ -9,4 +9,31 @@ class IosDevice < ActiveRecord::Base
 	# either dedicated for a one off scrape or for mass scrapes
 	enum purpose: [:one_off, :mass]
 
+  class << self
+
+    def create_with_proxy!(params)
+      softlayer_proxies = SoftlayerProxy.all
+
+      free_proxy = nil
+
+      softlayer_proxies.each do |softlayer_proxy|
+        if softlayer_proxy.ios_devices.empty?
+          free_proxy = softlayer_proxy
+          break
+        end
+      end
+
+      raise 'Cannot find a free Softlayer proxy' if free_proxy.nil?
+
+      ios_device = new(params)
+
+      ios_device.softlayer_proxy = free_proxy
+
+      ios_device.save!
+    end
+
+  end
+
+
+
 end
