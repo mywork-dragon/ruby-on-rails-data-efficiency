2af82f8fdb6e610bcb1344573f20556c5529b654
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-30 11:50:13 -0800

    switch to worker style

--- app/services/ios_tester_service.rb
+++ app/services/ios_tester_service.rb
@@ -1,18 +1,25 @@
 class IosTesterService
 
-  class << self
+  include Sidekiq::Worker
 
-    include IosWorker
+  sidekiq_options backtrace: true, queue: :ios_live_scan
 
-    def test_reserve
-      8.times.map do |x|
-        dev = nil
-        b = Benchmark.measure do
-          dev = reserve_device(:one_off)
-        end.real
+  include IosWorker
 
-        puts "Iteration #{x} took #{b} seconds to reserve device #{dev ? dev.id : 'none'}"
-      end
-    end
+  def perform(id)
+    reserve(id)
+  end
+
+  def reserve(id)
+    dev = nil
+    b = Benchmark.measure do
+      dev = reserve_device(:one_off)
+    end.real
+
+    puts "Iteration #{id} took #{b} seconds to reserve device #{dev ? dev.id : 'none'}"
+  end
+
+  def release_all
+    IosDevice.all.each {|x| x.update(in_use: false)}
   end
 end
