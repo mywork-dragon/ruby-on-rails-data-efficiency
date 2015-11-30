e713f6b960213c1cb9d902560b89c0d7fc20a9e7
mywork-dragon <dragonblueyounger@gmail.com>
2015-11-30 12:15:29 -0800

    get json checker

--- app/workers/ios_tester_service_worker.rb
+++ app/workers/ios_tester_service_worker.rb
@@ -7,7 +7,8 @@ class IosTesterServiceWorker
   include IosWorker
 
   def perform(id)
-    reserve(id)
+    # reserve(id)
+    check_app(id)
   end
 
   def reserve(id)
@@ -19,7 +20,31 @@ class IosTesterServiceWorker
     puts "Iteration #{id} took #{b} seconds to reserve device #{dev ? dev.id : 'none'}"
   end
 
+  def check_app(id)
+
+    data = nil
+    b = Benchmark.measure do
+      ios_app_id = 502
+      data = get_json(ios_app_id)
+    end.real
+
+    puts "Iteration #{id} took #{b} seconds to get data: #{data ? 'success' : 'empty'}"
+  end
+
   def release_all
     IosDevice.all.each {|x| x.update(in_use: false)}
+  end
+
+  def get_json(ios_app_id)
+    begin
+      app_identifier = IosApp.find(ios_app_id).app_identifier # TODO, uncomment this
+      url = "https://itunes.apple.com/lookup?id=#{app_identifier.to_s}&uslimit=1"
+
+      json = JSON.parse(Proxy.get_body_from_url(url))
+
+      json['results'].first
+    rescue
+      nil
+    end
   end
 end
