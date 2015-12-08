5d10a256ed14d415c6911969d6680c5350d51737
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-07 16:13:51 -0800

    SDK Details Page Query Button Platform Bug Fixed

--- public/app/app/scripts/controllers/sdk-details.js
+++ public/app/app/scripts/controllers/sdk-details.js
@@ -57,8 +57,9 @@ angular.module('appApp').controller("SdkDetailsCtrl", ["$http", "$routeParams", 
     };
     sdkDetailsCtrl.load();
 
-    sdkDetailsCtrl.submitSdkQuery = function() {
-      var path = API_URI_BASE + "app/app#/search?app=%7B%22sdkNames%22:%5B%7B%22id%22:" + sdkDetailsCtrl.sdkData.id + ",%22name%22:%22" + encodeURI(sdkDetailsCtrl.sdkData.name) + "%22%7D%5D%7D&company=%7B%7D&custom=%7B%7D&pageNum=1&pageSize=100&platform=%7B%22appPlatform%22:%22android%22%7D";
+    // Submits filtered search query via query string params
+    sdkDetailsCtrl.submitSdkQuery = function(platform) {
+      var path = API_URI_BASE + "app/app#/search?app=%7B%22sdkNames%22:%5B%7B%22id%22:" + sdkDetailsCtrl.sdkData.id + ",%22name%22:%22" + encodeURI(sdkDetailsCtrl.sdkData.name) + "%22%7D%5D%7D&company=%7B%7D&custom=%7B%7D&pageNum=1&pageSize=100&platform=%7B%22appPlatform%22:%22" + platform + "%22%7D";
       $window.location.href = path;
     };
 
