a1cd12025e7c1090ff8c6aebfe4991f602f3b1c3
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-14 18:19:43 -0800

    faster current apps query

--- app/models/ios_sdk.rb
+++ app/models/ios_sdk.rb
@@ -17,41 +17,14 @@ class IosSdk < ActiveRecord::Base
 
   enum source: [:cocoapods, :package_lookup]
 
-
   def get_current_apps(count_only: false)
 
-    # get all the successful snapshots that have the sdk
-    snaps = IpaSnapshot.find(IosSdksIpaSnapshot.where(ios_sdk_id: self.id).map {|row| row.ipa_snapshot_id})
-
-    # get the latest snapshot for each app that found
-    app_to_snap = snaps.reduce({}) do |memo, snapshot|
-      current_value = memo[snapshot.ios_app_id]
-
-      if !snapshot.scanned? # WOAHHH
-        nil # do nothing. we don't count this result
-      elsif current_value.present? # store only the most recent one
-
-        if current_value.good_as_of_date == snapshot.good_as_of_date
-          memo[snapshot.ios_app_id] = snapshot if snapshot.id > current_value.id
-        else
-          memo[snapshot.ios_app_id] = snapshot if snapshot.good_as_of_date > current_value.good_as_of_date
-        end
-      else
-        memo[snapshot.ios_app_id] = snapshot
-      end
-
-      memo
+    if count_only
+      self.ipa_snapshots.select('ios_app_id, max(good_as_of_date) as good_as_of_date').where(scan_status: 1).group(:ios_app_id).length
+    else
+      # TODO: revisit this to make it 1 query
+      IosApp.where(id: self.ipa_snapshots.select('ios_app_id, max(good_as_of_date) as good_as_of_date').where(scan_status: 1).group(:ios_app_id).pluck(:ios_app_id)
     end
-
-    # only keep those where the snapshot is the app's last snapshot
-    app_to_snap.select! do |ios_app_id, snapshot|
-      last_snap = IosApp.find(ios_app_id).get_last_ipa_snapshot(scan_success: true)
-      last_snap.id == snapshot.id ? true : false
-    end
-
-    count_only ? app_to_snap.keys.length : IosApp.find(app_to_snap.keys)
   end
-
-  
 
 end
