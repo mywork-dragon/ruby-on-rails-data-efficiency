1c5a0a6c6439ee4c2857150f4fee3c1dc212e04e
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-14 19:00:11 -0800

    Bug Fix for iOS Filtered Count

--- app/models/ios_sdk.rb
+++ app/models/ios_sdk.rb
@@ -22,8 +22,8 @@ class IosSdk < ActiveRecord::Base
     if count_only
       self.ipa_snapshots.select('ios_app_id, max(good_as_of_date) as good_as_of_date').where(scan_status: 1).group(:ios_app_id).length
     elsif filtered_count_only
-      ios_apps = IosApp.where(id: self.ipa_snapshots.select('ios_app_id, max(good_as_of_date) as good_as_of_date').where(scan_status: 1).group(:ios_app_id).pluck(:ios_app_id)).map(&id)
-      IosApp.instance_eval("self.includes(:ios_fb_ad_appearances, newest_ios_app_snapshot: :ios_app_categories, websites: :company).joins(:newest_ios_app_snapshot).where('ios_app_snapshots.name IS NOT null').joins(websites: :company).where('ios_apps.id IN (?)', #{ios_apps}).group('ios_apps.id').count.length")
+      ios_app_ids = IosApp.where(id: self.ipa_snapshots.select('ios_app_id, max(good_as_of_date) as good_as_of_date').where(scan_status: 1).group(:ios_app_id).pluck(:ios_app_id)).map(&:id)
+      IosApp.instance_eval("self.includes(:ios_fb_ad_appearances, newest_ios_app_snapshot: :ios_app_categories, websites: :company).joins(:newest_ios_app_snapshot).where('ios_app_snapshots.name IS NOT null').joins(websites: :company).where('ios_apps.id IN (?)', #{ios_app_ids}).group('ios_apps.id').count.length")
     else
       # TODO: revisit this to make it 1 query
       IosApp.where(id: self.ipa_snapshots.select('ios_app_id, max(good_as_of_date) as good_as_of_date').where(scan_status: 1).group(:ios_app_id).pluck(:ios_app_id))
