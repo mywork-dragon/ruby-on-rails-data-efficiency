408671e9dcd7ab09fbe032dfaac10d19744f1e56
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-18 15:05:47 -0800

    fix comparison error

--- app/services/ios_sdk_service.rb
+++ app/services/ios_sdk_service.rb
@@ -5,68 +5,6 @@ class IosSdkService
   class << self
 
     # for front end - getting sdks data into display type
-=begin
-    def get_sdk_response(ios_app_id)
-      resp = {
-        installed_sdks: [],
-        updated: nil,
-        error_code: nil
-      }
-
-      error_map = {
-        paid: 0,
-        taken_down: 1,
-        foreign: 2,
-        device_incompatible: 3,
-        not_ios: 4
-      }
-
-      # return error if it violates some conditions
-      app = IosApp.find(ios_app_id)
-
-      if app.display_type != "normal"
-        resp[:error_code] = error_map[app.display_type.to_sym]
-        return resp
-      end
-
-      price = Rails.env.production? ? app.newest_ios_app_snapshot.price.to_i : 0
-
-      if !price.zero?
-        resp[:error_code] = error_map[:paid]
-        return resp
-      end
-
-      snap = app.get_last_ipa_snapshot(scan_success: true)
-
-      # if no successful scan's done, return no data
-      if !snap.nil?
-
-        partitions = snap.ios_sdks.reduce({os: [], non_os: []}) do |memo, sdk|
-          if sdk.present? && !sdk.flagged
-            if has_os_favicon?(sdk.favicon)
-              memo[:os].push(sdk)
-            else
-              memo[:non_os].push(sdk)
-            end
-          end
-
-          memo
-        end
-
-        %i(os non_os).each do |property|
-          # use sort_by because it's an expensive operation and it's more efficient than sort for this type
-          partitions[property] = partitions[property].sort_by do |sdk|
-            sdk.get_current_apps(count_only: true)
-          end
-        end
-        
-        resp[:installed_sdks] = partitions[:non_os] + partitions[:os]
-        resp[:updated] = snap.good_as_of_date
-      end
-
-      resp
-    end
-=end
     def get_sdk_response(ios_app_id)
       resp = {
         installed_sdks: [],
@@ -124,7 +62,7 @@ class IosSdkService
         partioned_uninstalled_sdks = partition_sdks(ios_sdks: uninstalled_sdks)
 
         # get store snapshots
-        store_snaps = IosAppSnapshot.select(:version, :released).where(ios_app_id: ios_app_id).where('released < ?', [max_current_date, max_last_date].max).order(released: :desc)
+        store_snaps = IosAppSnapshot.select(:version, :released).where(ios_app_id: ios_app_id).where('released < ?', [max_current_date, max_last_date].compact.max).order(released: :desc)
 
         # format the responses
         resp[:installed_sdks] = partioned_installed_sdks.map do |sdk|
