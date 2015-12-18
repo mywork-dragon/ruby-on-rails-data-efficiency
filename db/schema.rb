8e44891f54c24e56a74a6f12e1d555ccc18d94e4
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-18 11:15:10 -0800

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151217212944) do
     t.boolean  "can_view_sdks",           default: false, null: false
     t.boolean  "can_view_storewide_sdks", default: false
     t.boolean  "can_view_exports",        default: true
+    t.boolean  "god_mode",                default: false
   end
 
   add_index "accounts", ["name"], name: "index_accounts_on_name", using: :btree
