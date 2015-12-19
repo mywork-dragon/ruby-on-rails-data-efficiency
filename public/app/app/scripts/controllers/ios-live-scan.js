c7181a9fc0c7fa10ebffbaa6b74d5ae1f8bb0647
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-18 18:09:37 -0800

    noSdkSnapshot Logic Revised

--- public/app/app/scripts/controllers/ios-live-scan.js
+++ public/app/app/scripts/controllers/ios-live-scan.js
@@ -199,15 +199,13 @@ angular.module('appApp').controller("IosLiveScanCtrl", ["$scope", "$http", "$rou
               }
 
               $interval.cancel(interval); // Exits interval loop
-              console.log('INTERVAL CANClED!!!', 'Status:', data.status);
 
             } else if(data.status == 1 || data.status == 10 || data.status == 11) { // if status 1, 9 or 10
               // Run for any qualifying status
               iosLiveScanCtrl.sdkQueryInProgress = false;
-              iosLiveScanCtrl.noSdkSnapshot = (!iosLiveScanCtrl.sdkData.sdkCompanies.length || !iosLiveScanCtrl.sdkData.sdkOpenSource.length); // Will show/hide view elements depending on data returned
+              iosLiveScanCtrl.noSdkSnapshot = !iosLiveScanCtrl.sdkData.sdkCompanies || !iosLiveScanCtrl.sdkData.sdkCompanies.length || !iosLiveScanCtrl.sdkData.sdkOpenSource || !iosLiveScanCtrl.sdkData.sdkOpenSource.length; // Will show/hide view elements depending on data returned
 
               $interval.cancel(interval); // Exits interval loop
-              console.log('INTERVAL CANClED!!!', 'Status:', data.status);
 
             }
           })
