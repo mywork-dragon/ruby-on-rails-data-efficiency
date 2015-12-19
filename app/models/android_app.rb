36462527bdd13b4dd52d967f35fd8bde86589137
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-19 13:49:34 -0800

    better installed query

--- app/models/android_app.rb
+++ app/models/android_app.rb
@@ -61,7 +61,7 @@ class AndroidApp < ActiveRecord::Base
   end
 
   def installed_sdks
-    newest_snap = self.newest_apk_snapshot
+    newest_snap = self.apk_snapshots.where(status: 1, scan_status: 1).last
     return nil if newest_snap.blank?
     newest_sdks = newest_snap.android_sdks
     sdk_apk = newest_sdks.map{|x| [x.id, newest_snap.id] }
@@ -69,7 +69,7 @@ class AndroidApp < ActiveRecord::Base
   end
 
   def uninstalled_sdks
-    newest_snap = self.newest_apk_snapshot
+    newest_snap = self.apk_snapshots.where(status: 1, scan_status: 1).last
     return nil if newest_snap.blank?
     newest_sdks = newest_snap.android_sdks
     snaps = self.apk_snapshots.where.not(id: newest_snap.id).map(&:id)
