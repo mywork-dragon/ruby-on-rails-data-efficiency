aaa9d1e6246b3967b405c20fcd12fd9dc7707807
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-20 00:10:50 -0800

    using DB instead

--- app/workers/package_search_service_worker.rb
+++ app/workers/package_search_service_worker.rb
@@ -2,8 +2,6 @@ class PackageSearchServiceWorker
 
   MAX_CONCURRENT_DOWNLOADS = 10
 
-  @concurrent_downloads = 0
-
   class << self
     attr_accessor :concurrent_downloads
   end
@@ -17,20 +15,32 @@ class PackageSearchServiceWorker
   end
 
   def wait_for_open_download_spot
+    private_ip = ip
+
     # ooh an infinite loop!
       while true
+        sdk_scraper = SdkScraper.find_by_private_ip(private_ip)
+        
+        concurrent_downloads = sdk_scraper.concurrent_downloads
 
-        if self.class.concurrent_downloads < MAX_CONCURRENT_DOWNLOADS
-          self.class.concurrent_downloads += 1
+        if concurrent_downloads < MAX_CONCURRENT_DOWNLOADS
+          sdk_scraper.concurrent_downloads = concurrent_downloads + 1
+          sdk_scraper.save
           break
         end
 
-        sleep(rand(0.1..0.125)) # sleep for a random time
+        sleep(rand(0.5..2.0)) # sleep for a random time
       end
   end
 
   def decrement_concurrent_downloads
-    self.class.concurrent_downloads -= 1
+    sdk_scraper = SdkScraper.find_by_private_ip(ip)
+    sdk_scraper.concurrent_downloads = concurrent_downloads + 1
+    sdk_scraper.save
+  end
+
+  def ip
+    `hostname -I`.strip
   end
 
 
