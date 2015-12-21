78f77a8592f4ded20f3249d62baa69f43910c35d
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-20 20:49:19 -0800

    uncomment stuff

--- app/workers/package_search_worker.rb
+++ app/workers/package_search_worker.rb
@@ -22,24 +22,23 @@ module PackageSearchWorker
       end
     end
 
-    # # puts "#{snap_id} => downloaded [#{a.real}]"
+    # puts "#{snap_id} => downloaded [#{a.real}]"
 
-    # dex = apk.dex
-    # packages = dex.classes.map do |cls|
-    #   next if cls.name.blank? || cls.name.downcase.include?(app_identifier.split('.')[1].downcase)
-    #   cls = cls.name.split('/')
-    #   cls.pop
-    #   cls = cls.join('.')
-    #   cls.slice!(0) if cls.slice(0) == 'L'
-    #   cls
-    # end.compact.uniq
+    packages = apk.dex.classes.map do |cls|
+      next if cls.name.blank? || cls.name.downcase.include?(app_identifier.split('.')[1].downcase)
+      cls = cls.name.split('/')
+      cls.pop
+      cls = cls.join('.')
+      cls.slice!(0) if cls.slice(0) == 'L'
+      cls
+    end.compact.uniq
 
-    # AndroidSdkService.classify(snap_id: snap_id, packages: packages)
+    AndroidSdkService.classify(snap_id: snap_id, packages: packages)
 
-    # # apk_snap = ApkSnapshot.find_by_id(snap_id)
-    # apk_snap.scan_status = :scan_success
-    # apk_snap.last_updated = DateTime.now
-    # apk_snap.save
+    # apk_snap = ApkSnapshot.find_by_id(snap_id)
+    apk_snap.scan_status = :scan_success
+    apk_snap.last_updated = DateTime.now
+    apk_snap.save
 
   end
 
