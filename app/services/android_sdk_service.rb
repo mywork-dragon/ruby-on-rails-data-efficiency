37be8d40dbc2465237eba24de730829409ae1fb4
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-20 23:40:24 -0800

    only catch RecordNotUnique

--- app/services/android_sdk_service.rb
+++ app/services/android_sdk_service.rb
@@ -10,16 +10,15 @@ class AndroidSdkService
 
 		def classify(snap_id:, packages:)
 
-      puts "#{snap_id} => starting scan"
+      # puts "#{snap_id} => starting scan"
 
 			# Save package if it matches a regex
       regex_check = miss_match(data: packages, check: :match_regex)
-  		# if regex_check[:matched].present?
-    #     # puts "#{snap_id} => regexes were matched"
-  		# 	regex_check[:matched].each do |p| 
-  		# 		save_package(package: p[:package], android_sdk_id: p[:android_sdk_id], snap_id: snap_id)
-  		# 	end
-    #   end
+  		if regex_check[:matched].present?
+  			regex_check[:matched].each do |p| 
+  				save_package(package: p[:package], android_sdk_id: p[:android_sdk_id], snap_id: snap_id)
+  			end
+      end
 
       # puts "#{snap_id} => regex [#{a.real}]" 
 
@@ -67,29 +66,26 @@ class AndroidSdkService
 		def save_package(package:, android_sdk_id:, snap_id:)
 
       # save sdk_packages
-    	begin
-    		SdkPackage.create(package: package)
-    	rescue ActiveRecord::RecordNotUnique => e
-    		nil
-    	end
-
-      sdk_package = SdkPackage.where(package: package).first
-    	if sdk_package.android_sdk_id != android_sdk_id
-	    	sdk_package.android_sdk_id = android_sdk_id
-	    	sdk_package.save
-	    end
+      sdk_package = begin
+        s = SdkPackage.create(package: package)
+        s.android_sdk_id = android_sdk_id
+        s.save
+        s
+      rescue ActiveRecord::RecordNotUnique => e
+        SdkPackage.where(package: package).first
+      end
 
       # save sdk_packages_apk_snapshots
       begin
         SdkPackagesApkSnapshot.create(sdk_package_id: sdk_package.id, apk_snapshot_id: snap_id)
-      rescue
+      rescue ActiveRecord::RecordNotUnique => e
         nil
       end
 
       # save android_sdks_apk_snapshots
       begin
-        AndroidSdksApkSnapshot.create(android_sdk_id: android_sdk_id, apk_snapshot_id: snap_id) if android_sdk_id && snap_id
-      rescue
+        AndroidSdksApkSnapshot.create(android_sdk_id: android_sdk_id, apk_snapshot_id: snap_id)
+      rescue ActiveRecord::RecordNotUnique => e
         nil
       end
       
