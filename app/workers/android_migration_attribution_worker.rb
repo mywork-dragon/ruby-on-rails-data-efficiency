802cfa83acacd900a60184a189e960d8075c8e35
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-22 16:04:27 -0800

    handle duplicates

--- app/workers/android_migration_attribution_worker.rb
+++ app/workers/android_migration_attribution_worker.rb
@@ -7,7 +7,13 @@ class AndroidMigrationAttributionWorker
     sdk = AndroidSdk.find(android_sdk_id)
     correct_sdk_id = sdk.name.split(':').second
     AndroidSdksApkSnapshot.where(android_sdk_id: sdk.id).find_each do |row|
-      row.update(android_sdk_id: correct_sdk_id)
+      exists = AndroidSdksApkSnapshot.where(apk_snapshot_id: row.apk_snapshot_id, android_sdk_id: correct_sdk_id).take
+
+      if exists.nil?
+        row.update(android_sdk_id: correct_sdk_id) 
+      else
+        row.delete
+      end
     end
   end
 end
