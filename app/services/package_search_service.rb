68ca7cec0212da4771709c837a37ebde5882a2cb
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-29 04:33:16 -1000

    optional return of ids array

--- app/services/package_search_service.rb
+++ app/services/package_search_service.rb
@@ -5,10 +5,22 @@ class PackageSearchService
     def run(n = 200)
       # SdkScraper.all.each{ |x| x.concurrent_apk_downloads = 0; x.save }
       # AndroidApp.where(newest_apk_snapshot_id: nil).joins(:newest_apk_snapshot).where('apk_snapshots.scan_version = ?',1).limit(n).each.with_index do |app, index|
+      
+      return_ids_array = false  # whether to return an array of ids (only do for small sample size)
+
+      if n < 5000
+        return_ids_array = true 
+        android_app_ids
+      end
+
       AndroidApp.where.not(newest_apk_snapshot_id: nil).limit(n).each.with_index do |app, index|
         puts index if index % 1e3 == 0
         PackageSearchServiceWorker.perform_async(app.id)
+
+        android_app_ids << app.id if return_ids_array
       end
+
+      return_ids_array ? android_app_ids : true
     end
 
     def android_by_app_id(ids)
