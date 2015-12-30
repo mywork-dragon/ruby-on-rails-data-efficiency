0d16dc3c1182873d5aa45b94c694ca5bb5267533
mywork-dragon <dragonblueyounger@gmail.com>
2015-12-30 11:02:40 -1000

    Add more search options

--- app/services/proxy_google_searcher_test_service.rb
+++ app/services/proxy_google_searcher_test_service.rb
@@ -26,6 +26,14 @@ class ProxyGoogleSearcherTestService
     #     cmcmcmmcmcmalsalsamflkamslkm
     #     )
 
+    # sdks = [
+    #   'site:github.com android',
+    #   'site:github.com blah',
+    #   'site:github.com window',
+    #   'site:github.com view',
+    #   'site:github.com mixpanel'
+    # ]
+
     results = sdks.each do |sdk|
       ap GoogleSearcher::Searcher.search("#{sdk} ios sdk", proxy_type: :android_classification)
       sleep(rand(1.0..2.0)) # be nice to google
