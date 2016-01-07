f6f82a4809043e55d8a848089e9f82e21ffc232a
mywork-dragon <dragonblueyounger@gmail.com>
2016-01-07 09:55:33 -0800

    better class dump arch selection

--- app/services/ios_device_service.rb
+++ app/services/ios_device_service.rb
@@ -826,7 +826,9 @@ class IosDeviceService
 
   def class_dump(src, dest)
     arch = @device.class_dump_arch || "arm64"
-    `/usr/local/bin/gtimeout 1m /usr/local/bin/class-dump --arch #{arch} \'#{src}\' > \'#{dest}\'`
+    accepted_arches = `/usr/local/bin/class-dump --list-arches \'#{src}\'`.chomp.split("\n")
+    arch_flag = accepted_arches.include?(arch) ? "--arch #{arch}" : "" # let class-dump decide if not
+    `/usr/local/bin/gtimeout 1m /usr/local/bin/class-dump #{arch_flag} \'#{src}\' > \'#{dest}\'`
   end
 
 end
