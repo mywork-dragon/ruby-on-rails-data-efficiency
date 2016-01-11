5084fd7324ef3862de42f89872c2e15c0af4805c
mywork-dragon <dragonblueyounger@gmail.com>
2016-01-11 14:04:43 -0800

    do local for all mac minis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,22 +2,12 @@ if Rails.env.production?
 
   if `uname`.chomp == 'Darwin'  # Is a Mac (is Darth Vader)
 
-    if false && `echo $USER`.include?('kylo')
-      Sidekiq.configure_server do |config|
-        config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
-      end
+    Sidekiq.configure_server do |config|
+      config.redis = { url: 'redis://localhost:6379' }
+    end
 
-      Sidekiq.configure_client do |config|
-        config.redis = { url: 'redis://varys-production.bsqwsz.0001.use1.cache.amazonaws.com:6379' }
-      end
-    else
-      Sidekiq.configure_server do |config|
-        config.redis = { url: 'redis://localhost:6379' }
-      end
-
-      Sidekiq.configure_client do |config|
-        config.redis = { url: 'redis://localhost:6379' }
-      end
+    Sidekiq.configure_client do |config|
+      config.redis = { url: 'redis://localhost:6379' }
     end
 
   else  # Is a cloud server
