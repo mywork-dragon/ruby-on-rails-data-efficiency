567df1475e958bb5db10ab2ed8293fb04eb35398
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-03 16:08:33 -0800

    added utilities for manual cocoapod creation. also added manual as ios sdk source

--- app/models/ios_sdk.rb
+++ app/models/ios_sdk.rb
@@ -15,12 +15,26 @@ class IosSdk < ActiveRecord::Base
   has_many :ios_sdk_source_matches, foreign_key: :source_sdk_id
   has_many :source_matches, through: :ios_sdk_source_matches, source: :match_sdk
 
-  enum source: [:cocoapods, :package_lookup]
+  enum source: [:cocoapods, :package_lookup, :manual]
 
   # TODO: currently returns taken down. Maybe revisit and fix later
   def get_current_apps
     # TODO: revisit this to make it 1 query
     IosApp.where(id: self.ipa_snapshots.select('ios_app_id, max(good_as_of_date) as good_as_of_date').where(scan_status: 1).group(:ios_app_id).pluck(:ios_app_id))
+  end
+
+  class << self
+    def create_manual(name:, website:, favicon: nil, open_source: nil, summary: nil, github_repo_identifier: nil)
+      IosSdk.create!({
+        name: name,
+        website: website,
+        favicon: favicon || FaviconService.get_favicon_from_url(url: website),
+        open_source: open_source || /(?:bitbucket|github|sourceforge)/.match(website),
+        summary: summary,
+        github_repo_identifier: github_repo_identifier,
+        source: :manual
+        })
+    end
   end
 
 end
