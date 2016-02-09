f24d556de57b4c0a34587ac099b78c1d14857362
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-09 00:09:55 -0800

    open url

--- app/workers/package_search_worker.rb
+++ app/workers/package_search_worker.rb
@@ -38,13 +38,13 @@ module PackageSearchWorker
         apk = apk_file.apk
         puts "apk"
         raise NoApk if !apk.exists?
-        apk_file = open(apk)
+        apk_file = open(apk.url)
         puts "opened"
         classify(zip_file: apk_file, android_app: android_app, apk_ss: apk_snap)
       elsif version == :zip
         zip = apk_file.zip
         raise NoZip if !zip.exists?
-        zip_file = open(zip)
+        zip_file = open(zip.url)
         classify(zip_file: zip_file, android_app: android_app, apk_ss: apk_snap)
       end
       
