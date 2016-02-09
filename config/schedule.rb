f998e8a483c3ba5504913963139d1fea24639779
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-09 11:04:07 -0800

    fix cron command

--- config/schedule.rb
+++ config/schedule.rb
@@ -35,8 +35,8 @@ every :day, :at => '6:05am', roles: [:scraper, :sdk_scraper, :sdk_scraper_live_s
 end
 
 # delete old snapshot files older than 1 day on the dark-side machines
-every :day, :at => '9:00am', roles: [:kylo_ren, :darth_vader] do
-  command 'rm -rf /tmp/*.classdump.txt /tmp/*.strings.txt /tmp/*.tree.txt /tmp/*_decrypted /tmp/*.decrypted', :output => '/var/log/varys/cron.log'
+every :day, :at => '9:00am', roles: [:kylo_ren, :darth_vader] do/
+  command 'find /tmp/ -mtime +0 \( -name \'*.decrypted\' -or -name \'*.classdump.txt\' -or -name \'*.tree.txt\' -or -name \'*.strings.txt\' -or -name \'*_decrypted\' \)', :output => '/var/log/varys/cron.log'
 end
 
 every 6.hours, roles: [:kylo_ren] do
