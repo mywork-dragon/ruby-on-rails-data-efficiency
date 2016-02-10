0c671270b105a7ac970ba0b9388f94cc15d25033
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-10 15:03:27 -0800

    uncomment functions

--- app/workers/ios_reclassification_service_worker.rb
+++ app/workers/ios_reclassification_service_worker.rb
@@ -2,15 +2,15 @@ class IosReclassificationServiceWorker < IosMassClassificationServiceWorker
 
   # only classify the below sources
   def classify_all_sources(ipa_snapshot_id:, classdump:, summary:)
-    # classdump_sdks = classify_classdump(summary['binary']['classdump'])
+    classdump_sdks = classify_classdump(summary['binary']['classdump'])
     files_sdks = sdks_from_files(summary['files'])
     strings_regex_sdks = sdks_from_string_regex(summary['binary']['strings'])
-    # js_tag_sdks = sdks_from_js_tags(ipa_snapshot_id, summary['files'])
+    js_tag_sdks = sdks_from_js_tags(ipa_snapshot_id, summary['files'])
     dll_sdks = sdks_from_dlls(ipa_snapshot_id, summary['files'])
 
-    # attribute_sdks_to_snap(snap_id: ipa_snapshot_id, sdks: classdump_sdks, method: :classdump)
+    attribute_sdks_to_snap(snap_id: ipa_snapshot_id, sdks: classdump_sdks, method: :classdump)
     attribute_sdks_to_snap(snap_id: ipa_snapshot_id, sdks: files_sdks, method: :file_regex)
-    # attribute_sdks_to_snap(snap_id: ipa_snapshot_id, sdks: js_tag_sdks method: :js_tag_regex)
+    attribute_sdks_to_snap(snap_id: ipa_snapshot_id, sdks: js_tag_sdks method: :js_tag_regex)
     attribute_sdks_to_snap(snap_id: ipa_snapshot_id, sdks: dll_sdks, method: :dll_regex)
     attribute_sdks_to_snap(snap_id: ipa_snapshot_id, sdks: strings_regex_sdks, method: :string_regex)
   end
