50df298ae13e1da40f4dbc3caad76c44c197fc55
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-16 22:33:44 -0800

    helper works

--- app/services/sdk_helper_service.rb
+++ app/services/sdk_helper_service.rb
@@ -3,20 +3,18 @@ class SdkHelperService
   class << self
 
     def dll_freqs(name)
-      freq_hash(like_query(SdkDll, name), ApkSnapshotsSdkDll, :sdk_dll_id)
+      freq_hash(like_query(SdkDll, name), ApkSnapshotsSdkDll, :sdk_dll_id, :name)
     end
 
     def like_query(the_class, name)
       the_class.where('name LIKE ?', "%#{name}%")
     end
 
-    def freq_hash(items, join_table_class, join_table_foreign_key)
+    def freq_hash(items, join_table_class, join_table_foreign_key, name_column)
       ret = {}
-      items.each do |item|
-        ret[item] = {item => join_table_class.where(join_table_foreign_key => item.id).count}
-      end
-      # ret.sort_by{ |k, v| v}
-      ret
+      items.map do |item|
+        ret[item] = {id: item.id, name_column => item.send(name_column), count: join_table_class.where(join_table_foreign_key => item.id).count}
+      end.sort_by{ |x| -x[:count]}
     end
 
   end
