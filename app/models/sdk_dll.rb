4fec6f545072f840f503caa1b0859fcd8f696a1d
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-17 10:44:35 -0800

    add helper methods for searching for dlls and tags

--- app/models/sdk_dll.rb
+++ app/models/sdk_dll.rb
@@ -6,4 +6,12 @@ class SdkDll < ActiveRecord::Base
   has_many :ipa_snapshots_sdk_dlls
   has_many :ipa_snapshots, through: :ipa_snapshots_sdk_dlls
 
+  class << self
+    def search_for_ios_dlls(sdk_name, limit: 20)
+      results = SdkDll.joins(:ipa_snapshots_sdk_dlls).where(id: SdkDll.where("name LIKE '%#{sdk_name}%'")).group(:sdk_dll_id).count
+
+      results.sort_by {|k,v| -v}.first(20).reduce({}) {|memo, arr| dll = SdkDll.find(arr[0]); memo[dll.name] = arr[1]; memo;}
+    end
+  end
+
 end
--- app/models/sdk_js_tag.rb
+++ app/models/sdk_js_tag.rb
@@ -6,4 +6,12 @@ class SdkJsTag < ActiveRecord::Base
   has_many :ipa_snapshots_sdk_js_tags
   has_many :ipa_snapshots, through: :ipa_snapshots_sdk_js_tags
 
+  class << self
+    def search_for_ios_tags(sdk_name, limit: 20)
+      results = SdkJsTag.joins(:ipa_snapshots_sdk_js_tags).where(id: SdkJsTag.where("name LIKE '%#{sdk_name}%'")).group(:sdk_js_tag_id).count
+
+      results.sort_by {|k,v| -v}.first(20).reduce({}) {|memo, arr| tag = SdkJsTag.find(arr[0]); memo[tag.name] = arr[1]; memo;}
+    end
+  end
+
 end
