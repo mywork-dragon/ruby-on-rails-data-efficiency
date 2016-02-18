d39add35d9df39327670ca53aa9e85b91139f51d
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-17 21:57:08 -0800

    sdk_helper

--- app/services/sdk_helper_service.rb
+++ app/services/sdk_helper_service.rb
@@ -2,15 +2,25 @@ class SdkHelperService
 
   class << self
 
-    def dll_freqs(name)
-      freq_hash(like_query(SdkDll, name), ApkSnapshotsSdkDll, :sdk_dll_id, :name)
+    def sdk_package_freq(name)
+      freq_hash(SdkPackage, SdkPackagesApkSnapshot, :sdk_package_id, :package)
     end
 
-    def like_query(the_class, name)
-      the_class.where('name LIKE ?', "%#{name}%")
+    def dll_freq(name)
+      freq_hash(SdkDll, ApkSnapshotsSdkDll, :sdk_dll_id, :name)
     end
 
-    def freq_hash(items, join_table_class, join_table_foreign_key, name_column)
+    def js_tag_freq(name)
+      freq_hash(SdkJsTag, ApkSnapshotsSdkJsTag, :sdk_js_tag_id, :name)
+    end
+
+    def like_query(the_class, name_column, name)
+      the_class.where("#{name_column.to_s} LIKE ?", "%#{name}%")
+    end
+
+    def freq_hash(item_class, join_table_class, join_table_foreign_key, name_column)
+      items = like_query(item_class, name_column, name)
+
       ret = {}
       items.map do |item|
         ret[item] = {id: item.id, name_column => item.send(name_column), count: join_table_class.where(join_table_foreign_key => item.id).count}
