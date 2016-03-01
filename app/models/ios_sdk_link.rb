45b4031c383ec8fc6aa1e482f56fea5eb0574216
mywork-dragon <dragonblueyounger@gmail.com>
2016-02-29 18:08:03 -0800

    add hacky validation

--- app/models/ios_sdk_link.rb
+++ app/models/ios_sdk_link.rb
@@ -1,4 +1,10 @@
 class IosSdkLink < ActiveRecord::Base
   belongs_to :source_sdk, class_name: 'IosSdk'
   belongs_to :dest_sdk, class_name: 'IosSdk'
+
+  before_create :ensure_no_chains
+
+  def ensure_no_chains
+    raise "Cannot link to an sdk that itself links to another" if IosSdkLink.where(source_sdk_id: self.dest_sdk_id).count > 0
+  end
 end
