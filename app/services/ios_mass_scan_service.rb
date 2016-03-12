6a978d481c65808be8ae0d6823ffb48afb5ebbe7
mywork-dragon <dragonblueyounger@gmail.com>
2016-03-11 17:42:47 -0800

    add recently released

--- app/services/ios_mass_scan_service.rb
+++ app/services/ios_mass_scan_service.rb
@@ -39,6 +39,15 @@ class IosMassScanService
       run_ids("Running #{n} at #{Time.now.strftime '%m/%d/%Y %H:%M %Z'}", mb_high_by_ratings)
     end
 
+    def scan_recently_released(lookback_time: 1.week.ago, ratings_min: 0)
+      recent = IosApp.joins(:newest_ios_app_snapshot).where('ios_apps.released > ?', 2.week.ago).where('ios_app_snapshots.ratings_all_count > ?', ratings_min).pluck(:id)
+
+      puts "Got #{recent.count} apps: Continue? [y/n]"
+      return unless gets.chomp.include?('y')
+
+      run_ids("Running #{recent.count} apps released after #{lookback_time}", recent)
+    end
+
     def run_recently_updated(n: 5000, ratings_min: 0)
       recent = IosApp.joins(:newest_ios_app_snapshot).where('ios_app_snapshots.released > ?', 2.week.ago).order('ios_app_snapshots.ratings_all_count DESC').limit(n).pluck(:id)
 
