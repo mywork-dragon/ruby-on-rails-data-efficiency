f217f5fce9f4d46a5c913e2e7be277358dcee506
mywork-dragon <dragonblueyounger@gmail.com>
2016-03-18 15:54:32 -0400

    set concurrency to 10

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,9 +51,10 @@ set :sdk_scraper_live_scan_concurrency, 30
 set :scraper_concurrency, 50
 set :scraper_master_concurrency, 50
 set :web_concurrency, 5
-set :darth_vader_concurrency, 10  # needs to be >= number of iOS devices
-set :kylo_ren_concurrency, 10     # needs to be >= number of iOS devices
+set :darth_vader_concurrency, 10
+set :kylo_ren_concurrency, 10
 set :ios_live_scan_concurrency, 50
+set :monitor_concurrency, 10
 
 # set :sidekiq_queue, %w(critical default low)
 
