449f4e672f4bafedaa8b027119cde870bfa88dca
mywork-dragon <dragonblueyounger@gmail.com>
2016-03-24 13:59:19 -0700

    remove custom auth

--- app/controllers/ewok_controller.rb
+++ app/controllers/ewok_controller.rb
@@ -1,8 +1,7 @@
 class EwokController < ApplicationController
 
   skip_before_filter  :verify_authenticity_token
-  before_action :authenticate_ewok, only: [:ewok_app_page, :ewok_check_exists]
-  before_action :set_current_user, only: [:ewok_app_page]
+  before_action :authenticate_ewok, only: [:ewok_app_page]
 
   def authenticate_ewok
     key = params['key']
@@ -10,17 +9,6 @@ class EwokController < ApplicationController
   end
 
   def ewok_app_page
-    # redirect to landing if app doesn't exist (if authenticate_request throws exception)
-    begin 
-      authenticate_request
-    rescue => e
-      puts e.message
-      puts e.backtrace
-      puts "redirect root"
-      redirect_to root_url
-      return
-    end
-
     url = params['url']
     app_url = EwokService.app_url(url)
 
