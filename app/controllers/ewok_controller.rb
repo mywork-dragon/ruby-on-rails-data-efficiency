9c01185a88b176b7b16cec89c5acbdf95bddeb9f
mywork-dragon <dragonblueyounger@gmail.com>
2016-03-24 13:11:06 -0700

    maybe this will work

--- app/controllers/ewok_controller.rb
+++ app/controllers/ewok_controller.rb
@@ -1,7 +1,8 @@
-class EwokController < ActionController::Base
+class EwokController < ApplicationController
 
   skip_before_filter  :verify_authenticity_token
-  before_action :authenticate_ewok only: [:ewok_app_page, :ewok_check_exists]
+  before_action :authenticate_ewok, only: [:ewok_app_page, :ewok_check_exists]
+  before_action :set_current_user, only: [:ewok_app_page]
 
   def authenticate_ewok
     fail InvalidKey.new(key: key) unless EwokService.correct_key?(key)
@@ -9,12 +10,27 @@ class EwokController < ActionController::Base
 
   # @osman
   def ewok_check_app_exists
-    
   end
 
   def ewok_app_page
+    # redirect to landing if app doesn't exist (if authenticate_request throws exception)
+    begin 
+      authenticate_request
+    rescue => e
+      redirect_to root_url
+      return
+    end
 
-    render json: EwokService.app_page.to_json
+    url = params['url']
+    app_url = EwokService.app_url(url)
+
+    if app_url.nil?    
+      redirect_to :currently_scraping 
+      return
+    end
+
+    redirect_to app_url
+    render nothing: true
   end
 
   class InvalidKey < StandardError
--- app/services/ewok_service.rb
+++ app/services/ewok_service.rb
@@ -9,6 +9,51 @@ class EwokService
       key == KEY
     end
 
+    def app_id_and_store(url)
+      if md = url.match(/itunes\.apple\.com\/.*id(\d+)/)
+        app_identifier = md.captures.first
+        ios_app = IosApp.find_by_app_identifier(app_identifier)
+        return ios_app ? {id: ios_app.id, store: :ios} : nil
+      elsif md = url.match(/play.google.com\/store\/apps\/details\?id=([^&]*)/)
+        app_identifer = md.captures.first
+        android_app = AndroidApp.find_by_app_identifier(app_identifier)
+        return android_app ? {id: android_app.id, store: :android} : nil
+      end
+      nil
+    end
+
+    def app_url(url)
+      ais = app_id_and_store(url)
+      
+      return nil if ais.nil?
+
+      id = ais[:id]
+      store = ais[:store]
+
+      if store == :ios
+        ret = "http://mightysignal.com/app/app#/app/ios/#{id}"
+      elsif store == :android
+        ret = "http://mightysignal.com/app/app#/app/android/#{id}" 
+      end
+
+      ret
+    end
+
+    def ios_app_url(id)
+    end
+
+    def android_app_url
+
+    def store
+      if 
+    end
+
+    def ios_app_in_db?
+    end
+
+    def android_app_in_db?
+    end
+
     def app_page
 
     end
