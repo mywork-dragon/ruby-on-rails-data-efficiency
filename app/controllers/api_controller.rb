ecb31bfea509cac7ddad479bd0f9ff3771a382fe
mywork-dragon <dragonblueyounger@gmail.com>
2016-03-23 23:49:06 -0700

    slightly better elasticsearch matching

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1354,30 +1354,21 @@ class ApiController < ApplicationController
   end
 
   def search_ios_sdk
-    query = params['query']
+    query = params['query'].downcase
     page = !params['page'].nil? ? params['page'].to_i : 1
     num_per_page = !params['numPerPage'].nil? ? params['numPerPage'].to_i : 100
 
     result_ids = AppsIndex::IosSdk.query(
-      multi_match: {
-        query: query,
-        operator: 'and',
-        fields: [:name],
-        type: 'cross_fields',
-        fuzziness: 1
+      query_string: {
+        query: "#{query}*",
+        default_field: 'name',
+        analyze_wildcard: true
       }
     ).limit(num_per_page).offset((page - 1) * num_per_page)
     total_sdks_count = result_ids.total_count # the total number of potential results for query (independent of paging)
     result_ids = result_ids.map { |result| result.attributes["id"] }
 
-    ios_sdks = result_ids.map{ |id| IosSdk.find_by_id(id) }.compact
-
-    sdks = []
-    ios_sdks.each do |sdk|
-      sdks << IosSdk.find(sdk)
-    end
-
-    total_apps_count = sdks.length
+    sdks = IosSdk.where(id: result_ids)
 
     results_json = []
     sdks.each do |sdk|
