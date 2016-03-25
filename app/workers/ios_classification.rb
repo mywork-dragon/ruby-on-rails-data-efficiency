5a3cfc5eb5518a50e373c1edb572641d696de8c7
mywork-dragon <dragonblueyounger@gmail.com>
2016-03-24 18:08:20 -0700

    another one

--- app/workers/ios_classification.rb
+++ app/workers/ios_classification.rb
@@ -13,11 +13,13 @@ module IosClassification
 
       # invalidate bad scans
       invalidate_bad_scans(snapshot)
-      
+
       begin
         ActivityWorker.new.perform(:log_ios_sdks, snapshot.ios_app_id)
-      rescue
-        nil
+      rescue => e
+        "Activity Worker failed"
+        puts e.message
+        puts e.backtrace
       end
 
       # sdks
