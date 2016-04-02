6c5df061026e3b5c6e5fcd0d5319d8fd109ec075
mywork-dragon <dragonblueyounger@gmail.com>
2016-04-01 17:07:37 -0700

    create helper for demo accounts

--- app/services/auth_service.rb
+++ app/services/auth_service.rb
@@ -9,6 +9,24 @@ class AuthService
       false
       
     end
+
+    def create_accounts(account_name, *emails)
+      account = Account.find_or_create_by(name: account_name)
+      account.can_view_support_desk = false
+      account.can_view_ad_spend = true
+      account.can_view_sdks = true
+      account.can_view_storewide_sdks = true
+      account.can_view_exports = true
+      account.can_view_ios_live_scan = true
+
+      accounts = []
+      emails.each do |email|
+        password = SecureRandom.hex(8)
+        user = account.users.create(email: email, password: password)
+        accounts << "Email: #{email} Password: #{password} Worked: #{user.valid?}"
+      end
+      accounts
+    end
     
   end
   
