e8921f1c530264c8e163e3eb6a58c4a461a62654
mywork-dragon <dragonblueyounger@gmail.com>
2016-04-05 15:10:32 -0700

    New Android SDK DL queueing logic

--- app/services/apk_snapshot_service.rb
+++ app/services/apk_snapshot_service.rb
@@ -25,18 +25,19 @@ class ApkSnapshotService
       #puts Sidekiq::Queue.new('sdk').size
     end
 
-    def run_weekly
+    def run_new
       batch = Sidekiq::Batch.new
-      size = weekly_apps.count
-      notes = "Weekly DL #{Time.now.strftime("%m/%d/%Y")}"
+      size = weekly_new_apps.count
+      notes = "Weekly DL (New) #{Time.now.strftime("%m/%d/%Y")}"
+      GoogleAccount.where(blocked: false, scrape_type: :full)
       j = ApkSnapshotJob.create!(notes: notes, job_type: :mass)
-      batch.description = "Weekly Android SDK DL: ~#{size} APKs"
-      batch.on(:complete, "ApkSnapshotService#on_complete_run_weekly", 'apk_snapshot_job_id' => j.id, 'apps_queued' => size)
+      batch.description = "Weekly Android SDK DL (New): ~#{size} APKs"
+      batch.on(:complete, "ApkSnapshotService#on_complete_run_new", 'apk_snapshot_job_id' => j.id, 'apps_queued' => size)
       batch_size = 10e3.to_i
-      weekly_apps.find_in_batches(batch_size: batch_size).with_index do |the_batch, index|
+      weekly_new_apps.find_in_batches(batch_size: batch_size).with_index do |the_batch, index|
         batch.jobs do
           li "App #{index*batch_size}"
-          args = the_batch.map{ |android_app| [j.id, batch.bid, android_app.id, nil] }
+          args = the_batch.map{ |android_app| [j.id, batch.bid, android_app.id] }
           Sidekiq::Client.push_bulk('class' => ApkSnapshotServiceWorker, 'args' => args)
         end  
       end
@@ -45,6 +46,36 @@ class ApkSnapshotService
       Slackiq.message(message, webhook_name: :main)
 
       true
+    end
+
+    def run_recently_updated
+      batch = Sidekiq::Batch.new
+      size = weekly_recently_updated_ids.count
+      notes = "Weekly DL (Recently Updated) #{Time.now.strftime("%m/%d/%Y")}"
+      set_google_accounts_in_use_false
+      j = ApkSnapshotJob.create!(notes: notes, job_type: :mass)
+      batch.description = "Weekly Android SDK DL (Recently Released): ~#{size} APKs"
+      batch.on(:complete, "ApkSnapshotService#on_complete_run_recently_released", 'apk_snapshot_job_id' => j.id, 'apps_queued' => size)
+      batch_size = 10e3.to_i
+      weekly_recently_updated_ids.each_slice(batch_size).with_index do |the_batch, index|
+        batch.jobs do
+          li "App #{index*batch_size}"
+          args = the_batch.map{ |android_app_id| [j.id, batch.bid, android_app_id] }
+          Sidekiq::Client.push_bulk('class' => ApkSnapshotServiceWorker, 'args' => args)
+        end
+      end
+
+      message = "Queued #{size} Android apps for DL ."
+      Slackiq.message(message, webhook_name: :main)
+
+      true
+    end
+
+    def set_google_accounts_in_use_false
+      GoogleAccount.where(blocked: false, scrape_type: :full).each do |ga|
+        ga.in_use = false
+        ga.save!
+      end
     end
 
     # Temp method to test Google account choosing
@@ -56,8 +87,12 @@ class ApkSnapshotService
 
     private
 
-    def weekly_apps
+    def weekly_new_apps
       AndroidApp.where(display_type: :normal, newest_apk_snapshot_id: nil).joins(:newest_android_app_snapshot).where('android_app_snapshots.price = ? AND android_app_snapshots.released > ?', 0, 2.weeks.ago)
+    end
+
+    def weekly_recently_updated_ids(limit: 50e3)
+      recently_updated_ids = AndroidApp.joins(:newest_android_app_snapshot).where('android_app_snapshots.released > ?', 2.week.ago).order('android_app_snapshots.ratings_all_count DESC').limit(limit).pluck(:id)
     end
     
     # def daemon(command)
@@ -75,7 +110,7 @@ class ApkSnapshotService
     Slackiq.message("ApkSnapshotService complete.", webhook_name: :main)
   end
 
-  def on_complete_run_weekly(status, options)
+  def on_complete_run_new(status, options)
     apk_snapshot_job_id = options['apk_snapshot_job_id']
 
     apps_downloaded = begin 
@@ -86,7 +121,21 @@ class ApkSnapshotService
 
     apps_queued = options['apps_queued']
 
-    Slackiq.notify(webhook_name: :main, status: status, title: "Weekly Android App Download complete", 'Apps Downloaded' => apps_downloaded, 'Apps Queued' => apps_queued, 'apk_snapshot_job_id' => apk_snapshot_job_id)
+    Slackiq.notify(webhook_name: :main, status: status, title: "Weekly Android App Download complete (New)", 'Apps Downloaded' => apps_downloaded, 'Apps Queued' => apps_queued, 'apk_snapshot_job_id' => apk_snapshot_job_id)
   end
   
+  def on_complete_run_recently_updated(status, options)
+    apk_snapshot_job_id = options['apk_snapshot_job_id']
+
+    apps_downloaded = begin 
+      ApkSnapshotJob.find(apk_snapshot_job_id).apk_snapshots.where(status: ApkSnapshot.statuses[:success]).count
+    rescue => e
+      "#{e.message} | #{e.backtrace}"
+    end
+
+    apps_queued = options['apps_queued']
+
+    Slackiq.notify(webhook_name: :main, status: status, title: "Weekly Android App Download complete (Recently Updated)", 'Apps Downloaded' => apps_downloaded, 'Apps Queued' => apps_queued, 'apk_snapshot_job_id' => apk_snapshot_job_id)
+  end
+
 end
