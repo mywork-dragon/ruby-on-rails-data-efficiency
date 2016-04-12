d604fed38a28a317209aef2a0020f262da5f58ac
mywork-dragon <dragonblueyounger@gmail.com>
2016-04-12 14:27:30 -0700

    google account test

--- app/models/google_account.rb
+++ app/models/google_account.rb
@@ -4,7 +4,7 @@ class GoogleAccount < ActiveRecord::Base
 	has_many :apk_snapshot_exceptions
 
 	enum device: [:moto_g_phone_1, :moto_g_phone_2, :nexus_9_tablet]
-	enum scrape_type: [:full, :live]
+	enum scrape_type: [:full, :live, :test]
 
 
   class << self
