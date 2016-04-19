d6b5d8c1b9e1cb8e8b6bcd07db750a231ffb1fb4
mywork-dragon <dragonblueyounger@gmail.com>
2016-04-19 12:13:02 -0700

    email_worker patch

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -3,8 +3,17 @@ class EmailWorker
   
   sidekiq_options queue: :mailers
 
-  def perform(lead_options)
+  def perform(method, *args)
+    self.send(method.to_sym, *args)
+  end
+
+  def contact_us(lead_options)
     ContactUsMailer.contact_us_email(lead_options).deliver
+  end
+
+  def invite_user(user_id)
+    user = User.find(user_id)
+    UserMailer.invite_email(user).deliver
   end
   
 end
