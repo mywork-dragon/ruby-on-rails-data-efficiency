79e8ddf50e2ba654571534b9c2a8e7d85e22110e
mywork-dragon <dragonblueyounger@gmail.com>
2016-04-20 15:32:52 -0700

    change email for invites

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            'mightysignalmailman',
-    password:             'iamthemailmanofmightysignalyo',
+    domain:               'mightysignal.com',
+    user_name:            'support@mightysignal.com',
+    password:             'Yb]fVjNRVAZdpE8pHM9,y9nYw',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
