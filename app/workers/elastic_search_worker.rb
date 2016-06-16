bcd5ac02cef49bffe3729c57157d45bc2adaa9f0
mywork-dragon <dragonblueyounger@gmail.com>
2016-06-15 17:11:41 -0700

    elasticsearch helpers (#602)
    * first pass at index all helper method
    
    * fix

--- app/workers/elastic_search_worker.rb
+++ app/workers/elastic_search_worker.rb
@@ -13,7 +13,7 @@ class ElasticSearchWorker
     batch.description = 'iOS Elasticsearch Index Updating'
     batch.on(:complete, 'ElasticSearchWorker#on_complete', 'queue_type' => 'iOS')
 
-    Slackiq.message('Starting iOS ElasticSearch index updating', webhook_name: :main)
+    notify('Starting iOS ElasticSearch index updating')
 
     batch.jobs do
       IosApp.find_in_batches(batch_size: 1000).with_index do |the_batch, index|
@@ -29,7 +29,7 @@ class ElasticSearchWorker
     batch.description = 'Android Elasticsearch Index Updating'
     batch.on(:complete, 'ElasticSearchWorker#on_complete', 'queue_type' => 'Android')
 
-    Slackiq.message('Starting Android ElasticSearch index updating', webhook_name: :main)
+    notify('Starting Android ElasticSearch index updating')
 
     batch.jobs do
       AndroidApp.find_in_batches(batch_size: 1000).with_index do |the_batch, index|
@@ -50,6 +50,30 @@ class ElasticSearchWorker
     app_ids.each_slice(100) do |ids|
       AppsIndex::AndroidApp.import(ids)
     end
+  end
+
+  def index_ios_sdks
+    notify('Starting iOS SDK index updating')
+    IosSdkIndex::IosSdk.import
+    notify('Finished iOS SDK index updating')
+  end
+
+  def index_android_sdks
+    notify('Starting Android SDK index updating')
+    AndroidSdkIndex::AndroidSdk.import
+    notify('Finished Android SDK index updating')
+  end
+
+  def notify(msg)
+    Slackiq.message(msg, webhook_name: :main)
+  end
+
+  # helper function for updating all the indices
+  def update_everything
+    ElasticSearchWorker.perform_async(:index_ios_sdks)
+    ElasticSearchWorker.perform_async(:index_android_sdks)
+    ElasticSearchWorker.perform_async(:queue_android_apps)
+    ElasticSearchWorker.perform_async(:queue_ios_apps)
   end
 
   def on_complete(status, options)
