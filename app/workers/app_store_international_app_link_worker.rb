bd1bc784d786af78a3ac8ee65eccb7abeda929ba
mywork-dragon <dragonblueyounger@gmail.com>
2016-06-24 16:46:16 -0700

    fix raw sql command

--- app/workers/app_store_international_app_link_worker.rb
+++ app/workers/app_store_international_app_link_worker.rb
@@ -6,7 +6,7 @@ class AppStoreInternationalAppLinkWorker
   def perform
     num = IosAppCurrentSnapshotBackup.count
     puts "There are #{num} rows. This should take approximately #{num * 1.5 / 100_000}s"
-    sql = 'insert into `app_store_ios_apps_backups` (ios_app_id, app_store_id, updated_at, created_at)
+    sql = 'insert into `app_stores_ios_app_backups` (ios_app_id, app_store_id, updated_at, created_at)
     select ios_app_id, app_store_id, updated_at, created_at  from `ios_app_current_snapshot_backups`;'
     ActiveRecord::Base.connection.execute(sql)
   end
