9c6b3561245c30624a7b982c4168eb78a10868b6
mywork-dragon <dragonblueyounger@gmail.com>
2016-08-25 14:53:34 -0700

    Open proxies (#683)
    * OpenProxy
    
    * add public_ip and helper
    
    * create_digital_ocean_tinyproxy! helper

--- app/models/open_proxy.rb
+++ app/models/open_proxy.rb
@@ -1,11 +1,16 @@
 class OpenProxy < ActiveRecord::Base
 
-  enum kind: [:digital_ocean_squid]
+  enum kind: [:digital_ocean_squid, :digital_ocean_tinyproxy]
 
   class << self
 
     def create_digital_ocean_squid!(public_ip:)
       params = {public_ip: public_ip, username: 'ms_open_proxy', password: '7CjnctEFkL9yvVCwEUd3VkHyiG8g2M', port: 3128, kind: :digital_ocean_squid}
+      self.create!(params)
+    end
+
+    def create_digital_ocean_tinyproxy!(public_ip:)
+      params = {public_ip: public_ip, port: 8888, kind: :digital_ocean_tinyproxy}
       self.create!(params)
     end
 
