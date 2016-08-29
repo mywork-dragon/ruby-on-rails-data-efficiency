d3e5f4e4ffa45bce2aeb6ac976a62e97402fc79c
mywork-dragon <dragonblueyounger@gmail.com>
2016-08-29 15:01:10 -0700

    change queue to default

--- app/workers/app_store_international_current_snapshot_worker.rb
+++ app/workers/app_store_international_current_snapshot_worker.rb
@@ -2,7 +2,7 @@ class AppStoreInternationalCurrentSnapshotWorker
   include Sidekiq::Worker
   include AppStoreInternationalSnapshotModule
   
-  sidekiq_options retry: 1, queue: :scraper
+  sidekiq_options retry: 1, queue: :default
 
   def initialize
     @current_tables = true
