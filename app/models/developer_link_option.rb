c8de84ff9178c15a2b5afbc25fe38d24a62b9a24
mywork-dragon <dragonblueyounger@gmail.com>
2016-08-29 13:33:57 -0700

    rename enum method name

--- app/models/developer_link_option.rb
+++ app/models/developer_link_option.rb
@@ -2,5 +2,5 @@ class DeveloperLinkOption < ActiveRecord::Base
   belongs_to :ios_developer
   belongs_to :android_developer
 
-  enum method: [:name, :website]
+  enum method: [:name_match, :website_match]
 end
