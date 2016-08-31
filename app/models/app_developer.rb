4f889083eb31fd33ac4ef464bb80705f460aaed9
mywork-dragon <dragonblueyounger@gmail.com>
2016-08-31 12:13:40 -0700

    add associations

--- app/models/app_developer.rb
+++ app/models/app_developer.rb
@@ -1,2 +1,4 @@
 class AppDeveloper < ActiveRecord::Base
+  has_many :ios_developers, through: :app_developers_developers, source: :developer, source_type: 'IosDeveloper'
+  has_many :android_developers, through: :app_developers_developers, source: :developer, source_type: 'AndroidDeveloper'
 end
