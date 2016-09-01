954b6e663dfcc7a9cb611d1445cc43990d42be60
mywork-dragon <dragonblueyounger@gmail.com>
2016-08-31 18:29:42 -0700

    compare results

--- app/services/developer_linking_service.rb
+++ app/services/developer_linking_service.rb
@@ -46,6 +46,50 @@ class DeveloperLinkingService
         ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{model.table_name}")
       end
     end
+
+    def compare_results
+      success = 0
+      failures = 0
+      answers = load_answers
+      answers.keys.each do |ios_developer_id|
+        android_developer_id = answers[ios_developer_id]
+        app_developer = IosDeveloper.find(ios_developer_id).app_developer
+
+        if android_developer_id == -1
+          if app_developer
+            failures += 1
+          else
+            success += 1
+          end
+        else
+          linked = app_developer.android_developers.find_by(id: android_developer_id)
+
+          if linked
+            if app_developers.android_developers.count + app_developers.ios_developers.count > 10
+              failures += 1
+            else
+              success += 1
+            end
+          else
+            failures += 1
+          end
+        end
+      end
+
+      puts "Successes: #{success}"
+      puts "Failures: #{failures}"
+      puts "Ratio: #{1.0 * success / (success + failures)}"
+    end
+
+    def load_answers
+      links = File.open('app/lib/publisher_links.txt') {|f| f.read}
+      ios_developer_ids = android_developer_ids = []
+      links.split(/\n/).reduce({}) do |memo, line|
+        parts = line.split.map(&:to_i)
+        memo[parts[0]] = parts[1]
+        memo
+      end
+    end
   end
 
   def on_complete_app_developers(status, options)
