d568cc9440721143e3bf934dee19eb817c25845e
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-06 12:06:54 -0700

    first pass at tos checker

--- app/workers/itunes_tos_worker.rb
+++ app/workers/itunes_tos_worker.rb
@@ -8,6 +8,35 @@ class ItunesTosWorker
   end
 
   def check_app_store(app_store_id)
+    updated_date = ItunesTos.itunes_updated_date(app_store_id)
+    newest_tos_snapshot = AppStore.find(app_store_id).newest_tos_snapshot
+
+    if newest_tos_snapshot.blank? || newest_tos_snapshot.last_updated_date < updated_date
+      save_snapshot(app_store_id, updated_date)
+    elsif newest_tos_snapshot.last_updated_date < updated_date
+      save_snapshot(app_store_id, updated_date)
+      disable_app_store(app_store_id)
+      trigger_alert(app_store_id, updated_date)
+    else
+      newest_tos_snapshot.touch_valid_date
+    end
+  end
+
+  def save_snapshot(app_store_id, updated_date)
+    AppStoreTosSnapshot.create!(
+      app_store_id: app_store_id,
+      last_updated_date: updated_date,
+    )
+  end
+
+  def disable_app_store(app_store_id)
+    AppStore.find(app_store_id).update!(
+      tos_valid: false
+    )
+  end
+
+  def trigger_alert(app_store_id, updated_date)
+    puts 'RIP'
   end
 
   def on_complete_check_app_stores(status, options)
