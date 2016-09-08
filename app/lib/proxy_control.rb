5f57b2d0f35350388e1017270296ebd914bd6314
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-08 11:17:14 -0700

    increase wait times

--- app/lib/proxy_control.rb
+++ app/lib/proxy_control.rb
@@ -18,11 +18,11 @@ class ProxyControl
   def activation_routine
     puts 'Starting temporary proxies'
     MightyAws::InstanceControl.new.start_temporary_proxies
-    puts 'Allowing 10s to start running'
-    sleep 10 # let proxies spin up and start running
+    puts 'Allowing 15s to start running'
+    sleep 15 # let proxies spin up and start running
     puts 'Registering temporary proxies'
     MightyAws::Api.new.register_temp_proxies_with_proxy_lbs
-    puts 'Allowing 35s for lbs to establish healthy connections with proxies'
-    sleep 35 # register unregistered proxies. Health check is every 30 seconds
+    puts 'Allowing 45s for lbs to establish healthy connections with proxies'
+    sleep 45 # register unregistered proxies. Health check is every 30 seconds
   end
 end
