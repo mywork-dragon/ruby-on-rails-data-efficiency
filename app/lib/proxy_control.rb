edb9a2b133120ab6f35bf17995cabe7ad5cf3139
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-07 18:54:20 -0700

    remove signals

--- app/lib/proxy_control.rb
+++ app/lib/proxy_control.rb
@@ -7,28 +7,15 @@ class ProxyControl
     toggle_proxies(activate: false)
   end
 
-  # should be thread-safe. needs more testing
   def toggle_proxies(activate:)
-    changed = false
-    signal = ProxySignal.transaction do
-      row = ProxySignal.last
-      if row.activated != activate
-        changed = true
-        row = ProxySignal.create!(activated: activate)
-      end
-      row
-    end
-
-    return signal unless changed
-    activate ? activation_routine(signal) : deactivation_routine(signal)
-    signal
+    activate ? activation_routine : deactivation_routine
   end
 
-  def deactivation_routine(signal)
+  def deactivation_routine
     MightyAws::InstanceControl.stop_temporary_proxies
   end
 
-  def activation_routine(signal)
+  def activation_routine
     MightyAws::InstanceControl.start_temporary_proxies
     sleep 10 # let proxies spin up and start running
     MightyAws::Api.new.register_temp_proxies_with_proxy_lbs
