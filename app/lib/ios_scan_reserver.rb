e963486f84ae24e8f6692bc8073e61159ff3f694
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-09 13:35:31 -0700

    first pass

--- app/lib/ios_scan_reserver.rb
+++ app/lib/ios_scan_reserver.rb
@@ -3,11 +3,12 @@ class IosScanReserver
   class UnregisteredScanPurpose < RuntimeError; end
   class InvalidInput < RuntimeError; end
   class UnknownCondition < RuntimeError; end
+  class NoAccountAvailable < RuntimeError; end
+  class UnreservedState < RuntimeError; end
 
   def initialize(ipa_snapshot)
     @ipa_snapshot = ipa_snapshot
     @device_reserver = IosDeviceReserver.new(self)
-    @swap_required = false
   end
 
   def reserve(scan_purpose, requirements)
@@ -18,15 +19,15 @@ class IosScanReserver
 
   def setup_app_store_account(reserve_type, app_store_id)
     raise InvalidInput unless device && app_store_id
-    existing_apple_account = device.apple_account
-    @apple_account = if existing_apple_account && existing_apple_account.app_store_id == app_store_id
+    existing_account = device.apple_account
+    @apple_account = if existing_account && existing_account.app_store_id == app_store_id
                        puts 'correctly configured'
                        existing_apple_account
                        @swap_required = false
     else
       raise UnknownCondition unless is_flex_type?(reserve_type)
-      # get apple account
       @swap_required = true
+      pick_flex_apple_account(app_store_id)
     end
     @apple_account.update!(last_used: Time.now)
   end
@@ -35,13 +36,28 @@ class IosScanReserver
     @device_reserver.device if @device_reserver
   end
 
-  def correct_app_store?
+  def correct_app_store?(apple_account, app_store_id)
+    apple_account && apple_account.app_store_id == app_store_id
+  end
+
+  # Can have one account on multiple devices. no "reserving"
+  def pick_flex_apple_account(app_store_id)
+    account = AppleAccount.transaction do
+      a = AppleAccount.where(
+        app_store_id: app_store_id,
+        kind: AppleAccount.kinds[:flex]
+      ).limit(1).order(last_used: :DESC).take
+    end
+    raise NoAccountAvailable unless account
+    account
   end
   
   def apple_account
+    @apple_account
   end
 
   def release
+    @device_reserver.release if device
   end
 
   def us_app?
@@ -60,5 +76,13 @@ class IosScanReserver
     else
       raise UnregisteredScanPurpose
     end
+  end
+
+  def account_changed
+    device.update!(apple_account: apple_account)
+  end
+
+  def is_swap_required?
+    raise UnreservedState unless !!defined?(@swap_required)
   end
 end
