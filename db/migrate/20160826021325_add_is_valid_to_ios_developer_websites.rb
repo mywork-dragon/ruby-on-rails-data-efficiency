85ac1a8b86c52f0caa69f316575f710857770c8b
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-12 15:27:15 -0700

    add back

--- db/migrate/20160826021325_add_is_valid_to_ios_developer_websites.rb
+++ db/migrate/20160826021325_add_is_valid_to_ios_developer_websites.rb
@@ -1,7 +1,7 @@
 class AddIsValidToIosDeveloperWebsites < ActiveRecord::Migration
   def change
-    #add_column :ios_developers_websites, :is_valid, :bool, default: true
-    #add_column :android_developers_websites, :is_valid, :bool, default: true
+    add_column :ios_developers_websites, :is_valid, :bool, default: true
+    add_column :android_developers_websites, :is_valid, :bool, default: true
     add_index :ios_developers_websites, [:ios_developer_id, :is_valid], name: 'ios_developers_websites_is_valid'
     add_index :android_developers_websites, [:android_developer_id, :is_valid], name: 'android_developers_websites_is_valid'
   end
