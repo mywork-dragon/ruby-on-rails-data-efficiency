de5f33bf92cae9468d719645cc1d0f7624ae37ac
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-12 15:13:06 -0700

    change name for index

--- db/migrate/20160826021325_add_is_valid_to_ios_developer_websites.rb
+++ db/migrate/20160826021325_add_is_valid_to_ios_developer_websites.rb
@@ -2,7 +2,7 @@ class AddIsValidToIosDeveloperWebsites < ActiveRecord::Migration
   def change
     add_column :ios_developers_websites, :is_valid, :bool, default: true
     add_column :android_developers_websites, :is_valid, :bool, default: true
-    add_index :ios_developers_websites, [:ios_developer_id, :is_valid]
-    add_index :android_developers_websites, [:android_developer_id, :is_valid]
+    add_index :ios_developers_websites, [:ios_developer_id, :is_valid], name: 'ios_developers_websites_is_valid'
+    add_index :android_developers_websites, [:android_developer_id, :is_valid], name: 'android_developers_websites_is_valid'
   end
 end
