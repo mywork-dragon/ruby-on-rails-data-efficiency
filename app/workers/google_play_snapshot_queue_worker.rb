c1fdcdadd95bcbe97ca1c47a849d98f45ba815af
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-14 13:55:11 -0700

    add messaging

--- app/workers/google_play_snapshot_queue_worker.rb
+++ app/workers/google_play_snapshot_queue_worker.rb
@@ -27,20 +27,26 @@ class GooglePlaySnapshotQueueWorker
   end
 
   def queue_valid(android_app_snapshot_job_id)
+    Slackiq.message('Queueing Google Play apps', webhook_name: :main)
     @android_app_snapshot_job_id = android_app_snapshot_job_id
     @query = { display_type: AndroidApp.display_types.values_at(:normal, :device_incompatible, :carrier_incompatible) }
     queue_worker
+    Slackiq.message('Finished queueing Google Play apps', webhook_name: :main)
   end
 
   def queue_all(android_app_snapshot_job_id)
+    Slackiq.message('Queueing *all* Google Play Apps', webhook_name: :main)
     @android_app_snapshot_job_id = android_app_snapshot_job_id
     @query = nil
     queue_worker
+    Slackiq.message('Finished queueing Google Play apps', webhook_name: :main)
   end
 
   def queue_ids(android_app_snapshot_job_id, android_app_ids)
+    Slackiq.message('Queueing specific Google play apps by id', webhook_name: :main)
     @android_app_snapshot_job_id = android_app_snapshot_job_id
     @query = { id: android_app_ids }
     queue_worker
+    Slackiq.message('Finished queueing Google Play apps', webhook_name: :main)
   end
 end
