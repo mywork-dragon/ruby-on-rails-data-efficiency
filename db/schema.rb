d7a3f832de6a4586b0f30689c18585466e46a6c9
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-16 12:14:32 -0700

    updated schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916182247) do
+ActiveRecord::Schema.define(version: 20160909174100) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name",                    limit: 191
