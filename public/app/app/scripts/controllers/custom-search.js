9497a6f299b1f4d69f8e096f749933f509f1d7ae
mywork-dragon <dragonblueyounger@gmail.com>
2016-09-27 15:18:26 -0700

    Watch for platform changes

--- public/app/app/scripts/controllers/custom-search.js
+++ public/app/app/scripts/controllers/custom-search.js
@@ -126,11 +126,12 @@ angular.module('appApp')
         }
       };
 
-      customSearchCtrl.toggledPlatform = function() {
+      $scope.$watch('customSearchCtrl.platform', function() {
+        customSearchCtrl.apps = [];
         customSearchCtrl.appNum = 0;
         customSearchCtrl.numApps = 0;
         customSearchCtrl.queryInProgress = false;
-      }
+      });
 
       customSearchCtrl.getLastUpdatedDaysClass = function(lastUpdatedDays) {
         return searchService.getLastUpdatedDaysClass(lastUpdatedDays);
