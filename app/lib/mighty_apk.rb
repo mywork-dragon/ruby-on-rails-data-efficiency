121da10cc5c714b5d147631f4512bd35ddcd66ac
mywork-dragon <dragonblueyounger@gmail.com>
2016-10-20 10:55:46 -0700

    fix problem with production eager loading ruby protocol buffers

--- app/lib/mighty_apk.rb
+++ app/lib/mighty_apk.rb
@@ -1,6 +1,10 @@
 require_relative 'mighty_apk/market_api'
 require_relative 'mighty_apk/market'
-require_relative 'mighty_apk/googleplay.pb'
+
+# production is eager loaded. redefining the protocol buffer file causes errors
+unless Rails.application.config.eager_load
+  require_relative 'mighty_apk/googleplay.pb'
+end
 
 module MightyApk
 end
