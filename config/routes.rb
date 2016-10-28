1209d46c525d419896f6c7ad924bfd8ef92ed61a
mywork-dragon <dragonblueyounger@gmail.com>
2016-10-28 01:41:59 -0700

    redirect privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   post 'subscribe' => 'welcome#subscribe', as: :subscribe
   post 'contact_us' => 'welcome#contact_us', as: :contact_us
   post 'try_it_out' => 'welcome#try_it_out', as: :try_it_out
+  get '/privacy', to: redirect('/legal/privacy.pdf')
   
   match 'auth/:provider/callback', to: 'salesforce_sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
