60a2581fce0edd8d4355bf5f8ac127c4c0ac3514
mywork-dragon <dragonblueyounger@gmail.com>
2016-11-30 17:07:11 -0800

    Fix ApkFile tests.

--- test/models/apk_file_test.rb
+++ test/models/apk_file_test.rb
@@ -1,12 +1,20 @@
 require 'test_helper'
 
 class ApkFileTest < ActiveSupport::TestCase
-  test "the truth" do
-    data = nil
+  test "store and get class lists" do
 
-    class MightyAwsMock
+    class MightyAwsS3Mock
+      attr_accessor :data
+      attr_accessor :key_stored_to
+      attr_accessor :key_returned_from
+
       def store(bucket:, key_path:, data_str:)
-        data = data_str
+        @data = data_str
+        @key_stored_to = key_path
+      end
+      def get_object(bucket:, key_path:)
+        @key_returned_from = key_path
+        StringIO.new @data
       end
     end
 
@@ -19,7 +27,8 @@ class ApkFileTest < ActiveSupport::TestCase
 
     apk_file = ApkFile.new
     # Inject a mock s3_client
-    apk_file.s3_client = MightyAwsMock.new
+    s3 = MightyAwsS3Mock.new
+    apk_file.s3_client = s3
 
     # Save the file
     apk_file.zip = stringio
@@ -30,6 +39,8 @@ class ApkFileTest < ActiveSupport::TestCase
     apk_file.upload_class_summary(['test.class', 'test.class2'])
 
     # Ensure the correct data was written.
-    assert data = "test.class\ntest.class2"
+    assert_equal "test.class\ntest.class2", s3.data
+    assert_equal ['test.class', 'test.class2'], apk_file.classes
+    assert_equal s3.key_stored_to,  s3.key_returned_from
   end
 end
