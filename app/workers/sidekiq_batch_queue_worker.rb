e900518adb2c9c9269a28a2d497499dae1877219
mywork-dragon <dragonblueyounger@gmail.com>
2016-12-19 18:45:36 -0800

    allow sidekiq batch queue override (#856)

--- app/workers/sidekiq_batch_queue_worker.rb
+++ app/workers/sidekiq_batch_queue_worker.rb
@@ -3,13 +3,17 @@ class SidekiqBatchQueueWorker
   
   sidekiq_options queue: :sidekiq_batcher, retry: false
 
-  def perform(class_name, args, bid)
+  def perform(class_name, args, bid, specified_queue = nil)
     batch = Sidekiq::Batch.new(bid)
+
+    worker_class = class_name.constantize
+    queue = (specified_queue || worker_class.sidekiq_options['queue']).to_s
 
     batch.jobs do
       Sidekiq::Client.push_bulk(
-        'class' => class_name.constantize,
-        'args' => args
+        'class' => worker_class,
+        'args' => args,
+        'queue' => queue
       )
     end
   end
