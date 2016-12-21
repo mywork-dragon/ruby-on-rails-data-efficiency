3305334c39b4a41ba5723db94cc5c33e4ceaa677
mywork-dragon <dragonblueyounger@gmail.com>
2016-12-20 18:23:24 -0800

    Containerize ApkSnapshotServiceWorker.

--- app/workers/apk_snapshot_service_worker.rb
+++ app/workers/apk_snapshot_service_worker.rb
@@ -2,14 +2,14 @@ class ApkSnapshotServiceWorker
 
   include Sidekiq::Worker
 
-  sidekiq_options backtrace: true, retry: false, queue: :sdk
+  sidekiq_options backtrace: true, retry: false, queue: :apk_snapshot_service
   
   RETRIES = 2 # use our own retries
 
   include ApkWorker
   
   def classify_if_necessary(apk_ss_id)
-    # NO OP
+    AndroidMassClassificationServiceWorker.perform_async(apk_ss_id)
   end
 
   def scrape_type
