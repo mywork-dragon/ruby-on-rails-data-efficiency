debf1265699dafcd1b2d885df2073e46ecd2b1b1
mywork-dragon <dragonblueyounger@gmail.com>
2017-01-11 14:51:17 -0800

    Fix return value of temporary_proxy_load_balancers. (#898)

--- app/lib/clients/proxy_base.rb
+++ app/lib/clients/proxy_base.rb
@@ -44,10 +44,11 @@ module ProxyBase
   # load balancers that will forward address to temporary proxies
   # TODO: hard-coded for now...move to DB
   def temporary_proxy_load_balancers
-    [
+    elbs = [
       'internal-01-proxy-balancer-633334655.us-east-1.elb.amazonaws.com',
       'internal-02-proxy-balancer-1130238239.us-east-1.elb.amazonaws.com'
     ]
+    { ip: elbs.sample, port: 8888}
   end
 
 end
