60fa35cbda6f54a5624c538dc1c433cfcc25ccd1
mywork-dragon <dragonblueyounger@gmail.com>
2017-01-12 19:54:06 -0800

    Add more proxy regions.

--- app/lib/proxy_regions.rb
+++ app/lib/proxy_regions.rb
@@ -2,6 +2,6 @@ module ProxyRegions
   extend ActiveSupport::Concern
   included do
     # ISO 3166-1 alpha-2
-    enum region: [:US, :BR, :IN, :FR, :KR, :CN, :DE, :ID, :RU, :VN]
+    enum region: [:US, :BR, :IN, :FR, :KR, :CN, :DE, :ID, :RU, :VN, :JP, :AU, :SG]
   end
 end
