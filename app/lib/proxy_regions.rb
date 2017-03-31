7997cbd67762735b6a488afa5e67ec9095913e80
mywork-dragon <dragonblueyounger@gmail.com>
2017-03-30 18:07:26 -0700

    Add more proxy regions. (#965)
    * Remove old test.
    
    * Add more iso 2 proxy regions.

--- app/lib/proxy_regions.rb
+++ app/lib/proxy_regions.rb
@@ -2,6 +2,10 @@ module ProxyRegions
   extend ActiveSupport::Concern
   included do
     # ISO 3166-1 alpha-2
-    enum region: [:US, :BR, :IN, :FR, :KR, :CN, :DE, :ID, :RU, :VN, :JP, :AU, :SG, :GB]
+    enum region: [
+      :US, :BR, :IN, :FR, :KR, :CN, :DE, :ID, :RU, :VN,
+      :JP, :AU, :SG, :GB, :DK, :NO, :SE, :FI, :ES, :CL,
+      :CO, :AR, :CA, :AT
+    ]
   end
 end
--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -10,33 +10,6 @@ class ApiControllerTest < ActionController::TestCase
     @ios_app = IosApp.create!(app_identifier: 123)
     @android_app = AndroidApp.create!(app_identifier: 'com.mighty.fun')
   end
-  #
-  # # called after every single test
-  # def teardown
-  #   puts "TURRDOWN"
-  # end
-  
-  # test "Should filter correctly for Mobile Priority" do
-  #   puts "cols: #{IosApp.column_names}"
-  #   ios_app = IosApp.create!(app_identifier: 123, mobile_priority: :medium)
-  #
-  #   ios_app_snapshot = IosAppSnapshot.create
-  #
-  #   ios_app.newest_ios_app_snapshot = ios_app_snapshot
-  #   ios_app.save
-  #
-  #   post(:filter_ios_apps, {'app' => {'mobilePriority' => "M"}})
-  #   puts "response: #{@response.body}"
-  #
-  #   puts "ios_app: #{IosApp.find_by_app_identifier(123).app_identifier}"
-  #
-  #   assert true
-  # end
-  
-  # test 'should fail' do
-  #   assert false
-  # end
-  #
 
   test "livescan enabled when admin account and service enabled." do
     ApplicationController.any_instance.stub(:logged_into_admin_account?) { true }
