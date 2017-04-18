d28ce8f920c92326dfbaea480caa4277f94ab337
mywork-dragon <dragonblueyounger@gmail.com>
2017-04-18 12:02:25 -0700

    Specify repo in build notification.

--- bin/notify_build.rb
+++ bin/notify_build.rb
@@ -9,7 +9,7 @@ url = 'https://hooks.slack.com/services/T02T20A54/B35F61F42/x2D3qY2r4XdCWf8z3KvP
 circle_tag = ENV['CIRCLE_TAG'] || 'unspecified'
 
 body = {
-  "text" => "Built `#{circle_tag}`. To deploy run ```\nmightydeploy --service <SERVICE_NAME> --images varys=sidekiq=nginx=#{circle_tag}\n```",
+  "text" => "Built varys `#{circle_tag}`. To deploy run ```\nmightydeploy --service <SERVICE_NAME> --images varys=sidekiq=nginx=#{circle_tag}\n```",
   "channel" => "#circle-ci",
   "username" => "deploy",
   "icon_emoji" => ":monkey_face:"
