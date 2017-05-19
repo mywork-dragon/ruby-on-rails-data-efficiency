811c5f303a827976644c9f143d0283602389e3eb
mywork-dragon <dragonblueyounger@gmail.com>
2017-05-19 11:23:43 -0700

    Add staging entry point. (#1019)

---
+++ bin/staging.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 )  
+HOST_SUFFIX='ms-internal.com'
+HOST="$ECS_SERVICE_NAME.$HOST_SUFFIX"
+HOSTED_ZONE_ID='Z3QMLWJ9T4W065'
+echo "Registering $HOST as $IP"
+
+
+INPUT_JSON="{
+  \"Comment\": \"Update the A record set\",
+  \"Changes\": [
+    {
+      \"Action\": \"UPSERT\",
+      \"ResourceRecordSet\": {
+        \"Name\": \"$HOST\",
+        \"Type\": \"A\",
+        \"TTL\": 5,
+        \"ResourceRecords\": [
+          {
+            \"Value\": \"$IP\"
+          }
+        ]
+      }
+    }
+  ]
+}"
+
+INPUT_JSON="{ \"ChangeBatch\": $INPUT_JSON }"
+
+aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --cli-input-json "$INPUT_JSON"
+
+bundle exec unicorn_rails -c config/unicorn.rb
