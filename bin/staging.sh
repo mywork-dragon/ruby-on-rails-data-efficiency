d6f06ace6efaeed654558fceb63adc7627ec7368
mywork-dragon <dragonblueyounger@gmail.com>
2017-06-05 18:21:34 -0700

    Use private ips for staging sites. (#1042)

--- bin/staging.sh
+++ bin/staging.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 )  
+IP=$( curl http://169.254.169.254/latest/meta-data/private-ipv4 )
 HOST_SUFFIX='ms-internal.com'
 HOST="$ECS_SERVICE_NAME.$HOST_SUFFIX"
 HOSTED_ZONE_ID='Z3QMLWJ9T4W065'
