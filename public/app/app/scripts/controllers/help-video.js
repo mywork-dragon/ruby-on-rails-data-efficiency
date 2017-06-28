979d088a75a47d6c7a2602bf09e11ba85e167e5a
mywork-dragon <dragonblueyounger@gmail.com>
2017-06-28 16:28:54 -0700

    Video guide links (#1088)
    * add initial files
    
    * add help button
    
    * add video modal controller
    
    * connect controller and modal instance controller
    
    * add video ids
    
    * get video url working
    
    * style modal
    
    * move urls to instance control and add type to modal title
    
    * add video links to sdk panel in app details
    
    * add video link to timeline
    
    * add video link to ad intelligence
    
    * update bower components
    
    * change live scan video button to link
    
    * change button in android sdk panel
    
    * change explore video link to button
    
    * change timeline button to link
    
    * style buttons and links
    
    * move ad intelligence link
    
    * move saved searches button, add video link
    
    * reduce saved searches button size
    
    * add divider and style

---
+++ public/app/app/scripts/controllers/help-video.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('appApp')
+  .controller("VideoModalCtrl", ["$scope", "$uibModal", function($scope, $uibModal) {
+
+    $scope.openVideoModal = function (type) {
+      $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'videoModalTitle',
+        ariaDescribedBy: 'videoModalBody',
+        templateUrl: 'help-video.html',
+        controller: 'VideoInstanceCtrl',
+        controllerAs: '$ctrl',
+        size: 'lg',
+        resolve: {
+          type: function () {
+            return type;
+          }
+        }
+      })
+    }
+  }])
+  .controller('VideoInstanceCtrl', function ($uibModalInstance, $sce, type) {
+    var $ctrl = this;
+    $ctrl.videoUrls = {
+      'Timeline': 'https://www.youtube.com/embed/gtqkNxgpyZk',
+      'Explore': 'https://www.youtube.com/embed/aQ-H79xbeAg',
+      'Ad Intelligence': 'https://www.youtube.com/embed/jp9c3xyqHOc',
+      'Live Scan': 'https://www.youtube.com/embed/h1mDtcHsQ1Q'
+    }
+
+    $ctrl.type = type;
+    $ctrl.videoUrl = $sce.trustAsResourceUrl($ctrl.videoUrls[type]);
+  })
