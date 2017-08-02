6495dd11ef6038577938d353a39d8ede6015b824
mywork-dragon <dragonblueyounger@gmail.com>
2017-08-02 15:56:11 -0700

    Add DomainLinker class. (#1143)
    * Add DomainLinker class.
    
    * Remove unused include.

---
+++ app/lib/domain_linker.rb
@@ -0,0 +1,30 @@
+# DomainLinker
+# Provides an interface for linking domains to publishers.
+
+class DomainLinker
+  def initialize()
+    @downloaded = false
+    @domain_to_publishers = Hash.new {[]}
+  end
+
+  def download!
+    domain_pub_url = 'https://feeds.mightysignal.com/v1/internal/domain-publisher-relationships/latest/domain-publisher-relationships.gz'
+    open(domain_pub_url, "JWT" => ENV['MS_FEEDS_TOKEN']) do |f_remote|
+      csv = CSV.new(Zlib::GzipReader.new(f_remote).read)
+      csv.each do |row|
+        @domain_to_publishers[row[0]] = @domain_to_publishers[row[0]] + [row[1]]
+      end
+    end
+    @downloaded = true
+  end
+
+  def domain_to_publisher(domain)
+    if !@downloaded
+      download!
+    end
+    return @domain_to_publishers[domain].map do |publisher_id|
+      platform, publisher_id = publisher_id.split '-'
+      "#{platform}_developer".classify.constantize.find(publisher_id)
+    end
+  end
+end
