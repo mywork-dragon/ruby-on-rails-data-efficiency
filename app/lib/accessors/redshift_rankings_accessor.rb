663e21683d929e0d048c75700eea9b5dccc554a1
mywork-dragon <dragonblueyounger@gmail.com>
2017-11-03 17:12:43 -0700

    Implementing newcomers from redshift (#1282)

--- app/lib/accessors/redshift_rankings_accessor.rb
+++ app/lib/accessors/redshift_rankings_accessor.rb
@@ -5,53 +5,17 @@ class RedshiftRankingsAccessor
     
     # Validate parameters
 
-    if (platforms - ['ios', 'android']).any?
-      raise "Unsupported platform option"
-    end
-
-    if (rank_types - ['free', 'paid', 'grossing']).any?
-      raise "Unsupported rank_type option"
-    end
-
-    if !['weekly_change', 'monthly_change'].include? sort_by
-      raise "Unsupported sort_by option"
-    end
-
-    if !size.is_a? Integer
-      raise "size must be an integer"
-    end
-
-    if !page_num.is_a? Integer
-      raise "page_num must be an integer"
-    end
-
-    countries.each do |country|
-      raise "Invalid country code." if country.length != 2
-    end
-
-    categories.each do |category|
-      raise "Invalid category" if category.include? "\"" or category.include? "'"
-    end
+    validate_parameters(platforms: platforms, countries: countries, categories: categories, rank_types: rank_types, size: size, page_num: page_num)
+    raise "Unsupported sort_by option" if !['weekly_change', 'monthly_change'].include? sort_by
 
     # Denormalize necessary parameters
 
-    denormalized_countries = []
-    if platforms.include?  'ios'
-      countries.each do |country_code|
-        denormalized_countries.push(country_code_to_ios(country_code))
-      end
-    end
-    denormalized_countries = denormalized_countries.compact + countries
-
-    denormalized_rank_types = []
-    rank_types.each do |rank_type|
-      denormalized_rank_types.push(rank_type_to_ios(rank_type)) if platforms.include? 'ios'
-      denormalized_rank_types.push(rank_type_to_android(rank_type)) if platforms.include? 'android'
-    end
+    denormalized_countries = generate_denormalized_countries(platforms, countries)
+    denormalized_rank_types = generate_denormalized_rank_types(platforms, rank_types)
 
     # Perform queries
 
-    where_clauses = build_where_clauses(platforms, denormalized_countries, categories, denormalized_rank_types, sort_by)
+    where_clauses = build_where_clauses(platforms, denormalized_countries, categories, denormalized_rank_types, sort_by: sort_by)
     order_by_clause = desc ? "ORDER BY #{sort_by} DESC" : "ORDER BY #{sort_by} ASC"
 
     get_trending_query = "SELECT * FROM daily_trends #{where_clauses} #{order_by_clause} OFFSET #{(page_num - 1) * size} LIMIT #{size}"
@@ -59,12 +23,34 @@ class RedshiftRankingsAccessor
 
     {
       "total" => RedshiftBase.query(get_total_query, expires: 30.minutes).fetch()[0]["count"],
-      "apps" => RedshiftBase.query(get_trending_query, expires: 30.minutes).fetch(),  # Note: the returned entries will have denormalized country and rank type attributes.
+      "apps" => RedshiftBase.query(get_trending_query, expires: 30.minutes).fetch()  # Note: the returned entries will have denormalized country and rank type attributes.
     }
   end
 
   def get_newcomers(platforms:[], countries:[], categories:[], rank_types:[], lookback_time: 14.days.ago, size: 20, page_num: 1)
-    nil # TODO
+
+    # Validate parameters
+
+    validate_parameters(platforms: platforms, countries: countries, categories: categories, rank_types: rank_types, size: size, page_num: page_num)
+    raise "Unsupported sort_by option" if !lookback_time.is_a? ActiveSupport::TimeWithZone
+
+    # Denormalize necessary parameters
+
+    denormalized_countries = generate_denormalized_countries(platforms, countries)
+    denormalized_rank_types = generate_denormalized_rank_types(platforms, rank_types)
+
+    # Perform queries
+
+    where_clauses = build_where_clauses(platforms, denormalized_countries, categories, denormalized_rank_types, lookback_time: lookback_time)
+    order_by_clause = "ORDER BY created_at DESC"
+
+    get_newcomers_query = "SELECT * FROM daily_newcomers #{where_clauses} #{order_by_clause} OFFSET #{(page_num - 1) * size} LIMIT #{size}"
+    get_total_query = "SELECT COUNT(app_identifier) FROM daily_newcomers #{where_clauses}"
+
+    {
+      "total" => RedshiftBase.query(get_total_query, expires: 30.minutes).fetch()[0]["count"],
+      "apps" => RedshiftBase.query(get_newcomers_query, expires: 30.minutes).fetch()  # Note: the returned entries will have denormalized country and rank type attributes.
+    }
   end
 
   def get_chart(platform:, country:, category:, rank_type:, size: 20, page_num: 1)
@@ -103,13 +89,59 @@ class RedshiftRankingsAccessor
 
 private
 
-  def build_where_clauses(platforms, countries, categories, rank_types, sort_by)
+  def generate_denormalized_countries(platforms, countries)
+    denormalized_countries = []
+    if platforms.include?  'ios'
+      countries.each do |country_code|
+        denormalized_countries.push(country_code_to_ios(country_code))
+      end
+    end
+    denormalized_countries.compact + countries
+  end
+
+  def generate_denormalized_rank_types(platforms, rank_types)
+    denormalized_rank_types = []
+    rank_types.each do |rank_type|
+      denormalized_rank_types.push(rank_type_to_ios(rank_type)) if platforms.include? 'ios'
+      denormalized_rank_types.push(rank_type_to_android(rank_type)) if platforms.include? 'android'
+    end
+    denormalized_rank_types
+  end
+
+  def validate_parameters(platforms:, countries:, categories:, rank_types:, size:, page_num:)
+    if (platforms - ['ios', 'android']).any?
+      raise "Unsupported platform option"
+    end
+
+    if (rank_types - ['free', 'paid', 'grossing']).any?
+      raise "Unsupported rank_type option"
+    end
+
+    if !size.is_a? Integer
+      raise "size must be an integer"
+    end
+
+    if !page_num.is_a? Integer
+      raise "page_num must be an integer"
+    end
+
+    countries.each do |country|
+      raise "Invalid country code." if country.length != 2
+    end
+
+    categories.each do |category|
+      raise "Invalid category" if category.include? "\"" or category.include? "'"
+    end
+  end
+
+  def build_where_clauses(platforms, countries, categories, rank_types, sort_by: nil, lookback_time: nil)
     where_clauses = []
     where_clauses.push("platform IN ('#{platforms.join("','")}')") if platforms.any?
     where_clauses.push("country IN ('#{countries.join("','")}')") if countries.any?
     where_clauses.push("category IN ('#{categories.join("','")}')") if categories.any?
     where_clauses.push("ranking_type IN ('#{rank_types.join("','")}')") if rank_types.any?
-    where_clauses.push("#{sort_by} IS NOT NULL")
+    where_clauses.push("#{sort_by} IS NOT NULL") if sort_by.present?
+    where_clauses.push("created_at > '#{lookback_time.strftime("%Y-%m-%d")}'") if lookback_time.present?
     return where_clauses.any? ? " WHERE " + where_clauses.join(" AND ") : ""
   end
 
