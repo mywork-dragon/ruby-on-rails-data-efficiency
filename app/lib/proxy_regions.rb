0d61466cb81d29551ccc08a71aaa1d706ab18ae9
mywork-dragon <dragonblueyounger@gmail.com>
2017-11-17 11:18:29 -0800

    Add Italy as a supported ProxyRegion.

--- app/lib/proxy_regions.rb
+++ app/lib/proxy_regions.rb
@@ -5,7 +5,7 @@ module ProxyRegions
     enum region: [
       :US, :BR, :IN, :FR, :KR, :CN, :DE, :ID, :RU, :VN,
       :JP, :AU, :SG, :GB, :DK, :NO, :SE, :FI, :ES, :CL,
-      :CO, :AR, :CA, :AT
+      :CO, :AR, :CA, :AT, :IT
     ]
   end
 end
