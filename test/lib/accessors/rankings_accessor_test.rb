bcd5e8a59a351ea6bed8f58238a8f0fc37feca22
mywork-dragon <dragonblueyounger@gmail.com>
2017-11-16 18:08:10 -0800

    Fixing rankings accessor test (#1316)

--- test/lib/accessors/rankings_accessor_test.rb
+++ test/lib/accessors/rankings_accessor_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class RankingsAccessorTest < ActiveSupport::TestCase
   test 'delegate has same methods as accessor' do
     x = RankingsAccessor.new
-    assert_equal [:delegate], (x.public_methods(false) -  x.delegate.public_methods(false))
+
+    # Using RankingsAccessor.any_instance.stub method in tests monkey patches 
+    # __android_categories_without_any_instance__ method.
+    assert_equal [], (x.public_methods(false) - x.delegate.public_methods(false)) - [:delegate, :__android_categories_without_any_instance__]
     assert_equal [:query_class_override=], (x.delegate.public_methods(false) -  x.public_methods(false))
   end
 end
