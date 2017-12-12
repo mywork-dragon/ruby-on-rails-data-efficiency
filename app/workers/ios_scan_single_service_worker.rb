95874e790060f8007b14338a76e76d9dd6ee540c
mywork-dragon <dragonblueyounger@gmail.com>
2017-12-11 19:50:19 -0800

    Revert "Update livescan queue."
    This reverts commit 1939638b28684d8f64456a9839ba7c82cea35463.

--- app/workers/ios_scan_single_service_worker.rb
+++ app/workers/ios_scan_single_service_worker.rb
@@ -2,7 +2,7 @@ class IosScanSingleServiceWorker
 
   include Sidekiq::Worker
 
-  sidekiq_options retry: false, queue: :lively
+  sidekiq_options retry: false, queue: :ios_live_scan
 
   def perform(ipa_snapshot_id, bid=nil)
     IosScanRunner.new(ipa_snapshot_id, :one_off, {
