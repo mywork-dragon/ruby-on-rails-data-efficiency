20ddb63b651e47c950a915a71f1b7bde94020496
mywork-dragon <dragonblueyounger@gmail.com>
2017-12-19 09:38:53 -0800

    Update the other fastest growing sdk blog post link.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -140,7 +140,7 @@ class WelcomeController < ApplicationController
   end
 
   def fastest_growing_sdks
-    @blog_post = 'https://blog.mightysignal.com'
+    @blog_post = "https://medium.com/@champagneshane/838657d3457a"
   end
 
   def lead_generation
