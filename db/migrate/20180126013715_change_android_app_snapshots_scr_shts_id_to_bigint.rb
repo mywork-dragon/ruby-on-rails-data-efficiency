3e8e77d2d7eb61fc20e660f8f8a2ebf0953b7d3d
mywork-dragon <dragonblueyounger@gmail.com>
2018-01-26 10:35:02 -0800

    Comment out migration

--- db/migrate/20180126013715_change_android_app_snapshots_scr_shts_id_to_bigint.rb
+++ db/migrate/20180126013715_change_android_app_snapshots_scr_shts_id_to_bigint.rb
@@ -1,5 +1,6 @@
 class ChangeAndroidAppSnapshotsScrShtsIdToBigint < ActiveRecord::Migration
   def change
-    change_column :android_app_snapshots_scr_shts, :id, :bigint, :null => false, :auto_increment => true
+    # change_column :android_app_snapshots_scr_shts, :id, :bigint, :null => false, :auto_increment => true
+    # This migration would not complete in production because the table was too large. 
   end
 end
