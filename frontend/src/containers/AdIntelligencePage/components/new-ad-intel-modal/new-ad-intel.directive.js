75e02450771de1c96da33fcbad7eb0db0a8206c4
mywork-dragon <dragonblueyounger@gmail.com>
2018-02-12 15:09:20 -0800

    Remove ad intel controller from modal directive (#1469)

--- frontend/src/containers/AdIntelligencePage/components/new-ad-intel-modal/new-ad-intel.directive.js
+++ frontend/src/containers/AdIntelligencePage/components/new-ad-intel-modal/new-ad-intel.directive.js
@@ -4,8 +4,9 @@ const template = require('./new-ad-intel-modal.html');
 
 angular.module('appApp')
   .directive('adIntelModal', () => ({
-    scope: true,
-    controller: 'AdIntelligenceController',
-    controllerAs: 'adIntel',
+    scope: {
+      adNetworks: '=',
+      modalNetwork: '=',
+    },
     template,
   }));
