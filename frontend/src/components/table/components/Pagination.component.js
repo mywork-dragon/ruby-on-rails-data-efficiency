70a678e5c94d0314ed8a41abd8c559409aeab0b3
mywork-dragon <dragonblueyounger@gmail.com>
2018-02-28 17:43:00 -0800

    Fix pagination (#1507)
    * Change to input type paginator when page count is over 50
    
    * Replace pagination input with antd number input
    
    * Allow strings
    
    * Update select element

--- frontend/src/components/table/components/Pagination.component.js
+++ frontend/src/components/table/components/Pagination.component.js
@@ -1,10 +1,11 @@
 /* eslint react/forbid-prop-types: 1 */
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import _ from 'lodash';
-
+import { InputNumber, Select } from 'antd';
 import ColumnPicker from './ColumnPicker.component';
+
+const Option = Select.Option;
 
 const defaultButton = props => (
   <button type="button" {...props} className="-btn">
@@ -16,96 +17,168 @@ defaultButton.propTypes = {
   children: PropTypes.element.isRequired,
 };
 
-const Pagination = ({
-  canNext,
-  canPrevious,
-  columns,
-  onPageChange,
-  onPageSizeChange,
-  page,
-  pages,
-  pageSizeOptions,
-  pageSize,
-  showColumnDropdown,
-  showPageSizeOptions,
-  showPageJump,
-  PreviousComponent = defaultButton,
-  NextComponent = defaultButton,
-  updateColumns,
-}) => (
-  <div className="-pagination">
-    <div className="-center">
-      {showColumnDropdown &&
-        <ColumnPicker
-          columns={columns}
-          onColumnChange={updateColumns}
-        />
-      }
-      {showPageSizeOptions &&
-        <span className="select-wrap -pageSizeOptions">
-          <select
-            onChange={e => onPageSizeChange(Number(e.target.value))}
-            value={pageSize}
+class Pagination extends React.Component {
+  constructor (props) {
+    super();
+
+    this.getSafePage = this.getSafePage.bind(this);
+    this.changePage = this.changePage.bind(this);
+    this.applyPage = this.applyPage.bind(this);
+
+    this.state = {
+      page: props.page,
+    };
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({ page: nextProps.page });
+  }
+
+  getSafePage (page) {
+    if (isNaN(page)) {
+      page = this.props.page;
+    }
+    return Math.min(Math.max(page, 0), this.props.pages - 1);
+  }
+
+  changePage (page) {
+    page = this.getSafePage(page);
+    this.setState({ page });
+    if (this.props.page !== page) {
+      this.props.onPageChange(page);
+    }
+  }
+
+  applyPage (e) {
+    if (e) { e.preventDefault(); }
+    const page = this.state.page;
+    this.changePage(page === '' ? this.props.page : page);
+  }
+
+  render () {
+    const {
+      canNext,
+      canPrevious,
+      columns,
+      onPageChange,
+      onPageSizeChange,
+      page,
+      pages,
+      pageSizeOptions,
+      pageSize,
+      showColumnDropdown,
+      showPageSizeOptions,
+      showPageJump,
+      PreviousComponent,
+      NextComponent,
+      updateColumns,
+    } = this.props;
+
+    let pagination;
+
+    if (showPageJump && pages > 50) {
+      pagination = (
+        <div className="-pageJump">
+          <InputNumber
+            max={pages}
+            min={1}
+            onBlur={this.applyPage}
+            onChange={(val) => {
+              const newPage = val - 1;
+              if (typeof val !== 'number') {
+                return this.setState({ page: val });
+              }
+              this.setState({ page: this.getSafePage(newPage) });
+            }}
+            size="small"
+            value={this.state.page === '' ? '' : this.state.page + 1}
+          />
+        </div>
+      );
+    } else if (showPageJump && pages <= 50) {
+      pagination = (
+        <div className="-pageJump">
+          <Select
+            onChange={(val) => {
+              const newPage = val - 1;
+              onPageChange(newPage);
+            }}
+            size="small"
+            value={page + 1}
           >
-            {pageSizeOptions.map((option, i) => (
+            {_.range(1, pages + 1).map((option, i) => (
               // eslint-disable-next-line react/no-array-index-key
-              <option key={i} value={option}>
-                {option} {'rows'}
-              </option>
+              <Option key={i} value={option}>
+                {option}
+              </Option>
             ))}
-          </select>
-        </span>}
-      <span className="-pageInfo">
-        {'Page'}{' '}
-        {showPageJump
-          ?
-            <div className="-pageJump">
+          </Select>
+        </div>
+      );
+    } else {
+      pagination = (
+        <span className="-currentPage">
+          {page + 1}
+        </span>
+      );
+    }
+
+    return (
+      <div className="-pagination">
+        <div className="-center">
+          {showColumnDropdown &&
+            <ColumnPicker
+              columns={columns}
+              onColumnChange={updateColumns}
+            />
+          }
+          {showPageSizeOptions &&
+            <span className="select-wrap -pageSizeOptions">
               <select
-                onChange={(e) => {
-                  const page = Number(e.target.value) - 1;
-                  onPageChange(page);
-                }}
-                value={page + 1}
+                onChange={e => onPageSizeChange(Number(e.target.value))}
+                value={pageSize}
               >
-                {_.range(1, pages + 1).map((option, i) => (
-                  // eslint-disable-next-line react/no-array-index-key
+                {pageSizeOptions.map((option, i) => (
+                    // eslint-disable-next-line react/no-array-index-key
                   <option key={i} value={option}>
-                    {option}
+                    {option} {'rows'}
                   </option>
-                ))}
+                  ))}
               </select>
-            </div> :
-            <span className="-currentPage">
-              {page + 1}
-            </span>}{' '}
-        {'of'}{' '}
-        <span className="-totalPages">{pages || 1}</span>
-      </span>
-    </div>
-    <div className="-previous">
-      <PreviousComponent
-        disabled={!canPrevious}
-        onClick={() => {
-          if (!canPrevious) return;
-          onPageChange(page - 1);
-        }}
-      >
-        <i className="fa fa-angle-left" />
-      </PreviousComponent>
-    </div>
-    <div className="-next">
-      <NextComponent
-        disabled={!canNext}
-        onClick={() => {
-          if (!canNext) return;
-          onPageChange(page + 1);
-        }}
-      >
-        <i className="fa fa-angle-right" />
-      </NextComponent>
-    </div>
-  </div>
-);
+            </span>}
+          <span className="-pageInfo">
+            {'Page'}{' '}
+            {pagination}
+            {' of '}
+            <span className="-totalPages">{pages || 1}</span>
+          </span>
+        </div>
+        <div className="-previous">
+          <PreviousComponent
+            disabled={!canPrevious}
+            onClick={() => {
+                      if (!canPrevious) return;
+                      onPageChange(page - 1);
+                    }}
+          >
+            <i className="fa fa-angle-left" />
+          </PreviousComponent>
+        </div>
+        <div className="-next">
+          <NextComponent
+            disabled={!canNext}
+            onClick={() => {
+                        if (!canNext) return;
+                        onPageChange(page + 1);
+                      }}
+          >
+            <i className="fa fa-angle-right" />
+          </NextComponent>
+        </div>
+      </div>
+    );
+  }
+}
 
 Pagination.propTypes = {
   canPrevious: PropTypes.bool.isRequired,
