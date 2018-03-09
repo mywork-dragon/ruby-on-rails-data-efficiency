35815f10c47d1f3c42af8bfecca415d072531524
mywork-dragon <dragonblueyounger@gmail.com>
2018-03-08 17:25:44 -0800

    Use hotstore for domain linker (#1529)

--- app/lib/domain_linker.rb
+++ app/lib/domain_linker.rb
@@ -3,32 +3,23 @@
 
 class DomainLinker
   @@hotstore = nil
+  @@dd_hotstore = nil
   def initialize()
     @downloaded = false
     @domain_to_publishers = Hash.new {[]}
     if @@hotstore.nil?
       @@hotstore = PublisherHotStore.new
     end
-  end
-
-  def download!
-    domain_pub_url = 'https://feeds.mightysignal.com/v1/internal/domain-publisher-relationships/latest/domain-publisher-relationships.gz'
-    open(domain_pub_url, "JWT" => ENV['MS_FEEDS_TOKEN']) do |f_remote|
-      csv = CSV.new(Zlib::GzipReader.new(f_remote).read)
-      csv.each do |row|
-        @domain_to_publishers[row[0]] = @domain_to_publishers[row[0]] + [row[1]]
-      end
+    if @@dd_hotstore.nil?
+      @@dd_hotstore = DomainDataHotStore.new
     end
-    @downloaded = true
   end
 
   def domain_to_publisher(domain)
-    if !@downloaded
-      download!
-    end
-    return @domain_to_publishers[domain].map do |publisher_id|
-      platform, publisher_id = publisher_id.split '-'
-      "#{platform}_developer".classify.constantize.find(publisher_id)
+    record = @@dd_hotstore.read(domain)
+
+    record.fetch('publishers', []).map do |publisher|
+      "#{publisher['platform']}_developer".classify.constantize.find(publisher['id'])
     end
   end
 
