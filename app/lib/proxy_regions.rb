d2d1b54ebcd287b0d71463cd8ef6a9f0cfc07e86
mywork-dragon <dragonblueyounger@gmail.com>
2019-02-28 16:48:27 -0500

    added indexes to region enum for quick look

--- app/lib/proxy_regions.rb
+++ app/lib/proxy_regions.rb
@@ -3,8 +3,13 @@ module ProxyRegions
   included do
     # ISO 3166-1 alpha-2
     enum region: [
+    #  0    1    2    3    4    5    6    7    8    9
       :US, :BR, :IN, :FR, :KR, :CN, :DE, :ID, :RU, :VN,
+
+    #  10   11   12   13   14   15   16   17   18   19
       :JP, :AU, :SG, :GB, :DK, :NO, :SE, :FI, :ES, :CL,
+
+    #  20   21   22   23   24
       :CO, :AR, :CA, :AT, :IT
     ]
   end
