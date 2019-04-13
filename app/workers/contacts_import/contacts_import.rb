0a142be6e4da4e2287e1532982a250c162510e1d
mywork-dragon <dragonblueyounger@gmail.com>
2019-04-13 13:23:12 -0500

    improved contacts import

--- app/workers/contacts_import/contacts_import.rb
+++ app/workers/contacts_import/contacts_import.rb
@@ -1,4 +1,5 @@
 require 'digest'
+require 'benchmark'
 class ContactsImport
   # This class imports and validate contacts from a file to clearbit.
   # It pulls the input data from AWS S3.
@@ -14,48 +15,59 @@ class ContactsImport
   sidekiq_options queue: :contacts_upload, retry: true
 
   STREAM_NAME = 'contacts_import'
-  POOL_SIZE   = 4 #Tied to DB connections pool in database.yml pool: 5 (must be less than those)
 
   def perform(file_name, file_content)
+    puts Benchmark.measure {
+
 
     contacts_data = CSV.parse(file_content, :headers => true)
-    websites_index = {}
+    headers, *contacts_data = contacts_data.to_a
+    fields_map = headers.each_with_index.inject({}){|memo,(name, position)| memo[name] = position and memo }
 
-    contacts_to_save = Queue.new
+    websites_index = {}
+    contacts_to_save = []
+
     ClearbitContact.transaction do
-      contacts_data.each do |contact_raw|
-        contacts_to_save.push ClearbitContact.find_or_initialize_by(email: contact_raw['employee_email']).tap do |contact_obj|
-          website_digest            = Digest::MD5.hexdigest "http://#{contact_raw["domain"]}-#{contact_raw["domain"]}"
-          contact_obj.given_name    = contact_raw["employee_first_name"]                 if contact_raw["employee_first_name"].present?
-          contact_obj.family_name   = contact_raw["employee_last_name"]                  if contact_raw["employee_last_name"].present?
-          contact_obj.full_name     = "#{contact_raw["employee_first_name"]} #{contact_raw["employee_last_name"]}".strip
-          contact_obj.linkedin      = contact_raw["employee_li"].andand.truncate(190)    if contact_raw["employee_li"].present?
-          contact_obj.email         = contact_raw["employee_email"]                      if contact_raw["employee_email"].present?
-          contact_obj.title         = contact_raw["employee_title"].andand.truncate(190) if contact_raw["employee_title"].present?
-          contact_obj.quality       = contact_raw["employee_email_confidence"]           if contact_raw["employee_email_confidence"].present?
+      old_logger = ActiveRecord::Base.logger
+      ActiveRecord::Base.logger = nil
+
+      contacts_to_save = contacts_data.map do |contact_raw|
+        employee_email = contact_raw[fields_map[ 'employee_email'           ]].presence
+        given_name     = contact_raw[fields_map[ 'employee_first_name'      ]].presence
+        family_name    = contact_raw[fields_map[ 'employee_last_name'       ]].presence
+        linkedin       = contact_raw[fields_map[ 'employee_li'              ]].presence
+        title          = contact_raw[fields_map[ 'employee_title'           ]].presence
+        quality        = contact_raw[fields_map[ 'employee_email_confidence']].presence
+        domain         = contact_raw[fields_map[ 'domain'                   ]].presence
+
+        ClearbitContact.find_or_initialize_by(email: employee_email).tap do |contact_obj|
+          website_digest            = Digest::MD5.hexdigest "http://#{domain}-#{domain}"
+          contact_obj.full_name     = "#{given_name} #{family_name}".strip.presence
+
+          contact_obj.given_name    = given_name     if given_name
+          contact_obj.family_name   = family_name    if family_name
+          contact_obj.email         = employee_email if employee_email
+          contact_obj.quality       = quality        if quality
+          contact_obj.linkedin      = linkedin.andand.truncate(190) if linkedin
+          contact_obj.title         = title.andand.truncate(190)    if title
+
           contact_obj.website       = websites_index[website_digest] ||
-                                      Website.find_or_create_by!(url: "http://#{contact_raw["domain"]}", domain: contact_raw["domain"]).tap do |web|
+                                      Website.find_or_create_by!(url: "http://#{domain}", domain: domain).tap do |web|
                                         websites_index[website_digest] = web
                                       end
-          contact_obj.website.domain_datum  = DomainDatum.find_or_initialize_by(domain: contact_raw["domain"])
-        end
-      end
 
-      threads = Array.new(POOL_SIZE) do
-        Thread.new do
-          begin
-            while contact_to_save = contacts_to_save.pop(true)
-              contact_to_save.save!
-            end
-          rescue ThreadError => e
-            raise e unless e.message == "queue empty"
-          end
+          contact_obj.website.domain_datum  = DomainDatum.find_or_initialize_by(domain: domain)
         end
       end
-      threads.map(&:join)
+      ActiveRecord::Base.logger = old_logger
+      result = ClearbitContact.import contacts_to_save, on_duplicate_key_update: [:given_name, :family_name, :linkedin, :email, :title, :quality]
     end
 
+  }
+
   rescue StandardError => e
+    byebug
     MightyAws::Firehose.new.send(stream_name: STREAM_NAME, data: file_name)
   end
+
 end
