6a463bf4d22e6567346a19f7f87e23d3cfcc9df2
mywork-dragon <dragonblueyounger@gmail.com>
2019-07-19 18:12:43 +0000

    more fixes

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,5 +1,6 @@
 module WelcomeHelper
-    def get_last(num, chart_json)
+    def get_last(num, chart_data)
+        chart_json = JSON.parse(chart_data)
         chart_json.sort_by{ |k,_| k.to_s.to_date }.reverse.first(num)
     end
 end
