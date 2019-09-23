3f341166982c03366f3c0c59b8c3424e9eb7a7b6
mywork-dragon <dragonblueyounger@gmail.com>
2019-09-23 16:56:42 -0500

    Changed data types to be integers

--- db/migrate/20190830212846_add_mau_to_app.rb
+++ db/migrate/20190830212846_add_mau_to_app.rb
@@ -1,9 +1,9 @@
 class AddMauToApp < ActiveRecord::Migration
   def change
-    add_column :android_apps, :mau, :decimal
-    add_column :android_apps, :mau_monthly_change, :decimal
+    add_column :android_apps, :mau, :integer
+    add_column :android_apps, :mau_monthly_change, :integer
 
-    add_column :ios_apps, :mau, :decimal
-    add_column :ios_apps, :mau_monthly_change, :decimal
+    add_column :ios_apps, :mau, :integer
+    add_column :ios_apps, :mau_monthly_change, :integer
   end
 end
