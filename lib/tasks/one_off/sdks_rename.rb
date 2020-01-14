a33acf538417309d9a063a7d16c584897440983b
mywork-dragon <dragonblueyounger@gmail.com>
2020-01-14 15:35:28 +0000

    PR changes

--- lib/tasks/one_off/sdks_rename.rb
+++ lib/tasks/one_off/sdks_rename.rb
@@ -4,19 +4,17 @@ desc 'Rename SDKs'
 task :rename_sdks => [:environment] do
     file_content = MightyAws::S3.new.retrieve( bucket: 'ms-misc', key_path: 'android_mass_rename_varys.csv', ungzip: false )
     csv = CSV.parse(file_content, headers: true)
-    destroyed = []
-    renamed = []
-    skipped_destroy = []
-    skipped_rename = []
     ActiveRecord::Base.transaction do
         csv.each do |row|
+            hose = {}
+            hose['id'] = row['ID']
             if row['Destroy?'] == 'TRUE'
                 begin
                     sdk = AndroidSdk.find row['ID']
                     sdk.destroy
-                    destroyed << row['ID']
-                rescue
-                    skipped_destroy << row['ID']
+                    hose['action'] = 'destroyed'
+                rescue Exception => e
+                    hose['error'] = e
                 end
             else
                 begin
@@ -26,10 +24,12 @@ task :rename_sdks => [:environment] do
                     if tag.present? && sdk.tags.empty?
                         sdk.tags << tag
                     end
-                rescue
-                    skipped_rename << row['ID']
+                    hose['action'] = 'updated'
+                rescue Exception => e
+                    hose['error'] = e
                 end
             end
+            res = MightyAws::Firehose.new.send(stream_name: 'sdk_rename', data: hose.to_json)
         end
     end
 end
