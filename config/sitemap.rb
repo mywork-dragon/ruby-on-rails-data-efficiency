4f65ee01f0e219c9e9daa61665206b5000fae048
mywork-dragon <dragonblueyounger@gmail.com>
2020-01-30 00:49:23 +0000

    copied ryans pr for sitemaps off master branch

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,23 @@
-require_relative 'sitemap_base'
+require 'rubygems'
+require 'sitemap_generator'
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://mightysignal.com"
+SitemapGenerator::Sitemap.sitemaps_host = "https://mightysignal-sitemaps.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new("mightysignal-sitemaps", aws_access_key_id: "#{ENV['S3_ACCESS_KEY_ID']}", aws_secret_access_key: "#{ENV['S3_SECRET_ACCESS_KEY']}", aws_region: 'us-east-1')
 
 SitemapGenerator::Sitemap.create do
+  group(:sitemaps_path => 'dynamic/', :filename => :dynamic, :changefreq => 'daily', :priority => 0.9) do
+    add root_path
+    add top_ios_sdks_path
+    add top_ios_apps_path
+    add top_android_sdks_path
+    add top_android_apps_path
+    add fastest_growing_android_sdks_path
+    add timeline_path
+  end
+
   group(:sitemaps_path => 'static/', :filename => :static, :changefreq => 'monthly', :priority => 0.5) do
     add data_path
     add publisher_contacts_path
@@ -34,6 +51,18 @@ SitemapGenerator::Sitemap.create do
     Tag.find_each do |tag|
       add sdk_category_page_path(tag.id, tag.name.to_s.parameterize)
       add sdk_category_directory_sdks_path(tag.id, tag.name.to_s.parameterize)
+    end
+  end
+  
+  group(:sitemaps_path => 'ios_apps/', :filename => :ios_apps, :changefreq => 'weekly', :priority => 0.6) do
+    IosApp.each do |id|
+      add "/a/ios/#{id}"
+    end
+  end
+  
+  group(:sitemaps_path => 'android_apps/', :filename => :android_apps, :changefreq => 'weekly', :priority => 0.6) do
+    AndroidApp.each do |id|
+      add "/a/android/#{id}"
     end
   end
 end
