68e011ddde4d4c845d259011bd1d43013396872d
mywork-dragon <dragonblueyounger@gmail.com>
2020-04-23 08:05:21 -0500

    Created actual tests for AppIdentifierHotStore#write method

--- app/lib/hot_store/app_identifier_hot_store.rb
+++ app/lib/hot_store/app_identifier_hot_store.rb
@@ -1,8 +1,10 @@
 class AppIdentifierHotStore < HotStore
 
+  # We don't have a key_set in the hotstore for this mapping
+
+  KEY_TYPE = 'ai'.freeze
+
   def write(platform, app_identifier, id)
-    puts "**********************************************"
-    puts "Received #{platform}, #{app_identifier}, #{id}"
-    true
+    write_flat_entry(KEY_TYPE, platform, app_identifier, id)
   end
 end
